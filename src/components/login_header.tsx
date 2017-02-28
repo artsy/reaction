00f3bbcda70b9f06a30ea13e059befdec7e4995d
xtina-starr <c.thompsonms@gmail.com>
2017-02-28 10:14:49 -0500

    use correct module

--- src/components/login_header.tsx
+++ src/components/login_header.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import styled from "styled-components"
-import Icon from "./icons"
+import Icon from "./icon"
 import Title from "./title"
 
 const Header = styled.header`
