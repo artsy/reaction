aa4abc2a11f7bef440f4d0301605a0719ed3e118
Yuki Nishijima <mail@yukinishijima.net>
2017-03-30 05:05:49 +0000

    Add the ability to specify a typeface

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -4,9 +4,11 @@ import * as fonts from "../assets/fonts"
 
 type TextAlign = "start" | "center" | "end"
 type TextSize = "small" | "large"
+type TextStyle = "primary" | "secondary"
 
 interface TextProps extends React.HTMLProps<HTMLParagraphElement> {
   textSize?: TextSize,
+  textStyle?: TextStyle,
   align?: TextAlign,
   color?: string,
 }
@@ -14,6 +16,11 @@ interface TextProps extends React.HTMLProps<HTMLParagraphElement> {
 const textSizes = {
   small: "17px",
   large: "20px",
+}
+
+const textStyleNameToCss = {
+  primary: fonts.primary.style,
+  secondary: fonts.secondary.style,
 }
 
 const RawText: React.SFC<TextProps> = props => (
@@ -26,11 +33,12 @@ const Text = styled(RawText)`
   font-size: ${props => textSizes[props.textSize]};
   text-align: ${props => props.align};
   color: ${props => props.color};
-  ${fonts.secondary.style};
+  ${props => textStyleNameToCss[props.textStyle]};
 `
 
 Text.defaultProps = {
   textSize: "small",
+  textStyle: "secondary",
   align: "start",
   color: "currentcolor",
 }
