f50d51c5c86f9530abb1cb19961a561dc0509b47
Charles Broskoski <broskoski@gmail.com>
2017-03-30 10:38:54 -0400

    Fix font path

--- src/assets/fonts.tsx
+++ src/assets/fonts.tsx
@@ -3,12 +3,12 @@ import { css, injectGlobal } from "styled-components"
 injectGlobal`
   @font-face {
     font-family: 'artsy-icons';
-    src: url("/fonts/artsy-icons.eot?uo9ko");
-    src: url("/fonts/artsy-icons.eot?#iefixuo9ko") format('embedded-opentype'),
-         url("/fonts/artsy-icons.woff2?uo9ko") format('woff2'),
-         url("/fonts/artsy-icons.ttf?uo9ko") format('truetype'),
-         url("/fonts/artsy-icons.woff?uo9ko") format('woff'),
-         url("/fonts/artsy-icons.svg?uo9ko#artsy-icons") format('svg');
+    src: url("/assets/fonts/artsy-icons.eot?uo9ko");
+    src: url("/assets/fonts/artsy-icons.eot?#iefixuo9ko") format('embedded-opentype'),
+         url("/assets/fonts/artsy-icons.woff2?uo9ko") format('woff2'),
+         url("/assets/fonts/artsy-icons.ttf?uo9ko") format('truetype'),
+         url("/assets/fonts/artsy-icons.woff?uo9ko") format('woff'),
+         url("/assets/fonts/artsy-icons.svg?uo9ko#artsy-icons") format('svg');
     font-weight: normal;
     font-style: normal;
   }
