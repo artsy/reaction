f6be618332081ff4e09388a7e5383fb3184a7fe7
Charles Broskoski <broskoski@gmail.com>
2017-04-04 16:19:31 -0400

    Change entry point location

---
+++ src/components/buttons/index.tsx
@@ -0,0 +1,13 @@
+import Button from "./default"
+import FacebookButton from "./facebook"
+import GhostButton from "./ghost"
+import InvertedButton from "./inverted"
+import TwitterButton from "./twitter"
+
+export default {
+  Button,
+  FacebookButton,
+  GhostButton,
+  InvertedButton,
+  TwitterButton,
+}
