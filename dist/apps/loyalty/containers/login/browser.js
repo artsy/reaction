3f98cfce23d53850b599ef2d85b06958e8279fdb
Charles Broskoski <broskoski@gmail.com>
2017-04-04 17:05:39 -0400

    Remove dist folder

--- dist/apps/loyalty/containers/login/browser.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var React = require("react");
-var react_dom_1 = require("react-dom");
-var index_1 = require("./index");
-react_dom_1.render(React.createElement(index_1.default, null), document.getElementById("app-container"));
-//# sourceMappingURL=browser.js.map
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var apps_1 = require("./apps");
-var components_1 = require("./components");
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    apps: apps_1.default,
-    components: components_1.default };
-//# sourceMappingURL=index.js.map
