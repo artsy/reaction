f7115faab992cb3a7b911f58e9b4e8f030369730
Eloy Durán <eloy.de.enige@gmail.com>
2017-04-14 13:10:14 +0200

    [dist] Fix source-maps to point to correct original files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,38 @@
-const babel = require('gulp-babel');
-const gulp = require('gulp');
-const clean = require('gulp-clean');
-const tsc = require('gulp-typescript');
-const sourcemaps = require('gulp-sourcemaps');
+const babel = require("gulp-babel")
+const clean = require("gulp-clean")
+const gulp = require("gulp")
+const path = require("path")
+const sourcemaps = require("gulp-sourcemaps")
+const tsc = require("gulp-typescript")
 
-let node;
+const srcDir = "./src"
+const outDir = "./dist"
 
 // TODO: read app name from command line option
-const appName = 'loyalty';
+const appName = "loyalty"
 
-gulp.task('clean', function() {
-    return gulp.src('dist', { read: false })
-        .pipe(clean());
-});
+gulp.task("clean", function() {
+  return gulp.src(outDir, { read: false })
+    .pipe(clean())
+})
 
-gulp.task('compile-server', () => {
-    const tsProject = tsc.createProject('tsconfig.json')
+gulp.task("compile-server", () => {
+  const tsProject = tsc.createProject("tsconfig.json")
 
-    const tsResult = gulp.src(`src/**/*.{ts,tsx}`)
-        .pipe(sourcemaps.init())
-        .pipe(tsProject())
+  const tsResult = gulp.src(`${srcDir}/**/*.{ts,tsx}`)
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
 
-    return tsResult.js
-        .pipe(babel())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'))
+  return tsResult.js
+    .pipe(babel())
+    .pipe(sourcemaps.write(".", {
+      // Properly map paths contained in the resulting source-map to the original source,
+      // as described here https://github.com/gulp-sourcemaps/gulp-sourcemaps/issues/191#issuecomment-278361101
+      mapSources: (sourcePath, file) => {
+        const distDir = path.join(outDir, path.dirname(file.relative))
+        const relativeDistDir = path.relative(distDir, srcDir)
+        return path.join(relativeDistDir, sourcePath)
+      },
+    }))
+    .pipe(gulp.dest(outDir))
 })
