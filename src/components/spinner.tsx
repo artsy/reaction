711df1f654878afd53fe3a480f88e5f22083b753
Charles Broskoski <broskoski@gmail.com>
2017-05-02 14:45:37 -0400

    Add Spinner component

---
+++ src/components/spinner.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+
+import styled, { keyframes } from "styled-components"
+
+interface Props extends React.HTMLProps<Spinner> {
+  width?: number,
+  height?: number
+}
+
+export class Spinner extends React.Component<Props, null> {
+  render() {
+    return (
+      <div className={this.props.className} />
+    )
+  }
+}
+
+const spin = keyframes`
+	100% {
+		transform: rotate(360deg)
+	}
+`
+
+const StyledSpinner = styled(Spinner)`
+  background: black;
+  animation: ${spin} 1s infinite linear;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  position: absolute;
+  top: calc(50% - ${props => props.height}px / 2);
+  left: calc(50% - ${props => props.width}px / 2);
+`
+
+StyledSpinner.defaultProps = {
+  width: 25,
+  height: 6,
+}
+
+export default StyledSpinner
