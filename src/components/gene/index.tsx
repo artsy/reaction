5039130fc0818ea29ad22710797aee889d5560fc
Charles Broskoski <broskoski@gmail.com>
2017-05-24 16:16:25 -0400

    Add ArtistRow to GeneContents view

--- src/components/gene/index.tsx
+++ src/components/gene/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react"
 import * as Relay from "react-relay"
 
+import Artworks from "../artwork_grid"
+import ArtistRow from "./artist_row"
+
+const PageSize = 10
+
 interface Props extends RelayProps, React.HTMLProps<GeneContents> {
   gene: any
 }
@@ -8,21 +13,54 @@ interface Props extends RelayProps, React.HTMLProps<GeneContents> {
 export class GeneContents extends React.Component<Props, null> {
 
   render() {
+
+    let artists = this.props.gene.artists.edges.map(edge => {
+      return (
+        <ArtistRow artist={edge.node as any} key={edge.__dataID__} />
+      )
+    })
+
     return (
       <div>
-        {this.props.gene.name}
-        {this.props.gene.mode}
+        {artists}
       </div>
     )
   }
 }
 
 export default Relay.createContainer(GeneContents, {
+  initialVariables: {
+    showArtists: true,
+    artworksSize: PageSize,
+    artistsSize: PageSize,
+    medium: "*",
+    aggregations: ["MEDIUM", "TOTAL", "PRICE_RANGE", "DIMENSION_RANGE"],
+    price_range: "*",
+    dimension_range: "*",
+    sort: "-partner_updated_at",
+  },
   fragments: {
     gene: () => Relay.QL`
       fragment on Gene {
         mode
         name
+        artists: artists_connection(first: $artistsSize) @include(if: $showArtists) {
+          edges {
+            node {
+              ${ArtistRow.getFragment("artist")}
+            }
+          }
+        }
+        artworks: artworks_connection(
+          first: $artworksSize,
+          aggregations: $aggregations, 
+          medium: $medium,
+          price_range: $price_range,
+          dimension_range: $dimension_range,
+          sort: $sort,
+        ) @skip(if: $showArtists) {
+          ${Artworks.getFragment("artworks")}
+        }
       }
     `,
   },
