619a9fa85f4a92f6d703ff48845f645b0274bf72
Eloy Durán <eloy.de.enige@gmail.com>
2017-05-31 12:13:28 +0200

    [helpers] Add typings for `media` functions.

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -11,15 +11,15 @@ import { media } from "./helpers"
 export const Grid = FlexGrid
 
 export const Col: FlexCol = styled(FlexCol)`
-     ${media.sm`
-      padding-left: 0.5rem;
-      padding-right: 0.5rem;
-    `}
-  `
+    ${media.sm`
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
+  `}
+`
 
 export const Row: FlexRow = styled(FlexRow)`
-    ${media.sm`
-      margin-left: 1rem;
-      margin-right: 1rem;
-    `}
-  `
+  ${media.sm`
+    margin-left: 1rem;
+    margin-right: 1rem;
+  `}
+`
