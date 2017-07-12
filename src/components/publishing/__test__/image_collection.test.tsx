16bc790652a4ad1a9e971c7657f788b4e03ea308
kanaabe <kanakoabe5@gmail.com>
2017-07-12 18:14:42 -0400

    Tests

--- src/components/publishing/__test__/image_collection.test.tsx
+++ src/components/publishing/__test__/image_collection.test.tsx
@@ -5,6 +5,6 @@ import ImageCollection from "../image_collection"
 import { Images } from "./fixtures"
 
 it("renders properly", () => {
-  const image = renderer.create(<ImageCollection images={Images} />).toJSON()
+  const image = renderer.create(<ImageCollection images={Images} width={900} targetHeight={400} gutter={10} />).toJSON()
   expect(image).toMatchSnapshot()
 })
