79f2cd0ccabb7895f5e47f03d180ae6296d0fb54
Eve Essex <eve.essex@gmail.com>
2017-09-13 16:55:24 -0400

    rename duplicate ids, fix react attr naming

--- src/components/publishing/icon/layout_text.tsx
+++ src/components/publishing/icon/layout_text.tsx
@@ -5,7 +5,7 @@ class IconLayoutText extends Component<any, null> {
     return (
       <svg id="layout-text" width="45px" height="30px" viewBox="0 0 45 30" xmlns="http://www.w3.org/2000/svg">
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-          <g id="group" fill={this.props.fill ? this.props.fill : "#000"}>
+          <g id="layout-text-group" fill={this.props.fill ? this.props.fill : "#000"}>
             <polyline points="0 2 34 2 34 0 0 0" />
             <polyline points="0 7 26 7 26 5 0 5" />
             <rect x="0" y="10" width="45" height="20" />
