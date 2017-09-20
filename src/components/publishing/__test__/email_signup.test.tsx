edd601c6abfd746c416a6ab9af8e48d4976b0711
kanaabe <kanakoabe5@gmail.com>
2017-09-20 14:01:40 -0400

    Remove babel-plugin-rewire and use jest mocking

--- src/components/publishing/__test__/email_signup.test.tsx
+++ src/components/publishing/__test__/email_signup.test.tsx
@@ -2,7 +2,12 @@ import { mount } from "enzyme"
 import "jest-styled-components"
 import * as React from "react"
 import * as renderer from "react-test-renderer"
+import request from "request"
 import EmailSignup from "../email_signup"
+
+jest.mock("request", () => {
+  return { post: jest.fn() }
+})
 
 describe("EmailSignup", () => {
   it("renders an email signup", () => {
@@ -11,12 +16,10 @@ describe("EmailSignup", () => {
   })
 
   it("submits an email", () => {
-    const post = jest.fn()
-    EmailSignup.__Rewire__("request", { post })
     const viewer = mount(<EmailSignup signupUrl="#" />)
     viewer.setState({ value: "foo@goo.net" })
     viewer.find("button").simulate("click")
-    expect(post).toBeCalled()
-    expect(post.mock.calls[0][0].email).toEqual("foo@goo.net")
+    expect(request.post).toBeCalled()
+    expect(request.post.mock.calls[0][0].email).toEqual("foo@goo.net")
   })
 })
