0c5c43a667aaf8d102508fd16780ae288cb93695
xtina-starr <c.thompsonms@gmail.com>
2017-10-09 17:32:53 -0400

    [Onboarding] use circle checkbox for icon

--- src/components/onboarding/__stories__/selectable_link.story.tsx
+++ src/components/onboarding/__stories__/selectable_link.story.tsx
@@ -1,12 +1,12 @@
-import { storiesOf } from "@storybook/react"
-import * as React from "react"
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
 
-import SelectableLink from "../selectable_link"
+import SelectableLink from '../selectable_link';
 
 storiesOf("Onboarding", module).add("SelectableLink", () => {
   return (
     <div style={{ width: "400px" }}>
-      <SelectableLink text="Buy Art & Design" onSelect={this.onSelect} />
+      <SelectableLink text="Buy Art & Design" onSelect={selected => selected} />
     </div>
   )
 })
