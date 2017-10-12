0442c8f101980c4e6f060de4e1e1fbe77ae06e54
xtina-starr <c.thompsonms@gmail.com>
2017-10-12 15:47:31 -0400

    [Onboarding] created ArtistSearchResult component and corresponding relay route/query

--- src/Components/Onboarding/Steps/Artists/ArtistList.tsx
+++ src/Components/Onboarding/Steps/Artists/ArtistList.tsx
@@ -1,15 +1,15 @@
 import * as React from "react"
 
-import PopularArtists from "./PopularArtists"
+import ArtistSearchResults from "./ArtistSearchResults"
+// import PopularArtists from "./PopularArtists"
 
 export interface Props {
   searchQuery: string
 }
 
 export default class ArtistList extends React.Component<Props, any> {
-  // if searchQ "" show pop else show filtered artists with this prop= Q
-
   render() {
-    return <PopularArtists />
+    return <ArtistSearchResults />
+    // return <PopularArtists />
   }
 }
