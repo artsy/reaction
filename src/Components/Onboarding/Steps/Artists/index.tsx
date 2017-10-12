3522bc1b721e58e5123a48ee8c65db9920ed12d9
Maxim Cramer <mennenia@gmail.com>
2017-10-12 17:04:35 +0100

    [Onboarding] Moved Input out up to the artist index

--- src/Components/Onboarding/Steps/Artists/index.tsx
+++ src/Components/Onboarding/Steps/Artists/index.tsx
@@ -1,10 +1,21 @@
 import * as React from "react"
 import * as Relay from "react-relay/classic"
+import styled from "styled-components"
+
+import colors from "../../../../Assets/Colors"
+import Icon from "../../../Icon"
+import Input from "../../../Input"
 
 import SelectableItemContainer from "./SelectableItemContainer"
 
 import { StepProps } from "../../Types"
 import { Layout } from "../Layout"
+
+const OnboardingSearchBox = styled.div`
+  width: 450px;
+  margin: 0 auto 100px;
+  border-bottom: 1px solid #e5e5e5;
+`
 
 export interface RelayProps {
   popular_artists: {
@@ -12,18 +23,14 @@ export interface RelayProps {
   }
 }
 
-/**
- * Artists
- * -> Input
- *    List
- *    -> Popular
- *       Search
- */
-
 class Artists extends React.Component<StepProps & RelayProps, null> {
   // onInputChange = e => {
   //   this.props.onStateChange({ nextButtonEnabled: true })
   // }
+
+  searchTextChanged(e) {
+    return null
+  }
 
   render() {
     return (
@@ -32,7 +39,19 @@ class Artists extends React.Component<StepProps & RelayProps, null> {
         subtitle="Follow one or more"
         onNextButtonPressed={null}
       >
-        <SelectableItemContainer artists={this.props.popular_artists.artists} placeholder="Search artists..." />
+        <OnboardingSearchBox>
+          <Input
+            placeholder={"Search artists..."}
+            leftView={<Icon name="search" color={colors.graySemibold} />}
+            block
+            onInput={this.searchTextChanged.bind(this)}
+            onPaste={this.searchTextChanged.bind(this)}
+            onCut={this.searchTextChanged.bind(this)}
+          />
+          <div style={{ marginBottom: "35px" }} />
+          {/* <ArtistList /> */}
+          <SelectableItemContainer artists={this.props.popular_artists.artists} />
+        </OnboardingSearchBox>
       </Layout>
     )
   }
