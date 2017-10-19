fe93c3f330c9503ef9e937470879f9d558b186ec
xtina-starr <c.thompsonms@gmail.com>
2017-10-19 13:46:37 -0400

    [Onboarding] nest match_artist inside viewer root object

--- src/Components/Onboarding/Steps/Artists/ArtistSearchResults.tsx
+++ src/Components/Onboarding/Steps/Artists/ArtistSearchResults.tsx
@@ -1,23 +1,23 @@
 import * as React from "react"
 import * as Relay from "react-relay"
 
-import { artsyNetworkLayer } from "../../../../Relay/config"
-import ArtistSearchQueryConfig from "../../../../Relay/Queries/ArtistSearchResults"
+import { ArtistSearchQueryConfig } from "../../../../Relay/Queries/ArtistSearch"
 
 import SelectableItemContainer from "./SelectableItemContainer"
 
 export interface RelayProps {
-  searchResults: any[]
-}
-
-export default class ArtistSearchResults extends React.Component<any, any> {
-  render() {
-    return <ArtistSearchResultsContentList />
+  viewer: {
+    match_artist: any[]
   }
 }
 
-function ArtistSearchResultsContentList() {
-  Relay.injectNetworkLayer(artsyNetworkLayer())
+// export default class ArtistSearchResults extends React.Component<any, any> {
+//   render() {
+//     return <ArtistSearchResultsContentList />
+//   }
+// }
+
+export default function ArtistSearchResultsContentList() {
   return (
     <Relay.RootContainer
       Component={wrappedArtistSearchResultsContent}
@@ -28,15 +28,20 @@ function ArtistSearchResultsContentList() {
 
 class ArtistSearchResultsContent extends React.Component<RelayProps, null> {
   render() {
-    return <SelectableItemContainer artists={this.props.searchResults} />
+    return <SelectableItemContainer artists={this.props.viewer.match_artist} />
   }
 }
 
 const wrappedArtistSearchResultsContent = Relay.createContainer(ArtistSearchResultsContent, {
+  initialVariables: {
+    term: null,
+  },
   fragments: {
-    searchResults: () => Relay.QL`
-      fragment on Artist @relay(plural: true) {
-        ${SelectableItemContainer.getFragment("artists")}
+    viewer: () => Relay.QL`
+      fragment on Viewer {
+        match_artist(term: $term) {
+          ${SelectableItemContainer.getFragment("artists")}
+        }
       }
     `,
   },
---
+++ src/Relay/Queries/ArtistSearch.ts
@@ -0,0 +1,19 @@
+import * as Relay from "react-relay"
+
+export class ArtistSearchQueryConfig extends Relay.Route {
+  public static queries = {
+    viewer: (component, params) => Relay.QL`
+      query {
+        viewer {
+          
+          ${component.getFragment("viewer", params)}
+        }
+      }
+    `,
+  }
+  public static paramDefinitions = {
+    term: { required: true },
+  }
+
+  public static routeName = "ArtistSearchQuery"
+}
