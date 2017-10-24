92a750f321ec18d4df6e1db031d2ac5316015ae2
Eve Essex <eve.essex@gmail.com>
2017-10-24 16:17:51 -0400

    include image-set layouts

--- src/Components/Publishing/Typings.ts
+++ src/Components/Publishing/Typings.ts
@@ -1,5 +1,5 @@
 export type Layout = "classic" | "standard" | "feature"
-export type SectionLayout = "blockquote" | "column_width" | "fillwidth" | "overflow_fillwidth"
+export type SectionLayout = "blockquote" | "column_width" | "fillwidth" | "full" | "mini" | "overflow_fillwidth"
 
 export type ArticleData = {
   layout: Layout
