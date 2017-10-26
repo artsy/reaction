6d48f7a7c16332a4b3f3849b4d5f592b7cac5d8f
Matthew Zikherman <matt@bestvendor.com>
2017-10-26 16:20:27 -0400

    keep this old school

--- src/Components/__stories__/ArtworkMetadata.story.tsx
+++ src/Components/__stories__/ArtworkMetadata.story.tsx
@@ -1,6 +1,6 @@
 import { storiesOf } from "@storybook/react"
 import * as React from "react"
-import * as Relay from "react-relay"
+import * as Relay from "react-relay/classic"
 
 import ArtworkMetadata from "../Artwork/Metadata"
 
