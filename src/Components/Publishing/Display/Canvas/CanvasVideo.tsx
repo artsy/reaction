620543cb18bae2dca61df1b20d36fd917b1707eb
Craig Spaeth <craigspaeth@gmail.com>
2017-10-26 14:08:14 -0400

    Cleanup

--- src/Components/Publishing/Display/Canvas/CanvasVideo.tsx
+++ src/Components/Publishing/Display/Canvas/CanvasVideo.tsx
@@ -1,3 +1,4 @@
+import { once } from "lodash"
 import React from "react"
 import styled from "styled-components"
 import track from "../../../../Utils/track"
@@ -18,13 +19,13 @@ export class CanvasVideo extends React.Component<VideoProps, any> {
     this.state = { isPlaying: false }
   }
 
-  @track(props => ({
+  @track(once((props) => ({
     action: "Click",
     label: "Display ad play video",
     entity_type: "display_ad",
     campaign_name: props.campaign.name,
     unit_layout: "canvas_standard"
-  }))
+  })))
   onPlayVideo() {
     if (this.video) {
       if (this.video.paused) {
