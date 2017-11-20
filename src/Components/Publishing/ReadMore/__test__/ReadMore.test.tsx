741550cf77787e4b043d82abaddf3b76f21fb3d2
kanaabe <kanakoabe5@gmail.com>
2017-11-20 14:56:32 -0500

    Adds a test

--- src/Components/Publishing/ReadMore/__test__/ReadMore.test.tsx
+++ src/Components/Publishing/ReadMore/__test__/ReadMore.test.tsx
@@ -29,6 +29,20 @@ describe("ReadMore", () => {
     expect(viewer.state().truncationHeight).toEqual(200)
   })
 
+  it("shows the whole article when not truncated", () => {
+    const readMore = (
+      <ReadMoreWrapper isTruncated={false} hideButton={jest.fn()}>
+        <Sections article={StandardArticle} />
+      </ReadMoreWrapper>
+    )
+    const viewer = mount(readMore)
+    jest.runAllTimers()
+    expect(viewer.state().truncationHeight).toEqual("100%")
+    expect(viewer.find("div").at(0).prop("style")).toEqual({
+      height: "100%",
+      overflow: "auto" })
+  })
+
   it("calls hideButton when truncation is too small", () => {
     const smallArticle = extend({}, StandardArticle, {
       sections: [
