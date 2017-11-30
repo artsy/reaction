c2f4ab9348c9a3009344ef39589cd0a849a02530
Eve Essex <eve.essex@gmail.com>
2017-11-30 11:26:21 -0500

    minor icon fixes

--- src/Components/Publishing/Icon/IconUnorderedList.tsx
+++ src/Components/Publishing/Icon/IconUnorderedList.tsx
@@ -7,7 +7,7 @@ export class IconUnorderedList extends Component<any, null> {
 
   render() {
     return (
-      <svg className='IconUnorderedList' x="0px" y="0px" viewBox="-297 389.3 15.7 15.7" enable-background="new -297 389.3 15.7 15.7" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <svg className='IconUnorderedList' x="0px" y="0px" viewBox="-297 389.3 15.7 15.7" enableBackground="new -297 389.3 15.7 15.7" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <rect x="-291.6" y="390.9" fill={this.props.color} width="10.2" height="1.3" />
         <rect x="-291.6" y="396.5" fill={this.props.color} width="10.2" height="1.3" />
         <rect x="-291.6" y="402.1" fill={this.props.color} width="10.2" height="1.3" />
