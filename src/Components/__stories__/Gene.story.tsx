a89ff30ac1d3c21a8e43843dfcb3dc06c3b482ec
Matthew Zikherman <matt@bestvendor.com>
2017-12-14 17:36:02 -0500

    Remove staging credentials

--- src/Components/__stories__/Gene.story.tsx
+++ src/Components/__stories__/Gene.story.tsx
@@ -8,7 +8,7 @@ storiesOf("Components/Pages/Gene/Contents", module)
   .add("Artists Mode - Minimalism", () => {
     return (
       <div>
-        < ContextProvider currentUser={{id: "58ed621f9c18db55bc6b2dde", name: "Matt", accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OGVkNjIxZjljMThkYjU1YmM2YjJkZGUiLCJzYWx0X2hhc2giOiI2ZTVkMWVhYzJmMWQzNDhmZmZmOGJmMDAyOGQ1OTViOCIsInJvbGVzIjoidXNlciIsInBhcnRuZXJfaWRzIjpbXSwiZXhwIjoxNTE3NzYwMjA0LCJpYXQiOjE1MTI1NzYyMDQsImF1ZCI6IjRlMzZlZmE0ZGI0ZTMyMDAwMTAwMDM1OSIsImlzcyI6IkdyYXZpdHkiLCJqdGkiOiI1YTI4MTRjYzhiM2I4MTQ1ZjI2YTliNzUifQ.ksCRUaQRvGNRW2VnOSAAT32r9PM9s04axn7SqI-_Kno"}}>
+        < ContextProvider currentUser={{id: "", name: "", accessToken: ""}}>
           <Contents geneID="minimalism" mode="artists" onStateChange={console.log} />
         </ContextProvider>
       </div>
@@ -18,7 +18,7 @@ storiesOf("Components/Pages/Gene/Contents", module)
   .add("Artworks Mode - Animals", () => {
     return (
       <div>
-        < ContextProvider currentUser={{id: "58ed621f9c18db55bc6b2dde", name: "Matt", accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OGVkNjIxZjljMThkYjU1YmM2YjJkZGUiLCJzYWx0X2hhc2giOiI2ZTVkMWVhYzJmMWQzNDhmZmZmOGJmMDAyOGQ1OTViOCIsInJvbGVzIjoidXNlciIsInBhcnRuZXJfaWRzIjpbXSwiZXhwIjoxNTE3NzYwMjA0LCJpYXQiOjE1MTI1NzYyMDQsImF1ZCI6IjRlMzZlZmE0ZGI0ZTMyMDAwMTAwMDM1OSIsImlzcyI6IkdyYXZpdHkiLCJqdGkiOiI1YTI4MTRjYzhiM2I4MTQ1ZjI2YTliNzUifQ.ksCRUaQRvGNRW2VnOSAAT32r9PM9s04axn7SqI-_Kno"}}>
+        < ContextProvider currentUser={{id: "", name: "", accessToken: ""}}>
           <Contents geneID="animals" mode="artworks"  onStateChange={console.log} />
         </ContextProvider>
       </div>
