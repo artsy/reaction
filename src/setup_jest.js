7a4b0d39e258c553599a2a7d6509d287db7e5af5
Luc Succes <l2succes@gmail.com>
2017-12-20 15:39:42 +0700

    [Dev] updates jest setup

--- src/setup_jest.js
+++ src/setup_jest.js
@@ -1,7 +1,14 @@
-window.matchMedia = window.matchMedia || (() => {
-  return {
-    matches: false,
-    addListener: () => {},
-    removeListener: () => {},
-  }
-})
+import Enzyme from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+
+Enzyme.configure({ adapter: new Adapter() })
+
+window.matchMedia =
+  window.matchMedia ||
+  (() => {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {},
+    }
+  })
