ec9ddc87c376c412fee6234c931298cda8574622
Eve Essex <eve.essex@gmail.com>
2017-12-21 12:16:23 -0500

    add test for editCaption

--- src/Components/Publishing/Sections/ImageCollection/__test__/Image.test.tsx
+++ src/Components/Publishing/Sections/ImageCollection/__test__/Image.test.tsx
@@ -1,7 +1,9 @@
+import { mount } from "enzyme"
 import "jest-styled-components"
 import React from "react"
 import renderer from "react-test-renderer"
 import { Images } from "../../../Fixtures/Components"
+import { EditableChild } from "../../../Fixtures/Helpers"
 import { Image } from "../Image"
 
 it("renders properly", () => {
@@ -14,15 +16,18 @@ it("renders a long caption properly", () => {
   expect(image).toMatchSnapshot()
 })
 
-it("renders a react child as caption properly", () => {
+it("renders an editable caption properly", () => {
   const image = renderer
     .create(
-    <Image image={Images[2]}>
-      <div>
-        <p>A React child as caption.</p>
-      </div>
-    </Image>
+    <Image image={Images[2]} editCaption={() => EditableChild('caption')} />
     )
     .toJSON()
   expect(image).toMatchSnapshot()
+})
+
+it("Renders editable fields if present", () => {
+  const component = mount(
+    <Image image={Images[2]} editCaption={() => EditableChild('caption')} />
+  )
+  expect(component.text()).toMatch("Child caption")
 })
