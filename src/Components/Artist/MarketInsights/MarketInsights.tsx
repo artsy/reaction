c623308e7e01cc2f07f301d3c96381892a253784
Matthew Zikherman <matt@bestvendor.com>
2018-01-11 16:30:20 -0500

    Tweak subject line for market insights feedback

--- src/Components/Artist/MarketInsights/MarketInsights.tsx
+++ src/Components/Artist/MarketInsights/MarketInsights.tsx
@@ -166,7 +166,7 @@ export class MarketInsights extends React.Component<MarketInsightsProps, null> {
         <TextLink
           color={colors.graySemibold}
           underline
-          href="mailto:productfeedback@artsy.net?subject=Feedback+on+About+the+Artist+Information"
+          href="mailto:productfeedback@artsy.net?subject=Feedback+on+%22About+the+Artist%22+information"
         >
           Tell us what you think.
         </TextLink>
