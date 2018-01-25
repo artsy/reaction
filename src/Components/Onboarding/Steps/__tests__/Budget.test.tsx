36442f5aa03a04b9654a697a676a6e5f3a207cb5
kanaabe <kanakoabe5@gmail.com>
2018-01-25 12:32:29 -0500

    Adds some snapshot tests

---
+++ src/Components/Onboarding/Steps/__tests__/Budget.test.tsx
@@ -0,0 +1,21 @@
+import { mount } from "enzyme"
+import "jest-styled-components"
+import React from "react"
+import renderer from "react-test-renderer"
+import { ContextProvider } from "../../../Artsy"
+import Budget from "../Budget"
+
+describe("Budget", () => {
+  describe("snapshot", () => {
+    it("renders the budget step", () => {
+      const component = renderer
+        .create(
+          <ContextProvider>
+            <Budget onNextButtonPressed={jest.fn()} />
+          </ContextProvider>
+        )
+        .toJSON()
+      expect(component).toMatchSnapshot()
+    })
+  })
+})
