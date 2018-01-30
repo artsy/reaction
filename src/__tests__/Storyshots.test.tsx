e1c9e82ec9671e0a0cf3d26364057aa20accc97b
kanaabe <kanakoabe5@gmail.com>
2018-01-30 16:04:38 -0500

    Custom test function

--- src/__tests__/Storyshots.test.tsx
+++ src/__tests__/Storyshots.test.tsx
@@ -2,4 +2,9 @@ import initStoryshots from "@storybook/addon-storyshots"
 
 initStoryshots({
   storyNameRegex: /Gene Follow/,
+  test: ({ story, context }) => {
+    // console.log(story)
+    // console.log(context)
+    // const storyElement = story.render(context)
+  },
 })
