3d0f297581745acd4eea84dbd6e311bdcad080aa
Luc Succes <l2succes@gmail.com>
2018-02-01 15:56:12 -0500

    [Publishing] adds lock icon

--- src/Components/Publishing/index.tsx
+++ src/Components/Publishing/index.tsx
@@ -6,7 +6,7 @@ export { Caption } from "./Sections/Caption"
 export { Embed } from "./Sections/Embed"
 export { FullscreenViewer } from "./Sections/FullscreenViewer/FullscreenViewer"
 export { Header } from "./Header/Header"
-export { InstantArticleEmailSignup } from './Email/InstantArticleEmailSignup'
+export { InstantArticleEmailSignup } from "./Email/InstantArticleEmailSignup"
 export { Image } from "./Sections/Image"
 export { ImageCollection } from "./Sections/ImageCollection"
 export { ImageSetPreview } from "./Sections/ImageSetPreview"
@@ -47,6 +47,7 @@ export { IconOrderedList } from "./Icon/IconOrderedList"
 export { IconVideoPlay } from "./Icon/IconVideoPlay"
 export { IconRemove } from "./Icon/IconRemove"
 export { IconUnorderedList } from "./Icon/IconUnorderedList"
+export { IconLock } from "./icon/IconLock"
 
 // Test Fixtures
 import {
@@ -58,7 +59,7 @@ import {
   ShortStandardArticle,
   StandardArticle,
   VideoArticle,
-  VideoArticleSponsored
+  VideoArticleSponsored,
 } from "./Fixtures/Articles"
 
 // FIXME: Refactor out SizeMe; see https://github.com/ctrlplusb/react-sizeme#server-side-rendering
@@ -74,9 +75,8 @@ export const Fixtures = {
   ShortStandardArticle,
   StandardArticle,
   VideoArticle,
-  VideoArticleSponsored
+  VideoArticleSponsored,
 }
-
 
 // Constants
 import * as AllConstants from "./Constants"
