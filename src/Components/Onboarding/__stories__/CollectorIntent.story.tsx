51dd07a8377ca7088a6c06cc662bf2bbc6ce2a19
xtina-starr <c.thompsonms@gmail.com>
2018-02-13 13:56:58 -0500

    [Onboarding] allow user to click next without making a selection on collector intent step

---
+++ src/Components/Onboarding/__stories__/CollectorIntent.story.tsx
@@ -0,0 +1,13 @@
+import { storiesOf } from "@storybook/react"
+import React from "react"
+
+import { ContextProvider } from "../../Artsy"
+import CollectorIntent from "../Steps/CollectorIntent"
+
+storiesOf("Onboarding", module).add("Collector Intent", () => {
+  return (
+    <ContextProvider>
+      <CollectorIntent onNextButtonPressed={() => null} />
+    </ContextProvider>
+  )
+})
