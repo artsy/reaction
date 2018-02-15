17f93740098085d4dc34f474eff76c87f9379bc1
Yuki Nishijima <yk.nishijima@gmail.com>
2018-02-15 16:53:16 -0500

    Move the router for the new onboarding from Force to Reaction

--- src/Components/Onboarding/__stories__/Wizard.story.tsx
+++ src/Components/Onboarding/__stories__/Wizard.story.tsx
@@ -2,34 +2,14 @@ import { storiesOf } from "@storybook/react"
 import React from "react"
 
 import { ContextProvider } from "../../Artsy"
-import Artists from "../Steps/Artists"
-import Budget from "../Steps/Budget"
-import CollectorIntent from "../Steps/CollectorIntent"
-import Genes from "../Steps/Genes"
-import Wizard from "../Wizard"
+import { Wizard } from "../Wizard"
 
 storiesOf("Onboarding", module)
   .add("Wizard", () => {
     return (
       <div>
         <ContextProvider>
-          <Wizard
-            stepComponents={[CollectorIntent, Artists, Genes, Budget]}
-            redirectTo={"/"}
-          />
-        </ContextProvider>
-      </div>
-    )
-  })
-  .add("Wizard - Force Step", () => {
-    return (
-      <div>
-        <ContextProvider>
-          <Wizard
-            stepComponents={[CollectorIntent, Artists, Genes, Budget]}
-            redirectTo="/"
-            forceStep="artists"
-          />
+          <Wizard />
         </ContextProvider>
       </div>
     )
