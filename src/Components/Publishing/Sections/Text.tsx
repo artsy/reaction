0981fad53285c4146aec64948fb12fc4195d957a
Eve Essex <eve.essex@gmail.com>
2018-03-19 15:34:42 -0400

    [FEATURE publishing] Insert content-end spans to standard/feature text

--- src/Components/Publishing/Sections/Text.tsx
+++ src/Components/Publishing/Sections/Text.tsx
@@ -20,8 +20,10 @@ export const Text: React.SFC<TextProps> = props => {
     layout,
     postscript,
   } = props
+  const formattedHtml = htmlMaybeWithContentEnd(html, isContentEnd)
+
   const child = html ? (
-    <div dangerouslySetInnerHTML={{ __html: html }} />
+    <div dangerouslySetInnerHTML={{ __html: formattedHtml }} />
   ) : (
     props.children
   )
@@ -41,4 +43,22 @@ export const Text: React.SFC<TextProps> = props => {
 
 Text.defaultProps = {
   color: "black",
+}
+
+const htmlMaybeWithContentEnd = (html, isContentEnd) => {
+  const doc = document.createElement("div")
+  doc.innerHTML = html
+  // remove existing content-end spans, remove after backfill
+  const existingSpans = doc.getElementsByClassName("content-end")
+  if (existingSpans.length) {
+    existingSpans[0].outerHTML = ""
+  }
+  if (isContentEnd) {
+    const allParagraphs = doc.getElementsByTagName("P")
+    const lastParagraph =
+      allParagraphs.length && allParagraphs[allParagraphs.length - 1]
+    lastParagraph.innerHTML =
+      lastParagraph.innerHTML + "<span class='content-end'> </span>"
+  }
+  return doc.innerHTML
 }
