6c6ba4476f0105dcfa09275ca60f904e35d117f9
Eve Essex <eve.essex@gmail.com>
2018-03-22 10:36:04 -0400

    insert content-end on componentDidMount

--- src/Components/Publishing/Sections/Text.tsx
+++ src/Components/Publishing/Sections/Text.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React, { Component } from "react"
 import { ArticleLayout } from "../Typings"
 import { StyledText } from "./StyledText"
 
-interface TextProps extends React.HTMLProps<HTMLDivElement> {
+interface Props extends React.HTMLProps<HTMLDivElement> {
   color?: string
   html?: string
   isContentEnd?: boolean
@@ -11,54 +11,66 @@ interface TextProps extends React.HTMLProps<HTMLDivElement> {
   postscript?: boolean
 }
 
-export const Text: React.SFC<TextProps> = props => {
-  const {
-    color,
-    html,
-    isContentEnd,
-    isContentStart,
-    layout,
-    postscript,
-  } = props
-  const formattedHtml = htmlMaybeWithContentEnd(html, isContentEnd)
-
-  const child = html ? (
-    <div dangerouslySetInnerHTML={{ __html: formattedHtml }} />
-  ) : (
-    props.children
-  )
-  return (
-    <StyledText
-      className="article__text-section"
-      color={color}
-      isContentStart={isContentStart}
-      layout={layout}
-      postscript={postscript}
-    >
-      {child}
-    </StyledText>
-  )
+interface State {
+  html: string
 }
 
-Text.defaultProps = {
-  color: "black",
-}
-
-const htmlMaybeWithContentEnd = (html = "", isContentEnd) => {
-  // Remove existing spans - TODO: Backfill out of articles
-  const cleanedHtml = html.replace("<span class='content-end'> </span>", "")
-
-  if (isContentEnd) {
-    const doc = document.createElement("div")
-    doc.innerHTML = html
-    const allParagraphs = doc.getElementsByTagName("P")
-    // insert content-end in last paragraph
-    const lastParagraph =
-      allParagraphs.length && allParagraphs[allParagraphs.length - 1]
-    lastParagraph.innerHTML =
-      lastParagraph.innerHTML + "<span class='content-end'> </span>"
-
-    return doc.innerHTML
+export class Text extends Component<Props, State> {
+  static defaultProps = {
+    color: "black",
   }
-  return cleanedHtml
+
+  state = {
+    html: this.props.html || "",
+  }
+
+  componentDidMount() {
+    const html = this.htmlMaybeWithContentEnd()
+
+    this.setState({ html })
+  }
+
+  htmlMaybeWithContentEnd = () => {
+    const { html } = this.state
+    const { isContentEnd } = this.props
+    // Remove existing spans - TODO: Backfill out of articles
+    const cleanedHtml = html.replace("<span class='content-end'> </span>", "")
+
+    if (isContentEnd) {
+      const doc = document.createElement("div")
+      doc.innerHTML = cleanedHtml
+
+      const paragraphs = doc.getElementsByTagName("P")
+      const lastParagraph =
+        paragraphs.length && paragraphs[paragraphs.length - 1]
+
+      // insert content-end in last paragraph
+      lastParagraph.innerHTML =
+        lastParagraph.innerHTML + "<span class='content-end'> </span>"
+
+      return doc.innerHTML
+    }
+    return cleanedHtml
+  }
+
+  render() {
+    const { children, color, isContentStart, layout, postscript } = this.props
+    const { html } = this.state
+
+    return (
+      <StyledText
+        className="article__text-section"
+        color={color}
+        isContentStart={isContentStart}
+        layout={layout}
+        postscript={postscript}
+      >
+        {html.length ? (
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        ) : (
+          children
+        )}
+      </StyledText>
+    )
+  }
 }
