df4fcee47309c82ad51f61a3d198b57d0796f997
kanaabe <kanakoabe5@gmail.com>
2018-04-12 14:33:32 -0400

    Add test

--- src/Components/Publishing/Layouts/__tests__/FeatureLayout.test.tsx
+++ src/Components/Publishing/Layouts/__tests__/FeatureLayout.test.tsx
@@ -1,10 +1,11 @@
 import { mount } from "enzyme"
 import "jest-styled-components"
 import React from "react"
-import { FeatureArticle } from "../../Fixtures/Articles"
+import { FeatureArticle, SeriesArticle } from "../../Fixtures/Articles"
 import { RelatedCanvas } from "../../Fixtures/Components"
 import { RelatedArticlesCanvas } from "../../RelatedArticles/RelatedArticlesCanvas"
 import { FeatureLayout } from "../FeatureLayout"
+import { Nav } from "../../Nav/Nav"
 
 jest.mock("react-sizeme", () => jest.fn(c => d => d))
 jest.mock("../../Sections/FullscreenViewer/withFullScreen", () => ({
@@ -30,4 +31,15 @@ it("Does not render RelatedArticlesCanvas if isSuper", () => {
     />
   )
   expect(article.find(RelatedArticlesCanvas).length).toBe(0)
+})
+
+it("renders a nav if article is in a series", () => {
+  const article = mount(
+    <FeatureLayout
+      article={FeatureArticle}
+      relatedArticlesForCanvas={RelatedCanvas}
+      seriesArticle={SeriesArticle}
+    />
+  )
+  expect(article.find(Nav).length).toBe(1)
 })
