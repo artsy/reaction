06f2f7d0539e6b4dfd105d7be2a77162a2f3ae5f
Eve Essex <eve.essex@gmail.com>
2018-04-18 16:07:24 -0400

    move related articles/series footer to component

--- src/Components/Publishing/Layouts/VideoLayout.tsx
+++ src/Components/Publishing/Layouts/VideoLayout.tsx
@@ -1,19 +1,16 @@
 import React, { Component } from "react"
-import { Col } from 'react-styled-flexboxgrid'
 import styled from "styled-components"
 import Events from "../../../Utils/Events"
 import { track } from "../../../Utils/track"
 import { media } from "../../Helpers"
 import { getEditorialHref } from "../Constants"
-import { Fonts } from "../Fonts"
 import { Nav } from "../Nav/Nav"
-import { ArticleCard, ArticleCardContainer } from "../Series/ArticleCard"
-import { SeriesAbout, SeriesAboutContainer } from "../Series/SeriesAbout"
 import { ArticleData } from "../Typings"
 import { VideoContainer, VideoPlayer } from "../Video/Player/VideoPlayer"
 import { MaxRow } from "../Video/Shared"
 import { VideoAbout, VideoAboutContainer } from "../Video/VideoAbout"
 import { VideoCover } from "../Video/VideoCover"
+import { RelatedArticleCardFooter } from "../RelatedArticles/RelatedArticleCardFooter"
 
 interface Props {
   article: ArticleData
@@ -26,39 +23,41 @@ interface State {
   hideCover: boolean
 }
 
-@track((props) => {
-  return {
-    page: "Article",
-    entity_type: "article",
-    entity_id: props.article.id
-  }
-}, {
-    dispatch: data => Events.postEvent(data)
+@track(
+  props => {
+    return {
+      page: "Article",
+      entity_type: "article",
+      entity_id: props.article.id,
+    }
+  },
+  {
+    dispatch: data => Events.postEvent(data),
   }
 )
 export class VideoLayout extends Component<Props, State> {
   state = {
     isPlaying: false,
-    hideCover: false
+    hideCover: false,
   }
 
   playVideo = () => {
     this.setState({
       isPlaying: true,
-      hideCover: true
+      hideCover: true,
     })
   }
 
-  onPlayToggle = (isPlaying) => {
+  onPlayToggle = isPlaying => {
     if (!isPlaying) {
       this.setState({
-        isPlaying
+        isPlaying,
       })
       setTimeout(this.setHideCover.bind(this), 30000)
     } else {
       this.setState({
         isPlaying,
-        hideCover: true
+        hideCover: true,
       })
     }
   }
@@ -66,27 +65,20 @@ export class VideoLayout extends Component<Props, State> {
   setHideCover = () => {
     if (!this.state.isPlaying) {
       this.setState({
-        hideCover: false
+        hideCover: false,
       })
     }
   }
 
   render() {
-    const {
-      article,
-      seriesArticle,
-      relatedArticles
-    } = this.props
-    const { media } = article
+    const { article, relatedArticles } = this.props
+    const { media, seriesArticle } = article
     const sponsor = seriesArticle ? seriesArticle.sponsor : article.sponsor
-    const seriesLink = seriesArticle && getEditorialHref('series', seriesArticle.slug)
+    const seriesLink =
+      seriesArticle && getEditorialHref("series", seriesArticle.slug)
     return (
       <VideoLayoutContainer>
-        <Nav
-          transparent
-          sponsor={sponsor}
-          canFix={false}
-        />
+        <Nav transparent sponsor={sponsor} canFix={false} />
         <VideoPlayerContainer>
           <VideoPlayer
             url={media.url}
@@ -104,57 +96,15 @@ export class VideoLayout extends Component<Props, State> {
           />
         </VideoPlayerContainer>
         <MaxRow>
-          <VideoAbout
-            article={article}
-            color="white"
-          />
+          <VideoAbout article={article} color="white" />
         </MaxRow>
-        {relatedArticles &&
-          <MaxRow>
-            <RelatedArticlesTitle>
-              {"More in "}
-              {seriesArticle ?
-                <Link href={seriesLink}>
-                  {seriesArticle.title}
-                </Link>
-                :
-                <span>{article.vertical && article.vertical.name}</span>
-              }
-            </RelatedArticlesTitle>
-          </MaxRow>
-        }
-        {relatedArticles &&
-          relatedArticles.map((relatedArticle, i) => {
-            return (
-              <MaxRow key={i}>
-                <Col xs={12}>
-                  <ArticleCard
-                    article={relatedArticle}
-                    color="white"
-                    series={seriesArticle}
-                  />
-                </Col>
-              </MaxRow>
-            )
-          })
-        }
-        {seriesArticle &&
-          <MaxRow>
-            <SeriesAbout
-              article={seriesArticle}
-              color="white"
-            />
-          </MaxRow>
-        }
+        {(relatedArticles || seriesArticle) && (
+          <RelatedArticleCardFooter {...this.props} color="white" />
+        )}
       </VideoLayoutContainer>
     )
   }
 }
-
-const RelatedArticlesTitle = styled(Col) `
-  ${Fonts.unica("s32")}
-  width: 100%;
-`
 
 const VideoLayoutContainer = styled.div`
   background: black;
@@ -166,26 +116,15 @@ const VideoLayoutContainer = styled.div`
     top: 0;
   }
 
-  ${ArticleCardContainer} {
-    margin-bottom: 60px;
-  }
-
-  ${RelatedArticlesTitle} {
-    margin-bottom: 40px;
-  }
-
-  ${SeriesAboutContainer}, ${VideoAboutContainer} {
+  ${VideoAboutContainer} {
     margin: 60px 0 100px 0;
   }
 
   ${media.sm`
-    ${ArticleCardContainer} {
-      margin-bottom: 30px;
-    }
-    ${SeriesAboutContainer}, ${VideoAboutContainer} {
+    ${VideoAboutContainer} {
       margin: 40px 0 100px 0;
     }
-  `}
+  `};
 `
 
 const VideoPlayerContainer = styled.div`
@@ -197,14 +136,4 @@ const VideoPlayerContainer = styled.div`
     position: absolute;
     top: 0;
   }
-`
-
-const Link = styled.a`
-  text-decoration: none;
-  color: white;
-  border-bottom: 2px solid;
-
-  ${media.sm`
-    display: block;
-  `}
 `
