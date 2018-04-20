fc89cab9a7a400b2e7476db6f7d0cf4506c02589
kanaabe <kanakoabe5@gmail.com>
2018-04-20 12:49:42 -0400

    [FIX news] Adds back read more

--- src/Components/Publishing/Layouts/__tests__/NewsLayout.test.js
+++ src/Components/Publishing/Layouts/__tests__/NewsLayout.test.js
@@ -69,4 +69,15 @@ describe("News Layout", () => {
 
     expect(component).toMatchSnapshot()
   })
+
+  describe("Analytics", () => {
+    it("tracks the expand button", () => {
+      const component = mount(<NewsLayout article={NewsArticle} isTruncated />)
+      expect(track.mock.calls[5][0]).toEqual(
+        expect.objectContaining({
+          action: "Clicked read more",
+        })
+      )
+    })
+  })
 })
