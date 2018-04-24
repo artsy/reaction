3cfd9f44d602a90b5f0e9aee6c3187ad91910e7d
Luc Succes <l2succes@gmail.com>
2018-04-24 08:42:21 -0400

    [BREAKING] Fix import

--- src/Components/Publishing/Video/Player/VideoControls.tsx
+++ src/Components/Publishing/Video/Player/VideoControls.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import styled from "styled-components"
-import { garamond } from "Assets/Fonts.tsx"
+import { garamond } from "Assets/Fonts"
 import { IconVideoFullscreen } from "../../Icon/IconVideoFullscreen"
 import { MuteUnmute, MuteUnmuteContainer } from "./MuteUnmute"
 import { PlayPause } from "./PlayPause"
