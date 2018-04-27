92a90d8f8337819d0049c865070962b76550b310
eriks <erik@artsymail.com>
2018-04-27 15:10:12 -0400

    cleanup

--- src/Components/Forms/OrderForm/index.tsx
+++ src/Components/Forms/OrderForm/index.tsx
@@ -1,17 +1,6 @@
 import React, { SFC } from "react"
 import { App } from "./App"
-// import { BrowserRouter } from "react-router-dom"
-// import { StaticRouter } from "react-router"
 
 export const OrderForm: SFC<{ onSubmit: any }> = ({ onSubmit }) => {
-  // const isClient = typeof window !== "undefined"
-  // const Router = isClient ? BrowserRouter : StaticRouter // TODO: Swap BrowserRouter with MemoryRouter to hide URLBar state
-  // const basename = isClient && "/order2"
-
-  // return (
-  //   <Router basename={basename}>
-  //     <App />
-  //   </Router>
-  // )
   return <App onSubmit={onSubmit} />
 }
