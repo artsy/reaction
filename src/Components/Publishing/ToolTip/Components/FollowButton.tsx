51c777c20790fcad8ddd0d450105ab0b6198564d
Eve Essex <eve.essex@gmail.com>
2018-05-03 14:01:44 -0400

    follow button has follwing/unfollow states

--- src/Components/Publishing/ToolTip/Components/FollowButton.tsx
+++ src/Components/Publishing/ToolTip/Components/FollowButton.tsx
@@ -1,13 +1,55 @@
-import styled from "styled-components"
+import styled, { StyledFunction } from "styled-components"
 import React from "react"
 import { unica } from "Assets/Fonts"
 import Colors from "Assets/Colors"
 
-export const FollowButton: React.SFC = () => {
-  return <FollowButtonContainer>Follow</FollowButtonContainer>
+interface Props {
+  item?: {
+    __id: string
+    id: string
+    is_followed: boolean | null
+  }
+  currentUser?: object
 }
 
-const FollowButtonContainer = styled.div`
+interface State {
+  showUnfollow: boolean
+}
+
+export class FollowButton extends React.Component<Props, State> {
+  state = {
+    showUnfollow: false,
+  }
+
+  render() {
+    const { showUnfollow } = this.state
+    const isFollowed = this.props.item ? this.props.item.is_followed : true
+    const text = isFollowed
+      ? showUnfollow
+        ? "Unfollow"
+        : "Following"
+      : "Follow"
+
+    return (
+      <FollowButtonContainer
+        isFollowed={isFollowed}
+        onMouseEnter={() => this.setState({ showUnfollow: true })}
+        onMouseLeave={() => this.setState({ showUnfollow: false })}
+      >
+        {text}
+      </FollowButtonContainer>
+    )
+  }
+}
+
+interface DivProps {
+  isFollowed: boolean
+}
+
+const Div: StyledFunction<DivProps & React.HTMLProps<HTMLDivElement>> =
+  styled.div
+
+const FollowButtonContainer = Div`
   border: 1px solid ${Colors.grayRegular};
   ${unica("s12", "medium")};
   width: 80px;
@@ -15,9 +57,13 @@ const FollowButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  color: ${props => (props.isFollowed ? Colors.grayMedium : "black")}
   &:hover {
-    border-color: black;
-    background: black;
-    color: white;
+    ${props =>
+      !props.isFollowed &&
+      `
+      border-color: black;`}
+    background: ${props => (props.isFollowed ? "white" : "black")}
+    color: ${props => (props.isFollowed ? Colors.redMedium : "white")}
   }
 `
