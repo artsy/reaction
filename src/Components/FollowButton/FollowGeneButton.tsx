42d10c04e75306e08f8fe14e5c36106151e7597e
Eve Essex <eve.essex@gmail.com>
2018-05-24 16:03:06 -0400

    add tracking data to onOpenAuthModal

--- src/Components/FollowButton/FollowGeneButton.tsx
+++ src/Components/FollowButton/FollowGeneButton.tsx
@@ -62,7 +62,11 @@ export class FollowGeneButton extends React.Component<Props> {
       })
       this.trackFollow()
     } else {
-      onOpenAuthModal && onOpenAuthModal("register")
+      onOpenAuthModal &&
+        onOpenAuthModal("register", {
+          context_module: "intext tooltip",
+          intent: "follow gene",
+        })
     }
   }
 
