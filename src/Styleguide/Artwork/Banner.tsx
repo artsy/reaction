72b8f87f2bef9ed5c4973b64abf589d99e6d6135
Zephraph <zephraph@gmail.com>
2018-06-07 15:12:26 -0400

    Banner updates

--- src/Styleguide/Artwork/Banner.tsx
+++ src/Styleguide/Artwork/Banner.tsx
@@ -1,22 +1,30 @@
 import React from "react"
 import styled from "styled-components"
+import { flexDirection } from "styled-system"
 import { Avatar } from "../Elements/Avatar"
+import { Sans } from "../Elements/Typography"
 
-const Flex = styled.div`
+interface FlexProps {
+  flexDirection: "row" | "column"
+}
+
+const Flex = styled.div.attrs<FlexProps>({})`
   display: flex;
-  flex-direction: row;
+  ${flexDirection};
 `
 
 export class Banner extends React.Component {
   render() {
     return (
-      <Flex>
+      <Flex flexDirection="row">
         <Avatar size="110px" src="https://picsum.photos/110/110/?random" />
-        <div>
-          <div>In show</div>
+        <Flex flexDirection="column">
+          <Sans weight="medium" size="2">
+            In show
+          </Sans>
           <div>Francesca DiMattio: Boucherouite</div>
           <div>Salon 94</div>
-        </div>
+        </Flex>
       </Flex>
     )
   }
