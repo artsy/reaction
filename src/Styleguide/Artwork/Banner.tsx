f33eaa8c75a24ef8729e3d758f9a9eba2bf87764
Zephraph <zephraph@gmail.com>
2018-06-08 12:19:57 -0400

    Add tentive props to banner

--- src/Styleguide/Artwork/Banner.tsx
+++ src/Styleguide/Artwork/Banner.tsx
@@ -3,14 +3,21 @@ import { Avatar } from "../Elements/Avatar"
 import { Sans, Serif } from "../Elements/Typography"
 import { Flex } from "../Elements/Flex"
 
-export class Banner extends React.Component {
+interface BannerProps {
+  src: string
+  badge: string
+  headline: string
+  subHeadline: string
+}
+
+export class Banner extends React.Component<BannerProps> {
   render() {
     return (
       <Flex flexDirection="row">
         <Avatar size="110px" src="https://picsum.photos/110/110/?random" />
         <Flex flexDirection="column" justifyContent="center" ml={4}>
           <Sans weight="medium" size="2">
-            In show
+            {this.props.badge}
           </Sans>
           <Serif size="4t">Francesca DiMattio: Boucherouite</Serif>
           <Serif size="4t" color="black60">
--- src/Styleguide/Artwork/__stories__/Banner.story.tsx
+++ src/Styleguide/Artwork/__stories__/Banner.story.tsx
@@ -6,7 +6,12 @@ import { Theme } from "../../theme"
 storiesOf("Styleguide/Artwork", module).add("Banner", () => {
   return (
     <Theme>
-      <Banner />
+      <Banner
+        src="https://picsum.photos/110/110/?random"
+        badge="In show"
+        headline="Francesca DiMattio: Boucherouite"
+        subHeadline="Salon 94"
+      />
     </Theme>
   )
 })
