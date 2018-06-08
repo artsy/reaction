42f4ac1b6b3001cc5c964752ebf4b275a68ad513
Zephraph <zephraph@gmail.com>
2018-06-08 13:24:20 -0400

    Remove theme wrapper, wire props

--- src/Styleguide/Artwork/Banner.tsx
+++ src/Styleguide/Artwork/Banner.tsx
@@ -3,7 +3,7 @@ import { Avatar } from "../Elements/Avatar"
 import { Sans, Serif } from "@artsy/palette"
 import { Flex } from "../Elements/Flex"
 
-interface BannerProps {
+export interface BannerProps {
   src: string
   badge: string
   headline: string
@@ -14,14 +14,14 @@ export class Banner extends React.Component<BannerProps> {
   render() {
     return (
       <Flex flexDirection="row">
-        <Avatar size="110px" src="https://picsum.photos/110/110/?random" />
+        <Avatar size="110px" src={this.props.src} />
         <Flex flexDirection="column" justifyContent="center" ml={4}>
           <Sans weight="medium" size="2">
             {this.props.badge}
           </Sans>
-          <Serif size="4t">Francesca DiMattio: Boucherouite</Serif>
+          <Serif size="4t">{this.props.headline}</Serif>
           <Serif size="4t" color="black60">
-            Salon 94
+            {this.props.subHeadline}
           </Serif>
         </Flex>
       </Flex>
--- src/Styleguide/Artwork/__stories__/Banner.story.tsx
+++ src/Styleguide/Artwork/__stories__/Banner.story.tsx
@@ -4,13 +4,11 @@ import { Banner } from "../Banner"
 
 storiesOf("Styleguide/Artwork", module).add("Banner", () => {
   return (
-    <Theme>
-      <Banner
-        src="https://picsum.photos/110/110/?random"
-        badge="In show"
-        headline="Francesca DiMattio: Boucherouite"
-        subHeadline="Salon 94"
-      />
-    </Theme>
+    <Banner
+      src="https://picsum.photos/110/110/?random"
+      badge="In show"
+      headline="Francesca DiMattio: Boucherouite"
+      subHeadline="Salon 94"
+    />
   )
 })
