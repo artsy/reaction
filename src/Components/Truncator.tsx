29045beba0f8ccde063ad3e650375a03ad1324a3
Christopher Pappas <damassi.pappas@gmail.com>
2018-06-08 23:16:41 -0700

    Add new <ReadMore /> component

--- src/Components/Truncator.tsx
+++ src/Components/Truncator.tsx
@@ -4,16 +4,29 @@ import { ErrorBoundary } from "Components/ErrorBoundary"
 
 interface Props {
   maxLineCount?: number
+  ellipsis?: string
+  ReadMoreLink?: () => any
 }
 
-export const Truncator: React.SFC<Props> = ({ children, maxLineCount }) => {
+export const Truncator: React.SFC<Props> = ({
+  ReadMoreLink,
+  children,
+  ellipsis,
+  maxLineCount,
+}) => {
   const html = ReactDOM.renderToStaticMarkup(<span>{children}</span>)
+  let readMoreHTML = null
+
+  if (ReadMoreLink) {
+    readMoreHTML = ReactDOM.renderToStaticMarkup(ReadMoreLink())
+  }
 
   // FIXME: Make safe for tests
   let HTMLEllipsis
 
   if (process.env.NODE_ENV !== "test") {
-    HTMLEllipsis = require("react-lines-ellipsis/lib/html")
+    const responsiveHOC = require("react-lines-ellipsis/lib/responsiveHOC")
+    HTMLEllipsis = responsiveHOC()(require("react-lines-ellipsis/lib/html"))
   } else {
     HTMLEllipsis = ({ unsafeHTML }) => (
       <div
@@ -30,7 +43,8 @@ export const Truncator: React.SFC<Props> = ({ children, maxLineCount }) => {
         unsafeHTML={html}
         trimRight={false}
         maxLine={maxLineCount || 2}
-        ellipsis="..."
+        ellipsis={ellipsis}
+        ellipsisHTML={readMoreHTML}
       />
     </ErrorBoundary>
   )
