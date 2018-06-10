af6e31f28abc496e551f9d818128e5236a62f1dd
Zephraph <zephraph@gmail.com>
2018-06-10 01:59:06 -0400

    Banner, gallery responsive; nicer stories

--- src/Styleguide/Pages/Artwork/__stories__/Gallery.story.tsx
+++ src/Styleguide/Pages/Artwork/__stories__/Gallery.story.tsx
@@ -1,13 +1,32 @@
 import React from "react"
 import { storiesOf } from "storybook/storiesOf"
-import { Gallery } from "../Gallery"
+import { Gallery, SmallGallery, LargeGallery } from "../Gallery"
+import { Section } from "../../../Utils/Section"
 
 storiesOf("Styleguide/Artwork", module).add("Gallery", () => {
   return (
-    <Gallery
-      src="https://picsum.photos/110/110/?random"
-      headline="Salon 94"
-      subHeadline="New York, London, Beijing, Hong Kong"
-    />
+    <React.Fragment>
+      <Section title="Responsive">
+        <Gallery
+          src="https://picsum.photos/110/110/?random"
+          headline="Salon 94"
+          subHeadline="New York, London, Beijing, Hong Kong"
+        />
+      </Section>
+      <Section title="Large Gallery">
+        <LargeGallery
+          src="https://picsum.photos/110/110/?random"
+          headline="Salon 94"
+          subHeadline="New York, London, Beijing, Hong Kong"
+        />
+      </Section>
+      <Section title="Small Gallery">
+        <SmallGallery
+          src="https://picsum.photos/110/110/?random"
+          headline="Salon 94"
+          subHeadline="New York, London, Beijing, Hong Kong"
+        />
+      </Section>
+    </React.Fragment>
   )
 })
