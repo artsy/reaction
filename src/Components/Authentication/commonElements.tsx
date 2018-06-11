11490b644625b02acaac0d9c921d5e6514515215
Luc Succes <l2succes@gmail.com>
2018-06-11 17:15:36 -0400

    [Auth] display desktop global errors

--- src/Components/Authentication/commonElements.tsx
+++ src/Components/Authentication/commonElements.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 
 import Colors from "Assets/Colors"
+import { growAndFadeIn } from "Assets/Animations"
 import { garamond, unica } from "Assets/Fonts"
 import Checkbox from "../Checkbox"
 
@@ -21,7 +22,7 @@ export const TOSCheckbox = ({ error, name, onChange, value, ...props }) => (
 )
 
 const StyledCheckbox = styled(Checkbox)`
-  margin-bottom: 5px;
+  margin: 20px 0;
 `
 
 export const SmallTextLink = styled.a`
@@ -32,7 +33,7 @@ export const SmallTextLink = styled.a`
 `
 
 export const SmallText = styled.span`
-  margin: 0px;
+  margin: 0;
   color: ${Colors.black30};
   ${unica("s12")};
 `
@@ -46,4 +47,15 @@ export const MobileHeader = styled.div`
   margin: 20px 0 0;
   ${garamond("s23")};
   font-weight: bold;
+`
+
+export const Error = styled.div.attrs<{ show: boolean }>({})`
+  ${unica("s12")};
+  margin-top: ${p => (p.show ? "10px" : "0")};
+  margin-bottom: 10px;
+  color: ${Colors.redMedium};
+  visibility: ${p => (p.show ? "visible" : "hidden")};
+  transition: visibility 0.2s linear;
+  animation: ${p => p.show && growAndFadeIn("16px")} 0.25s linear;
+  height: ${p => (p.show ? "16px" : "0")};
 `
