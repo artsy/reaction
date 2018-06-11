b138c8da22c8a073e0ac399a27f0240a7d08fdff
Christopher Pappas <damassi.pappas@gmail.com>
2018-06-11 12:15:01 -0700

    [PATCH tsconfig] Add declaration to config

--- src/Styleguide/Elements/Card.tsx
+++ src/Styleguide/Elements/Card.tsx
@@ -2,6 +2,11 @@ import styled from "styled-components"
 import { Flex } from "./Flex"
 import { themeGet } from "styled-system"
 
+// @ts-ignore
+import { ClassAttributes, HTMLAttributes } from "react"
+// @ts-ignore
+import { FlexProps } from "./Flex"
+
 export const Card = styled(Flex)`
   border: 1px solid ${themeGet("colors.black10")};
   border-radius: 2px;
