fca355f980a6545a66b2ee1486cf54dfcebcf310
Eloy Durán <eloy.de.enige@gmail.com>
2018-06-12 12:49:11 +0200

    [Artwork/ImageCarousel] Get aspect ratio and filling right.

--- src/Styleguide/Artwork/ImageCarousel.tsx
+++ src/Styleguide/Artwork/ImageCarousel.tsx
@@ -4,22 +4,61 @@ import styled from "styled-components"
 import { Flex } from "Styleguide/Elements/Flex"
 import Icon from "Components/Icon"
 
-const ImageContainer = styled.a`
-  cursor: zoom-in;
+/**
+ * The image should be placed 100px from the top of the document and leave 60px
+ * for the buttons below the image.
+ */
+const ButtonsContainerHeight = 60
+const ImageContainerViewportMargin = 100 + ButtonsContainerHeight
+
+interface ImageCarouselProps {
+  src: string
+}
+
+const Container = styled(Flex)`
+  background-color: #0fdb82;
 `
 
-export class ImageCarousel extends React.Component {
+const ZoomHitTarget = styled.a`
+  cursor: zoom-in;
+  width: 100%;
+  min-height: 450px;
+  height: calc(100vh - ${ImageContainerViewportMargin}px);
+  line-height: 0; /* Don’t introduce visual margins */
+`
+
+const ImageContainer = styled(Flex)`
+  height: 100%;
+  width: 100%;
+`
+
+const Image = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+`
+
+const ButtonsContainer = styled(Flex)`
+  background-color: #f1af1b;
+`
+
+export class ImageCarousel extends React.Component<ImageCarouselProps> {
   render() {
     return (
-      <Flex flexDirection="column">
-        <ImageContainer>
-          <img src="https://picsum.photos/622/755/?random" />
-        </ImageContainer>
-        <Flex
+      <Container flexDirection="column">
+        <ZoomHitTarget>
+          <ImageContainer
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Image src={this.props.src} />
+          </ImageContainer>
+        </ZoomHitTarget>
+        <ButtonsContainer
           flexDirection="row"
           alignItems="center"
           justifyContent="center"
-          flexBasis="60px"
+          flexBasis={`${ButtonsContainerHeight}px`}
         >
           <div>
             <a href="#TODO">
@@ -29,8 +68,8 @@ export class ImageCarousel extends React.Component {
               <Icon name="share" color="black" />
             </a>
           </div>
-        </Flex>
-      </Flex>
+        </ButtonsContainer>
+      </Container>
     )
   }
 }
--- src/Styleguide/Artwork/__stories__/ImageCarousel.story.tsx
+++ src/Styleguide/Artwork/__stories__/ImageCarousel.story.tsx
@@ -2,15 +2,43 @@ import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { ImageCarousel } from "../ImageCarousel"
 import { Col } from "Styleguide/Elements/Grid"
+import { Section } from "../../Utils/Section"
 
-storiesOf("Styleguide/Artwork/ImageCarousel", module)
-  .add("With a single image", () => {
-    return (
-      <Col sm="8">
-        <ImageCarousel />
-      </Col>
-    )
-  })
-  .add("With multiple images", () => {
-    return <ImageCarousel />
-  })
+storiesOf("Styleguide/Artwork", module).add("ImageCarousel", () => {
+  return (
+    <React.Fragment>
+      <Section title="With a short portrait image">
+        <Col sm="8">
+          <ImageCarousel src="https://picsum.photos/300/400/?random" />
+        </Col>
+      </Section>
+      <Section title="With a tall portrait image">
+        <Col sm="8">
+          <ImageCarousel src="https://picsum.photos/400/2000/?random" />
+        </Col>
+      </Section>
+      <Section title="With a narrow landscape image">
+        <Col sm="8">
+          <ImageCarousel src="https://picsum.photos/500/400/?random" />
+        </Col>
+      </Section>
+      <Section title="With a wide landscape image">
+        <Col sm="8">
+          <ImageCarousel src="https://picsum.photos/2000/400/?random" />
+        </Col>
+      </Section>
+    </React.Fragment>
+  )
+})
+
+// storiesOf("Styleguide/Artwork", module).add("ImageCarousel", () => {
+//   return (
+//     <React.Fragment>
+//       <Section title="With a tall portrait image">
+//         <Col sm="8">
+//           <ImageCarousel src="https://picsum.photos/400/2000/?random" />
+//         </Col>
+//       </Section>
+//     </React.Fragment>
+//   )
+// })
