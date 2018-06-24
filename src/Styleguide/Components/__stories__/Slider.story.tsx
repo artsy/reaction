c7e7f1eb5c3d71a66b33fa2db01b4c8256f226af
Christopher Pappas <damassi.pappas@gmail.com>
2018-06-24 12:47:15 -0700

    Move Slider images to fixtures

--- src/Styleguide/Components/__stories__/Slider.story.tsx
+++ src/Styleguide/Components/__stories__/Slider.story.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { LargeSlider, Slider, SmallSlider } from "Styleguide/Components/Slider"
 import { Box } from "Styleguide/Elements/Box"
+import { images } from "Styleguide/Pages/Fixtures/Slider"
 import { Section } from "Styleguide/Utils/Section"
 
 storiesOf("Styleguide/Components", module).add("Slider", () => {
@@ -25,70 +26,3 @@ storiesOf("Styleguide/Components", module).add("Slider", () => {
     </React.Fragment>
   )
 })
-
-const images = [
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=225&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FzPgGQ802qxkHR85T7GAEAw%2Flarge.jpg",
-      width: 225,
-      height: 300,
-    },
-  },
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=208&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FRgh7CxrtmPgLgKz2fC8g3Q%2Flarge.jpg",
-      width: 208,
-      height: 300,
-    },
-  },
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=195&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FuKQRWPcxmMU_1C7iYBnVcw%2Flarge.jpg",
-      width: 195,
-      height: 300,
-    },
-  },
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=661&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fxv2Kv6g8nocq9xATLvS3Vw%2Flarge.jpg",
-      width: 661,
-      height: 300,
-    },
-  },
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=382&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FiwKcC80o_Ef-dH1Xh0k0Ug%2Flarge.jpg",
-      width: 382,
-      height: 300,
-    },
-  },
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=217&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FrC3xdKws0h2ZlR_fmAK-dQ%2Flarge.jpg",
-      width: 217,
-      height: 300,
-    },
-  },
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=244&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2F2pn4iPeFtUwJpOSM1EIJWA%2Flarge.jpg",
-      width: 244,
-      height: 300,
-    },
-  },
-  {
-    resized: {
-      url:
-        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=249&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FTDHuYvPI_VMX9jLIh-XxmQ%2Flarge.jpg",
-      width: 249,
-      height: 300,
-    },
-  },
-]
---
+++ src/Styleguide/Pages/Fixtures/Slider.ts
@@ -0,0 +1,66 @@
+export const images = [
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=225&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FzPgGQ802qxkHR85T7GAEAw%2Flarge.jpg",
+      width: 225,
+      height: 300,
+    },
+  },
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=208&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FRgh7CxrtmPgLgKz2fC8g3Q%2Flarge.jpg",
+      width: 208,
+      height: 300,
+    },
+  },
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=195&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FuKQRWPcxmMU_1C7iYBnVcw%2Flarge.jpg",
+      width: 195,
+      height: 300,
+    },
+  },
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=661&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fxv2Kv6g8nocq9xATLvS3Vw%2Flarge.jpg",
+      width: 661,
+      height: 300,
+    },
+  },
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=382&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FiwKcC80o_Ef-dH1Xh0k0Ug%2Flarge.jpg",
+      width: 382,
+      height: 300,
+    },
+  },
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=217&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FrC3xdKws0h2ZlR_fmAK-dQ%2Flarge.jpg",
+      width: 217,
+      height: 300,
+    },
+  },
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=244&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2F2pn4iPeFtUwJpOSM1EIJWA%2Flarge.jpg",
+      width: 244,
+      height: 300,
+    },
+  },
+  {
+    resized: {
+      url:
+        "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=249&height=300&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FTDHuYvPI_VMX9jLIh-XxmQ%2Flarge.jpg",
+      width: 249,
+      height: 300,
+    },
+  },
+]
