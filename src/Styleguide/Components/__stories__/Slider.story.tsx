3d7c057d95f3269a810da4c44d19fb86420070ae
Christopher Pappas <damassi.pappas@gmail.com>
2018-06-26 15:04:36 -0700

    Add artwork brick example

--- src/Styleguide/Components/__stories__/Slider.story.tsx
+++ src/Styleguide/Components/__stories__/Slider.story.tsx
@@ -1,25 +1,34 @@
+import { FillwidthItem } from "Components/Artwork/FillwidthItem"
 import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { LargeSlider, Slider, SmallSlider } from "Styleguide/Components/Slider"
 import { Box } from "Styleguide/Elements/Box"
 import { Image } from "Styleguide/Elements/Image"
-import { images } from "Styleguide/Pages/Fixtures/Slider"
+import { artworkBricks, images } from "Styleguide/Pages/Fixtures/Slider"
 import { Section } from "Styleguide/Utils/Section"
 
 storiesOf("Styleguide/Components", module).add("Slider", () => {
   return (
     <React.Fragment>
-      <Section title="Custom Slide">
+      <Section title="Artwork Brick">
         <Box width="70%">
           <Slider
-            data={images}
-            render={props => {
+            data={artworkBricks}
+            render={artwork => {
+              const {
+                node: {
+                  image: { aspect_ratio },
+                },
+              } = artwork
+
               return (
-                <Image
-                  px={5}
-                  src={props.resized.url}
-                  width={props.resized.width}
-                  height={props.resized.height}
+                <FillwidthItem
+                  artwork={artwork.node}
+                  targetHeight={200}
+                  imageHeight={200}
+                  width={200 * aspect_ratio}
+                  margin={20}
+                  useRelay={false}
                 />
               )
             }}
--- src/Styleguide/Pages/Fixtures/Slider.ts
+++ src/Styleguide/Pages/Fixtures/Slider.ts
@@ -64,3 +64,369 @@ export const images = [
     },
   },
 ]
+
+export const artworkBricks = [
+  {
+    node: {
+      __id:
+        "QXJ0d29yazpwYWJsby1waWNhc3NvLXBvcnRyYWl0LW9mLWFydC1kZWFsZXItYW1icm9pc2Utdm9sbGFyZC0xODY3LTE5Mzk=",
+      image: {
+        aspect_ratio: 0.69,
+        placeholder: "143.94010294805804%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/Rgh7CxrtmPgLgKz2fC8g3Q/large.jpg",
+      },
+      href:
+        "/artwork/pablo-picasso-portrait-of-art-dealer-ambroise-vollard-1867-1939",
+      title: "Portrait of Art Dealer Ambroise Vollard (1867-1939)",
+      date: "Spring 1910",
+      sale_message: null,
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: "Pushkin Museum of Fine Arts, Moscow",
+      partner: {
+        name: "ARS/Art Resource",
+        href: "/ars-slash-art-resource",
+        __id: "UGFydG5lcjphcnMtc2xhc2gtYXJ0LXJlc291cmNl",
+        type: "Institution",
+      },
+      sale: null,
+      _id: "4f750b5ab4e2f50001000487",
+      is_inquireable: false,
+      sale_artwork: null,
+      id: "pablo-picasso-portrait-of-art-dealer-ambroise-vollard-1867-1939",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLWxhLXZpZS1saWZl",
+      image: {
+        aspect_ratio: 0.65,
+        placeholder: "153.63924050632912%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/uKQRWPcxmMU_1C7iYBnVcw/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-la-vie-life",
+      title: "La Vie [Life]",
+      date: "1903",
+      sale_message: null,
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: "The Cleveland Museum of Art, Cleveland",
+      partner: {
+        name: "ARS/Art Resource",
+        href: "/ars-slash-art-resource",
+        __id: "UGFydG5lcjphcnMtc2xhc2gtYXJ0LXJlc291cmNl",
+        type: "Institution",
+      },
+      sale: null,
+      _id: "4f750af433b7a500010004c6",
+      is_inquireable: false,
+      sale_artwork: null,
+      id: "pablo-picasso-la-vie-life",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLWd1ZXJuaWNh",
+      image: {
+        aspect_ratio: 2.21,
+        placeholder: "45.33571004159239%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/xv2Kv6g8nocq9xATLvS3Vw/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-guernica",
+      title: "Guernica",
+      date: "1937",
+      sale_message: null,
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: null,
+      partner: {
+        name: "Museo Reina Sofía",
+        href: "/museo-reina-sofia",
+        __id: "UGFydG5lcjptdXNlby1yZWluYS1zb2ZpYQ==",
+        type: "Institution",
+      },
+      sale: null,
+      _id: "55ef2b547261693af00000ab",
+      is_inquireable: false,
+      sale_artwork: null,
+      id: "pablo-picasso-guernica",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id:
+        "QXJ0d29yazpwYWJsby1waWNhc3NvLXN0aWxsLWxpZmUtd2l0aC1jaGFpci1jYW5pbmc=",
+      image: {
+        aspect_ratio: 1.28,
+        placeholder: "78.37734404898585%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/iwKcC80o_Ef-dH1Xh0k0Ug/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-still-life-with-chair-caning",
+      title: "Still-life with Chair Caning",
+      date: "Spring 1912",
+      sale_message: null,
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: "Musée National Picasso, Paris",
+      partner: {
+        name: "ARS/Art Resource",
+        href: "/ars-slash-art-resource",
+        __id: "UGFydG5lcjphcnMtc2xhc2gtYXJ0LXJlc291cmNl",
+        type: "Institution",
+      },
+      sale: null,
+      _id: "4f750b33653da10001000518",
+      is_inquireable: false,
+      sale_artwork: null,
+      id: "pablo-picasso-still-life-with-chair-caning",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLWRhbmllbC1oZW5yeS1rYWhud2VpbGVy",
+      image: {
+        aspect_ratio: 0.72,
+        placeholder: "137.99448022079116%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/rC3xdKws0h2ZlR_fmAK-dQ/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-daniel-henry-kahnweiler",
+      title: "Daniel-Henry Kahnweiler",
+      date: "1910",
+      sale_message: null,
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: null,
+      partner: {
+        name: "Art Institute of Chicago",
+        href: "/artic",
+        __id: "UGFydG5lcjphcnQtaW5zdGl0dXRlLW9mLWNoaWNhZ28=",
+        type: "Institution",
+      },
+      sale: null,
+      _id: "537e69741a1e86510c000037",
+      is_inquireable: false,
+      sale_artwork: null,
+      id: "pablo-picasso-daniel-henry-kahnweiler",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLXZpb2xpbi13aXRoLXNoZWV0LW9mLW11c2lj",
+      image: {
+        aspect_ratio: 0.82,
+        placeholder: "122.55180820804551%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/2pn4iPeFtUwJpOSM1EIJWA/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-violin-with-sheet-of-music",
+      title: "Violin with sheet of music",
+      date: "Autumn-1912",
+      sale_message: null,
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: "Musée national Picasso Paris, Paris",
+      partner: {
+        name: "ARS/Art Resource",
+        href: "/ars-slash-art-resource",
+        __id: "UGFydG5lcjphcnMtc2xhc2gtYXJ0LXJlc291cmNl",
+        type: "Institution",
+      },
+      sale: null,
+      _id: "4f750b26653da1000100050e",
+      is_inquireable: false,
+      sale_artwork: null,
+      id: "pablo-picasso-violin-with-sheet-of-music",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLWZlbW1lLWF1LW1vdWNob2lyLTE=",
+      image: {
+        aspect_ratio: 0.83,
+        placeholder: "120.125%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/TDHuYvPI_VMX9jLIh-XxmQ/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-femme-au-mouchoir-1",
+      title: "Femme au Mouchoir",
+      date: "1937",
+      sale_message: "Contact For Price",
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: null,
+      partner: {
+        name: "michael lisi / contemporary art",
+        href: "/michael-lisi-contemporary-art",
+        __id: "UGFydG5lcjptaWNoYWVsLWxpc2ktc2xhc2gtY29udGVtcG9yYXJ5LWFydA==",
+        type: "Gallery",
+      },
+      sale: null,
+      _id: "5792600b2a893a5b1b001360",
+      is_inquireable: true,
+      sale_artwork: null,
+      id: "pablo-picasso-femme-au-mouchoir-1",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLWxlLXJldmUtdGhlLWRyZWFt",
+      image: {
+        aspect_ratio: 0.74,
+        placeholder: "135.08771929824562%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/q1nXR7uZPvLoOXfgjLcGrg/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-le-reve-the-dream",
+      title: "Le Rêve (The Dream)",
+      date: "1932",
+      sale_message: null,
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: "Private Collection",
+      partner: {
+        name: "ARS/Art Resource",
+        href: "/ars-slash-art-resource",
+        __id: "UGFydG5lcjphcnMtc2xhc2gtYXJ0LXJlc291cmNl",
+        type: "Institution",
+      },
+      sale: null,
+      _id: "4f750b2233b7a500010004e4",
+      is_inquireable: false,
+      sale_artwork: null,
+      id: "pablo-picasso-le-reve-the-dream",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLWNpbmVzaWFzLWV0LXNhLW15cnJoaW5l",
+      image: {
+        aspect_ratio: 0.66,
+        placeholder: "152.4390243902439%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/9q-VozYbQY1lnUGlHd2GuQ/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-cinesias-et-sa-myrrhine",
+      title: "Cinésias et sa Myrrhine",
+      date: "1934",
+      sale_message: "Sold",
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: null,
+      partner: {
+        name: "Galerie d'Orsay",
+        href: "/galerie-dorsay",
+        __id: "UGFydG5lcjpnYWxlcmllLWRvcnNheQ==",
+        type: "Gallery",
+      },
+      sale: null,
+      _id: "5880de729c18db30cc001a9b",
+      is_inquireable: true,
+      sale_artwork: null,
+      id: "pablo-picasso-cinesias-et-sa-myrrhine",
+      is_saved: false,
+    },
+  },
+  {
+    node: {
+      __id: "QXJ0d29yazpwYWJsby1waWNhc3NvLXBpY2hldC10ZXRlLWNhcnJlZQ==",
+      image: {
+        aspect_ratio: 0.99,
+        placeholder: "101.04166666666667%",
+        url:
+          "https://d32dm0rphc51dk.cloudfront.net/jLvJU0z74q-FTVySvhFinA/large.jpg",
+      },
+      href: "/artwork/pablo-picasso-pichet-tete-carree",
+      title: "Pichet Tête Carrée",
+      date: "1953",
+      sale_message: "Contact For Price",
+      cultural_maker: null,
+      artists: [
+        {
+          __id: "QXJ0aXN0OnBhYmxvLXBpY2Fzc28=",
+          href: "/artist/pablo-picasso",
+          name: "Pablo Picasso",
+        },
+      ],
+      collecting_institution: null,
+      partner: {
+        name: "EHC Fine Art",
+        href: "/ehc-fine-art",
+        __id: "UGFydG5lcjplaGMtZmluZS1hcnQ=",
+        type: "Gallery",
+      },
+      sale: null,
+      _id: "58f6a5de8b0c143ab70b2830",
+      is_inquireable: true,
+      sale_artwork: null,
+      id: "pablo-picasso-pichet-tete-carree",
+      is_saved: false,
+    },
+  },
+]
