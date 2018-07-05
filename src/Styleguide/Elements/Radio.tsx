cbaae9ac243e4b3ca776064a7ef920b9a9cc9569
Zephraph <zephraph@gmail.com>
2018-07-05 14:25:20 -0400

    Add hover behavior to radio button

--- src/Styleguide/Elements/Radio.tsx
+++ src/Styleguide/Elements/Radio.tsx
@@ -62,7 +62,7 @@ export class Radio extends React.Component<RadioProps, RadioState> {
     return (
       <Flex my={0.3} onClick={() => this.toggleSelected()}>
         <RadioButton border={1} mr={1} selected={selected}>
-          {selected && <InnerCircle />}
+          <InnerCircle />
         </RadioButton>
         <Label>{children}</Label>
       </Flex>
@@ -70,30 +70,44 @@ export class Radio extends React.Component<RadioProps, RadioState> {
   }
 }
 
-const RadioButton = styled.div.attrs<RadioToggleProps>({})`
-  ${borders};
-  ${styledSpace};
-  background-color: ${({ selected }) =>
-    selected ? color("black100") : color("white100")};
-  border-color: ${({ selected }) =>
-    selected ? color("black100") : color("black10")};
-  width: ${space(2)}px;
-  height: ${space(2)}px;
-  border-radius: 50%;
+const Label = styled.div`
   cursor: pointer;
+  user-select: none;
 `
 
 const InnerCircle = styled.div`
   width: ${space(1)}px;
   height: ${space(1)}px;
   border-radius: 50%;
-  background-color: white;
   position: relative;
   left: 3px;
   top: 3px;
 `
 
-const Label = styled.div`
+const radioBackgroundColor = ({ selected }) =>
+  selected ? color("black100") : color("black5")
+const radioBorderColor = ({ selected }) =>
+  selected ? color("black100") : color("black10")
+const innerBackgroundColor = ({ selected }) =>
+  selected ? color("white100") : color("black5")
+
+const RadioButton = styled.div.attrs<RadioToggleProps>({})`
+  ${borders};
+  ${styledSpace};
+  background-color: ${radioBackgroundColor};
+  border-color: ${radioBorderColor};
+  width: ${space(2)}px;
+  height: ${space(2)}px;
+  border-radius: 50%;
   cursor: pointer;
-  user-select: none;
+  ${InnerCircle} {
+    background-color: ${innerBackgroundColor};
+  }
+
+  &:hover {
+    background-color: ${({ selected }) => !selected && color("black10")};
+    ${InnerCircle} {
+      background-color: ${color("white100")};
+    }
+  }
 `
