8579eab8c463f590d868945a4089ada841b7c34a
Matthew Zikherman <matt@bestvendor.com>
2018-07-12 13:00:53 -0400

    Guard against multiple in-flight filter requests

--- src/Apps/Artist/Routes/Overview/Components/ArtworkFilter/ArtworkFilterRefetch.tsx
+++ src/Apps/Artist/Routes/Overview/Components/ArtworkFilter/ArtworkFilterRefetch.tsx
@@ -11,14 +11,9 @@ interface Props {
   artistID: string
 }
 
-interface State {
-  loading: boolean
-}
+class ArtworkGridRefetchContainerWrapper extends React.Component<Props> {
+  private isLoading = false // Used to prevent multiple in-flight requests
 
-class ArtworkGridRefetchContainerWrapper extends React.Component<Props, State> {
-  state = {
-    loading: false,
-  }
   componentDidUpdate(prevProps) {
     Object.keys(this.props.filters).forEach(key => {
       if (this.props.filters[key] !== prevProps.filters[key]) {
@@ -28,24 +23,24 @@ class ArtworkGridRefetchContainerWrapper extends React.Component<Props, State> {
   }
 
   loadFilter = () => {
-    if (!this.state.loading) {
-      this.setState({ loading: true }, () => {
-        this.props.relay.refetch(
-          {
-            ...this.props.filters,
-            artistNodeID: this.props.artist.__id,
-          },
-          null,
-          error => {
-            if (error) {
-              console.error(error)
-            }
-            this.setState({ loading: false })
+    if (!this.isLoading) {
+      this.isLoading = true
+      this.props.relay.refetch(
+        {
+          ...this.props.filters,
+          artistNodeID: this.props.artist.__id,
+        },
+        null,
+        error => {
+          if (error) {
+            console.error(error)
           }
-        )
-      })
+          this.isLoading = false
+        }
+      )
     }
   }
+
   render() {
     return (
       <ArtworkGridRefetchContainer
