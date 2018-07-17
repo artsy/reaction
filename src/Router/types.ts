e304b92dbac329d17a66a2ae05fa593c12e3c0a4
Eloy Durán <eloy.de.enige@gmail.com>
2018-07-17 13:26:00 +0200

    [Router] Document some TODOs for AppConfig.

--- src/Router/types.ts
+++ src/Router/types.ts
@@ -10,18 +10,28 @@ export interface AppConfig {
   historyProtocol?: HistoryProtocol
   initialBreakpoint?: Breakpoint
   initialRoute?: string
-  initialState?: Array<Container<any>>
   initialAppState?: object // TODO: Deprecated
   routes: RouteConfig
   user?: User
 
+  // TODO: This is only used by the server app and so maybe it should move into
+  //       a server app config interface that extends this AppConfig interface?
+  //       That way it can be made required as well and inform the caller that
+  //       the argument is needed, right now that won’t happen.
+  url?: string
+
+  // TODO: This could get some generics so that the callback is typed and maybe
+  //       the various ‘apps’ (artist, artwork, etc) could have their own
+  //       exported `build*App` variants that only specify the types of
+  //       available state that can be provided/subscribed to by the host. E.g.
+  //
+  //         export buildArtistClientApp: BuildClientApp<[FilterState]> = buildClientApp
+  //
+  initialState?: Array<Container<any>>
   subscribe?: {
     to: SubscribeProps["to"]
     onChange: (...instances: Array<Container<any>>) => void
   }
-
-  // TODO: What is this used for?
-  url?: string
 }
 
 export type App = React.ComponentType<{}>
