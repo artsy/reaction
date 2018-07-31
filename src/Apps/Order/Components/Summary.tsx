1a2c5724f3b403e30e1ff926248169f29572e37f
Sarah Weir <sweir27@gmail.com>
2018-07-31 15:52:16 -0400

    subscribe to appstate to reference mediator

--- src/Apps/Order/Components/Summary.tsx
+++ src/Apps/Order/Components/Summary.tsx
@@ -3,7 +3,9 @@ import React, { Component } from "react"
 import { Spacer } from "Styleguide/Elements/Spacer"
 import { Placeholder } from "Styleguide/Utils/Placeholder"
 
+import { AppState } from "Router/state"
 import styled from "styled-components"
+import { Subscribe } from "unstated"
 
 type Mediator = {
   trigger: (action: string, config?: object) => void
@@ -16,11 +18,17 @@ export interface SummaryProps {
 export class Summary extends Component<SummaryProps> {
   render() {
     return (
-      <>
-        <Placeholder height="390px" name="Sidebar" />
-        {this.props.children}
-        <Helper mediator={this.props.mediator} />
-      </>
+      <Subscribe to={[AppState]}>
+        {({ state }) => {
+          const { mediator } = state
+
+          return <>
+              <Placeholder height="390px" name="Sidebar" />
+              {this.props.children}
+              <Helper mediator={mediator} />
+            </>
+        }}
+      </Subscribe>
     )
   }
 }
@@ -35,14 +43,19 @@ const Helper: React.SFC<SummaryProps> = ({ mediator }) => (
     <Sans size="2" color="black60">
       Have a question?{" "}
       <Link
-        onClick={() => mediator && mediator.trigger("openOrdersBuyerFAQModal")}
+        onClick={() => {
+          mediator && mediator.trigger("openOrdersBuyerFAQModal")
+        }}
       >
         Read our FAQ
       </Link>{" "}
       or{" "}
       <Link
         onClick={() =>
-          mediator && mediator.trigger("openOrdersContactArtsyModal")
+          mediator &&
+          mediator.trigger("openOrdersContactArtsyModal", {
+            artworkId: "lisa-breslow-cactus",
+          })
         }
       >
         ask an Artsy Specialist
