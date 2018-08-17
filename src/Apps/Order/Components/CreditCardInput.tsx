1dd11b2529112fe88e31ae0318ee2dff29c4d619
Zephraph <zephraph@gmail.com>
2018-08-17 16:11:37 -0400

    Add credit card input to payments page

---
+++ src/Apps/Order/Components/CreditCardInput.tsx
@@ -0,0 +1,54 @@
+import { color } from "@artsy/palette"
+import { fontFamily } from "@artsy/palette/dist/platform/fonts"
+import { border } from "Components/Mixins"
+import React from "react"
+import { CardElement } from "react-stripe-elements"
+import styled from "styled-components"
+import { BorderBox } from "Styleguide/Elements/Box"
+
+const StyledCardElement = styled(CardElement)`
+  width: 100%;
+  font-family: "Comic Sans";
+`
+
+interface StyledBorderBox {
+  hasError: boolean
+}
+const StyledBorderBox = styled(BorderBox).attrs<StyledBorderBox>({})`
+  ${border};
+`
+
+export class CreditCardInput extends React.Component {
+  state = {
+    focused: false,
+    error: false,
+  }
+
+  render() {
+    return (
+      <StyledBorderBox
+        className={`${this.state.focused ? "focused" : ""}`}
+        hasError={this.state.error}
+        p={1}
+      >
+        <StyledCardElement
+          onChange={res => {
+            this.setState({ error: res.error })
+          }}
+          onFocus={() => this.setState({ focused: true })}
+          onBlur={() => this.setState({ focused: false })}
+          style={{
+            base: {
+              "::placeholder": {
+                color: color("black30"),
+              },
+              fontFamily: fontFamily.serif.regular as string,
+              fontSmoothing: "antialiased",
+              lineHeight: "20px",
+            },
+          }}
+        />
+      </StyledBorderBox>
+    )
+  }
+}
--- src/Apps/Order/Routes/Payment/index.tsx
+++ src/Apps/Order/Routes/Payment/index.tsx
@@ -1,10 +1,13 @@
 import { Payment_order } from "__generated__/Payment_order.graphql"
 import { BuyNowStepper } from "Apps/Order/Components/BuyNowStepper"
+import { CreditCardInput } from "Apps/Order/Components/CreditCardInput"
 import { TwoColumnLayout } from "Apps/Order/Components/TwoColumnLayout"
 import React, { Component } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
+import { Elements } from "react-stripe-elements"
 import { Link } from "Router"
 import { Button } from "Styleguide/Elements/Button"
+import { Checkbox } from "Styleguide/Elements/Checkbox"
 import { Flex } from "Styleguide/Elements/Flex"
 import { Col, Row } from "Styleguide/Elements/Grid"
 import { Join } from "Styleguide/Elements/Join"
@@ -17,6 +20,14 @@ import { TransactionSummaryFragmentContainer as TransactionSummary } from "../..
 export interface PaymentProps {
   order: Payment_order
 }
+
+const ContinueButton = ({ order }) => (
+  <Link to={`/order2/${order.id}/review`}>
+    <Button size="large" width="100%">
+      Continue
+    </Button>
+  </Link>
+)
 
 export class PaymentRoute extends Component<PaymentProps> {
   render() {
@@ -31,46 +42,44 @@ export class PaymentRoute extends Component<PaymentProps> {
 
         <Spacer mb={3} />
 
-        <Responsive>
-          {({ xs }) => (
-            <TwoColumnLayout
-              Content={
-                <>
-                  <Join separator={<Spacer mb={3} />}>
-                    <Placeholder height="68px" name="Credit Card" />
-                    <Placeholder height="20px" name="Billing/Shipping Check" />
-                    {!xs && (
-                      <Link to={`/order2/${order.id}/review`}>
-                        <Button size="large" width="100%">
-                          Continue
-                        </Button>
-                      </Link>
+        <Elements>
+          <Responsive>
+            {({ xs }) => (
+              <TwoColumnLayout
+                Content={
+                  <>
+                    <Join separator={<Spacer mb={3} />}>
+                      <CreditCardInput />
+                      <Checkbox>
+                        Billing and shipping addresses are the same
+                      </Checkbox>
+                      <Placeholder
+                        height="20px"
+                        name="Billing/Shipping Check"
+                      />
+                      {!xs && <ContinueButton order={order} />}
+                    </Join>
+                    <Spacer mb={3} />
+                  </>
+                }
+                Sidebar={
+                  <Flex flexDirection="column">
+                    <TransactionSummary order={order} mb={xs ? 2 : 3} />
+                    <Helper
+                      artworkId={order.lineItems.edges[0].node.artwork.id}
+                    />
+                    {xs && (
+                      <>
+                        <Spacer mb={3} />
+                        <ContinueButton order={order} />
+                      </>
                     )}
-                  </Join>
-                  <Spacer mb={3} />
-                </>
-              }
-              Sidebar={
-                <Flex flexDirection="column">
-                  <TransactionSummary order={order} mb={xs ? 2 : 3} />
-                  <Helper
-                    artworkId={order.lineItems.edges[0].node.artwork.id}
-                  />
-                  {xs && (
-                    <>
-                      <Spacer mb={3} />
-                      <Link to={`/order2/${order.id}/review`}>
-                        <Button size="large" width="100%">
-                          Continue
-                        </Button>
-                      </Link>
-                    </>
-                  )}
-                </Flex>
-              }
-            />
-          )}
-        </Responsive>
+                  </Flex>
+                }
+              />
+            )}
+          </Responsive>
+        </Elements>
       </>
     )
   }
