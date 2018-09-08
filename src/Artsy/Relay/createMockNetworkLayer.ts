6a97b81151cb4e30930e7de2b3ee53eb34e4b075
Orta Therox <orta.therox@gmail.com>
2018-09-08 08:22:57 -0400

    Uses the schema SDL instead of the JSON

--- src/Artsy/Relay/createMockNetworkLayer.ts
+++ src/Artsy/Relay/createMockNetworkLayer.ts
@@ -1,12 +1,16 @@
+import { readFileSync } from "fs"
 import { IMocks } from "graphql-tools/dist/Interfaces"
 import getNetworkLayer from "relay-mock-network-layer"
 import { Network } from "relay-runtime"
-import schema from "../../../data/schema.json"
+
+// TODO: This doesn't work in client side rendering
+const schemaDSL = readFileSync("data/schema.graphql", "utf8")
 
 export const createMockNetworkLayer = (mockResolvers: IMocks) => {
+  const schemaObj = { data: schemaDSL }
   return Network.create(
     getNetworkLayer({
-      schema,
+      schemaObj,
       mocks: mockResolvers,
     })
   )
