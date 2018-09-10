24981ca2cca72c090bdef87aecab00f17092cd3a
Luc Succes <l2succes@gmail.com>
2018-09-10 15:41:01 -0400

    [Collect] add support for collect/<medium> urls

--- src/Apps/Collect/CollectApp.tsx
+++ src/Apps/Collect/CollectApp.tsx
@@ -29,8 +29,34 @@ export class CollectApp extends Component<CollectAppProps> {
 export const CollectAppFragmentContainer = createFragmentContainer(
   CollectApp,
   graphql`
-    fragment CollectApp_viewer on Viewer {
+    fragment CollectApp_viewer on Viewer
+      @argumentDefinitions(
+        medium: { type: "String", defaultValue: "*" }
+        major_periods: { type: "[String]" }
+        partner_id: { type: "ID" }
+        for_sale: { type: "Boolean" }
+        at_auction: { type: "Boolean" }
+        acquireable: { type: "Boolean" }
+        inquireable_only: { type: "Boolean" }
+        aggregations: {
+          type: "[ArtworkAggregation]"
+          defaultValue: [MAJOR_PERIOD, MEDIUM, TOTAL]
+        }
+        sort: { type: "String", defaultValue: "-partner_updated_at" }
+        price_range: { type: "String" }
+      ) {
       ...ArtworkGrid_viewer
+        @arguments(
+          medium: $medium
+          major_periods: $major_periods
+          partner_id: $partner_id
+          for_sale: $for_sale
+          sort: $sort
+          acquireable: $acquireable
+          at_auction: $at_auction
+          inquireable_only: $inquireable_only
+          price_range: $price_range
+        )
     }
   `
 )
--- src/Apps/Collect/Components/ArtworkGrid/CollectArtworkGrid.tsx
+++ src/Apps/Collect/Components/ArtworkGrid/CollectArtworkGrid.tsx
@@ -1,5 +1,5 @@
 import { ArtworkFilterArtworkGrid_filtered_artworks } from "__generated__/ArtworkFilterArtworkGrid_filtered_artworks.graphql"
-import { FilterState } from "Apps/Artist/Routes/Overview/state"
+import { FilterState } from "Apps/Collect/FilterState"
 import { ContextConsumer } from "Artsy/Router"
 import ArtworkGrid from "Components/ArtworkGrid"
 import React, { Component } from "react"
@@ -106,7 +106,6 @@ class Artworks extends Component<Props, LoadingAreaState> {
                         onNext={() => {
                           this.loadNext(filters, mediator)
                         }}
-                        scrollTo="#jump--artistArtworkGrid"
                       />
                     </Box>
                   </LoadingArea>
