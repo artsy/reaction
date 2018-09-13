6ac887d20ea6ab5714598a294d9a953e06d0855f
Luc Succes <l2succes@gmail.com>
2018-09-13 17:10:15 -0400

    [PATCH collect] adds footer

--- src/Apps/Collect/CollectApp.tsx
+++ src/Apps/Collect/CollectApp.tsx
@@ -1,8 +1,15 @@
+import { Separator } from "@artsy/palette"
 import { Box, Flex, Serif } from "@artsy/palette"
 import { CollectApp_viewer } from "__generated__/CollectApp_viewer.graphql"
 import React, { Component } from "react"
+import { LazyLoadComponent } from "react-lazy-load-image-component"
 import { createFragmentContainer, graphql } from "react-relay"
 import { ArtworkGridFragmentContainer as ArtworkGrid } from "./Components/ArtworkGrid"
+
+import {
+  Footer,
+  RecentlyViewedQueryRenderer as RecentlyViewed,
+} from "Styleguide/Components"
 
 export interface CollectAppProps {
   name: string
@@ -13,11 +20,26 @@ export class CollectApp extends Component<CollectAppProps> {
   render() {
     return (
       <Flex flexDirection="column">
-        <Box my={2}>
+        <Box mt={3} mb={4}>
           <Serif size="8">Collect Art &amp; Design Online</Serif>
         </Box>
         <Box>
           <ArtworkGrid viewer={this.props.viewer} />
+        </Box>
+
+        {typeof window !== "undefined" && (
+          <>
+            <Separator my={6} />
+            <LazyLoadComponent threshold={1000}>
+              <RecentlyViewed />
+            </LazyLoadComponent>
+          </>
+        )}
+
+        <Separator mt={6} mb={3} />
+
+        <Box>
+          <Footer />
         </Box>
       </Flex>
     )
