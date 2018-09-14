7774e85941c2dabae3f209f05f9e60a08d60e42d
sepans <sepans@sepans.com>
2018-09-14 16:00:14 -0400

    Fixes safari icon direction

--- src/Assets/Icons/ChevronIcon.tsx
+++ src/Assets/Icons/ChevronIcon.tsx
@@ -1,11 +1,12 @@
 import { color } from "@artsy/palette"
 import React from "react"
+import styled from "styled-components"
 
 export enum Direction {
   LEFT = "rotate(0)",
-  RIGHT = "rotate(180)",
-  UP = "rotate(90)",
-  DOWN = "rotate(270)",
+  RIGHT = "rotate(180deg)",
+  UP = "rotate(90deg)",
+  DOWN = "rotate(270deg)",
 }
 
 interface IconProps {
@@ -18,18 +19,23 @@ interface IconProps {
   width?: number
 }
 
+const RotateIcon = styled.div<{ direction?: Direction }>`
+  transform: ${({ direction }) => direction || Direction.RIGHT};
+`
+
 export const ChevronIcon = ({ direction, fill, height, width }: IconProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 12 12"
-    width={`${width || 8}px`}
-    height={`${height || 8}px`}
-    transform={direction || Direction.RIGHT}
-  >
-    <path
-      fill={fill ? fill : color("black10")}
-      fillRule="evenodd"
-      d="M9.091 10.758l-.788.771-5.832-5.708L8.303.113l.788.771-5.044 4.937 5.044 4.937"
-    />
-  </svg>
+  <RotateIcon direction={direction}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 12 12"
+      width={`${width || 8}px`}
+      height={`${height || 8}px`}
+    >
+      <path
+        fill={fill ? fill : color("black10")}
+        fillRule="evenodd"
+        d="M9.091 10.758l-.788.771-5.832-5.708L8.303.113l.788.771-5.044 4.937 5.044 4.937"
+      />
+    </svg>
+  </RotateIcon>
 )
