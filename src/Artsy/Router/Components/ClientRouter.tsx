ca85f248039cd0c20ed9226d54b4a14e01ea1073
Eloy Durán <eloy.de.enige@gmail.com>
2018-09-27 13:22:17 +0200

    [Router] Add typings to routes.

--- src/Artsy/Router/Components/ClientRouter.tsx
+++ src/Artsy/Router/Components/ClientRouter.tsx
@@ -2,13 +2,14 @@ import { createMockNetworkLayer } from "Artsy/Relay/createMockNetworkLayer"
 import { buildClientApp } from "Artsy/Router/buildClientApp"
 import { ContextProps } from "Artsy/SystemContext"
 import { HistoryOptions } from "farce"
+import { RouteConfig } from "found"
 import { IMocks } from "graphql-tools/dist/Interfaces"
 import React from "react"
 import { getUser } from "Utils/getUser"
 import { MatchingMediaQueries } from "Utils/Responsive"
 
 interface Props {
-  routes: object[]
+  routes: RouteConfig[]
   initialMatchingMediaQueries?: MatchingMediaQueries
   initialRoute?: string
   initialState?: object
