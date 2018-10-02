43ff7b95972c8833d5e0ea5142335e306fdbd19b
Eloy Durán <eloy.de.enige@gmail.com>
2018-10-02 18:07:39 +0200

    [DevTools] Add renderUntil which is used to test an async tree.

---
+++ src/DevTools/__test__/renderUntil.test.tsx
@@ -0,0 +1,55 @@
+import * as React from "react"
+import { renderUntil } from "../renderUntil"
+
+class Component extends React.Component {
+  state = {
+    data: "Loading",
+  }
+
+  componentDidMount() {
+    setImmediate(() => {
+      this.setState({ data: "ohai" })
+    })
+  }
+
+  render() {
+    return <div>{this.state.data}</div>
+  }
+}
+
+describe("renderUntil", () => {
+  it("yields an enzyme wrapper to the `until` block until it returns true", () => {
+    const states = []
+    return renderUntil(wrapper => {
+      const text = wrapper.find("div").text()
+      states.push(text)
+      return text !== "Loading"
+    }, <Component />).then(() => {
+      expect(states).toEqual(["Loading", "ohai"])
+    })
+  })
+
+  it("resolves the promise with an enzyme wrapper with the final state", () => {
+    return renderUntil(
+      wrapper => wrapper.find("div").text() !== "Loading",
+      <Component />
+    ).then(wrapper => {
+      expect(wrapper.find("div").text()).toEqual("ohai")
+    })
+  })
+
+  // TODO: Whatever way I try to test this, it just doesn’t work as expected.
+  // it("rejects the promise when a render error occurs", () => {
+  //   return renderUntil(wrapper => {
+  //     setImmediate(() => {
+  //       wrapper
+  //         .find(Component)
+  //         .first()
+  //         .simulateError(new Error("ohnoes"))
+  //     })
+  //     return false
+  //   }, <Component />).then(wrapper => {
+  //     expect(wrapper.find("div").text()).toEqual("ohai")
+  //   })
+  // })
+})
