345fa4594830a0b03ec29306f188c2d484cc0f82
Will Doenlen <wdoenlen@gmail.com>
2018-10-09 12:47:20 -0400

    Convert stateless Helper component to class-based component

--- src/Apps/Order/Components/Helper.tsx
+++ src/Apps/Order/Components/Helper.tsx
@@ -1,6 +1,6 @@
 import { Sans, Spacer } from "@artsy/palette"
 import { ContextConsumer } from "Artsy/Router"
-import React from "react"
+import React, { Component } from "react"
 import styled from "styled-components"
 
 const Link = styled.a`
@@ -11,28 +11,32 @@ interface HelperProps {
   artworkId: string | null
 }
 
-export const Helper: React.SFC<HelperProps> = ({ artworkId }) => (
-  <ContextConsumer>
-    {({ mediator }) => (
-      <>
-        <Sans size="2" color="black60">
-          Have a question?{" "}
-          <Link onClick={() => mediator.trigger("openOrdersBuyerFAQModal")}>
-            Read our FAQ
-          </Link>{" "}
-          or{" "}
-          <Link
-            onClick={() =>
-              mediator.trigger("openOrdersContactArtsyModal", {
-                artworkId,
-              })
-            }
-          >
-            ask a specialist
-          </Link>.
-        </Sans>
-        <Spacer mb={2} />
-      </>
-    )}
-  </ContextConsumer>
-)
+export class Helper extends Component<HelperProps> {
+  render() {
+    return (
+      <ContextConsumer>
+        {({ mediator }) => (
+          <>
+            <Sans size="2" color="black60">
+              Have a question?{" "}
+              <Link onClick={() => mediator.trigger("openOrdersBuyerFAQModal")}>
+                Read our FAQ
+              </Link>{" "}
+              or{" "}
+              <Link
+                onClick={() =>
+                  mediator.trigger("openOrdersContactArtsyModal", {
+                    artworkId: this.props.artworkId,
+                  })
+                }
+              >
+                ask a specialist
+              </Link>.
+            </Sans>
+            <Spacer mb={2} />
+          </>
+        )}
+      </ContextConsumer>
+    )
+  }
+}
