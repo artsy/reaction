9ae175c2695c1a0693d6e40ceab97b33dad48fb7
Eve Essex <eve.essex@gmail.com>
2018-10-22 11:59:03 -0400

    [PATCH collections] use hardcoded medium list if none are returned from query

--- src/Apps/Collect/Components/Filters/MediumFilter.tsx
+++ src/Apps/Collect/Components/Filters/MediumFilter.tsx
@@ -1,8 +1,7 @@
+import { Radio } from "@artsy/palette"
 import { ContextConsumer } from "Artsy/SystemContext"
 import React from "react"
 import { FilterState } from "../../FilterState"
-
-import { Radio } from "@artsy/palette"
 
 export const MediumFilter: React.SFC<{
   filters: FilterState
@@ -10,30 +9,80 @@ export const MediumFilter: React.SFC<{
     id: string
     name: string
   }>
-}> = ({ filters, mediums }) => (
-  <ContextConsumer>
-    {({ mediator }) =>
-      mediums.map((medium, index) => {
-        const isSelected = filters.state.medium === medium.id
+}> = ({ filters, mediums }) => {
+  const allowedMediums = mediums && mediums.length ? mediums : hardcodedMediums
+  return (
+    <ContextConsumer>
+      {({ mediator }) =>
+        allowedMediums.map((medium, index) => {
+          const isSelected = filters.state.medium === medium.id
 
-        return (
-          <Radio
-            my={0.3}
-            selected={isSelected}
-            value={medium.id}
-            onSelect={({ selected }) => {
-              if (selected) {
-                return filters.setFilter("medium", medium.id, mediator)
-              } else {
-                return filters.unsetFilter("medium", mediator)
-              }
-            }}
-            key={index}
-          >
-            {medium.name}
-          </Radio>
-        )
-      })
-    }
-  </ContextConsumer>
-)
+          return (
+            <Radio
+              my={0.3}
+              selected={isSelected}
+              value={medium.id}
+              onSelect={({ selected }) => {
+                if (selected) {
+                  return filters.setFilter("medium", medium.id, mediator)
+                } else {
+                  return filters.unsetFilter("medium", mediator)
+                }
+              }}
+              key={index}
+            >
+              {medium.name}
+            </Radio>
+          )
+        })
+      }
+    </ContextConsumer>
+  )
+}
+
+const hardcodedMediums = [
+  {
+    id: "painting",
+    name: "Painting",
+  },
+  {
+    id: "photography",
+    name: "Photography",
+  },
+  {
+    id: "sculpture",
+    name: "Sculpture",
+  },
+  {
+    id: "prints",
+    name: "Prints",
+  },
+  {
+    id: "work-on-Paper",
+    name: "Work on Paper",
+  },
+  {
+    id: "design",
+    name: "Design",
+  },
+  {
+    id: "drawing",
+    name: "Drawing",
+  },
+  {
+    id: "installation",
+    name: "Installation",
+  },
+  {
+    id: "film-slash-video",
+    name: "Film/Video",
+  },
+  {
+    id: "jewelry",
+    name: "Jewelry",
+  },
+  {
+    id: "performance-art",
+    name: "Performance Art",
+  },
+]
