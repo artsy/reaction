8314aee07a72550087fae52a98a082a027788581
Matthew Zikherman <matt@bestvendor.com>
2018-10-22 14:25:38 -0400

    [Testing] Add a test for the artist page header

---
+++ src/Apps/__test__/Fixtures/Artist/ArtistHeader.tsx
@@ -0,0 +1,30 @@
+export const ArtistHeaderFixture = {
+  _id: "4d8b92884eb68a1b2c0001d8",
+  id: "cecily-brown",
+  name: "Cecily Brown",
+  nationality: "British",
+  years: "born 1969",
+  counts: { follows: 9135 },
+  carousel: {
+    images: [
+      {
+        href: "/show/two-palms-two-palms-at-the-armory-show-2016",
+        resized: {
+          url:
+            "https://d7hftxdivxxvm.cloudfront.net?resize_to=fit&width=359&height=200&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FlL-b8v9q-GLBENMOg9pVGQ%2Flarge.jpg",
+          width: 359,
+          height: 200,
+        },
+      },
+    ],
+  },
+  __id: "QXJ0aXN0OmNlY2lseS1icm93bg==",
+  is_followed: true,
+  statuses: {
+    shows: true,
+    artists: true,
+    articles: false,
+    cv: true,
+    auction_lots: true,
+  },
+}
---
+++ src/__generated__/ArtistHeader_Test_Query.graphql.ts
@@ -0,0 +1,258 @@
+/* tslint:disable */
+
+import { ConcreteRequest } from "relay-runtime";
+import { ArtistHeader_artist$ref } from "./ArtistHeader_artist.graphql";
+export type ArtistHeader_Test_QueryVariables = {};
+export type ArtistHeader_Test_QueryResponse = {
+    readonly artist: ({
+        readonly " $fragmentRefs": ArtistHeader_artist$ref;
+    }) | null;
+};
+export type ArtistHeader_Test_Query = {
+    readonly response: ArtistHeader_Test_QueryResponse;
+    readonly variables: ArtistHeader_Test_QueryVariables;
+};
+
+
+
+/*
+query ArtistHeader_Test_Query {
+  artist(id: "pablo-picasso") {
+    ...ArtistHeader_artist
+    __id
+  }
+}
+
+fragment ArtistHeader_artist on Artist {
+  _id
+  id
+  name
+  nationality
+  years
+  counts {
+    follows
+  }
+  carousel {
+    images {
+      href
+      resized(height: 200) {
+        url
+        width
+        height
+      }
+    }
+  }
+  ...FollowArtistButton_artist
+  __id
+}
+
+fragment FollowArtistButton_artist on Artist {
+  __id
+  id
+  is_followed
+  counts {
+    follows
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "id",
+    "value": "pablo-picasso",
+    "type": "String!"
+  }
+],
+v1 = {
+  "kind": "ScalarField",
+  "alias": null,
+  "name": "__id",
+  "args": null,
+  "storageKey": null
+};
+return {
+  "kind": "Request",
+  "operationKind": "query",
+  "name": "ArtistHeader_Test_Query",
+  "id": null,
+  "text": "query ArtistHeader_Test_Query {\n  artist(id: \"pablo-picasso\") {\n    ...ArtistHeader_artist\n    __id\n  }\n}\n\nfragment ArtistHeader_artist on Artist {\n  _id\n  id\n  name\n  nationality\n  years\n  counts {\n    follows\n  }\n  carousel {\n    images {\n      href\n      resized(height: 200) {\n        url\n        width\n        height\n      }\n    }\n  }\n  ...FollowArtistButton_artist\n  __id\n}\n\nfragment FollowArtistButton_artist on Artist {\n  __id\n  id\n  is_followed\n  counts {\n    follows\n  }\n}\n",
+  "metadata": {},
+  "fragment": {
+    "kind": "Fragment",
+    "name": "ArtistHeader_Test_Query",
+    "type": "Query",
+    "metadata": null,
+    "argumentDefinitions": [],
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "name": "artist",
+        "storageKey": "artist(id:\"pablo-picasso\")",
+        "args": v0,
+        "concreteType": "Artist",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "FragmentSpread",
+            "name": "ArtistHeader_artist",
+            "args": null
+          },
+          v1
+        ]
+      }
+    ]
+  },
+  "operation": {
+    "kind": "Operation",
+    "name": "ArtistHeader_Test_Query",
+    "argumentDefinitions": [],
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "name": "artist",
+        "storageKey": "artist(id:\"pablo-picasso\")",
+        "args": v0,
+        "concreteType": "Artist",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "_id",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "id",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "name",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "nationality",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "years",
+            "args": null,
+            "storageKey": null
+          },
+          {
+            "kind": "LinkedField",
+            "alias": null,
+            "name": "counts",
+            "storageKey": null,
+            "args": null,
+            "concreteType": "ArtistCounts",
+            "plural": false,
+            "selections": [
+              {
+                "kind": "ScalarField",
+                "alias": null,
+                "name": "follows",
+                "args": null,
+                "storageKey": null
+              }
+            ]
+          },
+          {
+            "kind": "LinkedField",
+            "alias": null,
+            "name": "carousel",
+            "storageKey": null,
+            "args": null,
+            "concreteType": "ArtistCarousel",
+            "plural": false,
+            "selections": [
+              {
+                "kind": "LinkedField",
+                "alias": null,
+                "name": "images",
+                "storageKey": null,
+                "args": null,
+                "concreteType": "Image",
+                "plural": true,
+                "selections": [
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "name": "href",
+                    "args": null,
+                    "storageKey": null
+                  },
+                  {
+                    "kind": "LinkedField",
+                    "alias": null,
+                    "name": "resized",
+                    "storageKey": "resized(height:200)",
+                    "args": [
+                      {
+                        "kind": "Literal",
+                        "name": "height",
+                        "value": 200,
+                        "type": "Int"
+                      }
+                    ],
+                    "concreteType": "ResizedImageUrl",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "url",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "width",
+                        "args": null,
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "height",
+                        "args": null,
+                        "storageKey": null
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          v1,
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "name": "is_followed",
+            "args": null,
+            "storageKey": null
+          }
+        ]
+      }
+    ]
+  }
+};
+})();
+(node as any).hash = '3e20df33f85575b920fbfb7abe31f131';
+export default node;
