2ffe2aa898cb413028fa7c40c10d384c0f528681
oxaudo <oksana@artsymail.com>
2018-10-23 11:55:05 -0400

    more updates to spec

--- src/Apps/Artwork/Components/__tests__/ArtworkSidebar.test.tsx
+++ src/Apps/Artwork/Components/__tests__/ArtworkSidebar.test.tsx
@@ -1,13 +1,13 @@
 import { cloneDeep } from "lodash"
 import { graphql } from "react-relay"
 import { renderRelayTree } from "../../../../DevTools"
-import { ArtworkSidebarFixture } from "../../../__test__/Fixtures/Artwork/ArtworkSidebar"
+import { NoAuctionArtworkSidebarFixture } from "../../../__test__/Fixtures/Artwork/ArtworkSidebar"
 import { ArtworkSidebarFragmentContainer } from "../ArtworkSidebar/index"
 
 jest.unmock("react-relay")
 
-describe("ArtworkSidebar", () => {
-  const getWrapper = async (response = ArtworkSidebarFixture) => {
+describe("NoAuctionArtworkSidebar", () => {
+  const getWrapper = async (response = NoAuctionArtworkSidebarFixture) => {
     return await renderRelayTree({
       Component: ArtworkSidebarFragmentContainer,
       query: graphql`
@@ -32,7 +32,7 @@ describe("ArtworkSidebar", () => {
         expect(
           wrapper.find({
             href: "/artist/josef-albers",
-          })
+          }).length
         ).toBe(1)
       })
       it("renders artist follow button for single artist", async () => {
@@ -43,7 +43,7 @@ describe("ArtworkSidebar", () => {
 
     describe("ArtworkSidebarArtists with multiple artists", () => {
       beforeEach(() => {
-        data = cloneDeep(ArtworkSidebarFixture)
+        data = cloneDeep(NoAuctionArtworkSidebarFixture)
         data.artists.push({
           __id: "QXJ0aXN0OmVkLXJ1c2NoYQ==",
           id: "ed-ruscha",
@@ -59,7 +59,17 @@ describe("ArtworkSidebar", () => {
       it("displays artist names for multiople artists", async () => {
         const wrapper = await getWrapper(data)
         expect(wrapper.html()).toContain("Josef Albers")
+        expect(
+          wrapper.find({
+            href: "/artist/josef-albers",
+          }).length
+        ).toBe(1)
         expect(wrapper.html()).toContain("Ed Ruscha")
+        expect(
+          wrapper.find({
+            href: "/artist/ed-ruscha",
+          }).length
+        ).toBe(1)
       })
       it("does not display follow buttons", async () => {
         const wrapper = await getWrapper(data)
--- src/Apps/__test__/Fixtures/Artwork/ArtworkSidebar.ts
+++ src/Apps/__test__/Fixtures/Artwork/ArtworkSidebar.ts
@@ -1,4 +1,4 @@
-export const ArtworkSidebarFixture = {
+export const NoAuctionArtworkSidebarFixture = {
   id: "josef-albers-homage-to-the-square-85",
   is_biddable: false,
   is_in_auction: false,
