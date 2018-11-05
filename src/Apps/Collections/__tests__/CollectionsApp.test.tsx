6cd02d3ccc3cf2304b85933ff7dd2f1583100de9
Eve Essex <eve.essex@gmail.com>
2018-11-05 12:53:48 -0500

    add tests for collections app

--- src/Apps/Collections/__tests__/CollectionsApp.test.tsx
+++ src/Apps/Collections/__tests__/CollectionsApp.test.tsx
@@ -1,37 +1,38 @@
-// import { Sans } from "@artsy/palette"
-// import { CollectionsApp_collections } from "__generated__/CollectionsApp_collections.graphql"
-// import { CollectionsGrid } from "Apps/Collections/Components/CollectionsGrid"
-// import React from "react"
-// import { graphql } from "react-relay"
-// import { MockBoot, MockRelayRenderer, renderUntil } from "../../../DevTools"
-// import { CollectionsFixture } from "../../__test__/Fixtures/CollectionsFixture"
-// import { CollectionsAppFragmentContainer as CollectionsApp } from "../CollectionsApp"
+import React from "react"
+import { graphql } from "react-relay"
+import { EntityHeader } from "Styleguide/Components/EntityHeader"
+import { MockBoot, renderRelayTree } from "../../../DevTools"
+import { CollectionsFixture } from "../../__test__/Fixtures/CollectionsFixture"
+import { CollectionsAppFragmentContainer as CollectionsApp } from "../CollectionsApp"
 
 jest.unmock("react-relay")
 
 describe("CollectionApp", () => {
   it("renders a relay tree correctly", async () => {
-    // const tree = await renderUntil(
-    //   wrapper => {
-    //     return wrapper.find(Sans).length > 0
-    //   },
-    //   <MockBoot breakpoint="lg">
-    //     <MockRelayRenderer
-    //       Component={CollectionsApp}
-    //       query={graphql`
-    //         query CollectionsAppQuery {
-    //           collections: marketingCollections {
-    //             ...CollectionsApp_collections
-    //           }
-    //         }
-    //       `}
-    //       mockResolvers={{
-    //         MarketingCollection: () => CollectionsFixture,
-    //       }}
-    //     />
-    //   </MockBoot>
-    // )
+    const getRelayWrapper = async () => {
+      return await renderRelayTree({
+        Component: ({ collections }) => (
+          <MockBoot breakpoint="lg">
+            <CollectionsApp collections={collections} />
+          </MockBoot>
+        ),
+        query: graphql`
+          query CollectionsAppQuery {
+            collections: marketingCollections {
+              ...CollectionsApp_collections
+            }
+          }
+        `,
+        mockResolvers: {
+          Query: () => ({
+            marketingCollections: CollectionsFixture,
+          }),
+        },
+      })
+    }
+    const tree = await getRelayWrapper()
 
-    expect(true).toBeTruthy()
+    expect(tree.find(EntityHeader).length).toBe(6)
+    expect(tree.text()).toMatch("Big Artists, Small Sculptures")
   })
 })
