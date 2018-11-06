e2c912c8ec1fad987caa7a74b719ea355ca28be2
Will Doenlen <wdoenlen@gmail.com>
2018-11-06 09:59:43 -0500

    Use Array.find when searching for errors and errorInfO

--- src/Utils/logger.ts
+++ src/Utils/logger.ts
@@ -1,4 +1,3 @@
-import { head } from "lodash"
 import { isErrorInfo, sendErrorToService } from "Utils/errors"
 
 export const shouldCaptureError = (environment: string) =>
@@ -15,8 +14,8 @@ export default function createLogger(namespace = "reaction") {
       console.warn(formattedNamespace, ...warnings, "\n")
     },
     error: (...errors) => {
-      const error = head(errors.filter(e => e instanceof Error))
-      const errorInfo = head(errors.filter(e => isErrorInfo(e)))
+      const error = errors.find(e => e instanceof Error)
+      const errorInfo = errors.find(isErrorInfo)
 
       if (error && shouldCaptureError(process.env.NODE_ENV)) {
         sendErrorToService(error, errorInfo)
