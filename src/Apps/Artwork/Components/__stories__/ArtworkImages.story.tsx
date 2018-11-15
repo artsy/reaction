a15f550853d739d56889bf8efa88302e416c9899
Adam Iskounen <adam.iskounen@artsymail.com>
2018-11-15 16:42:04 -0500

    Cleaned up artwork image story.

--- src/Apps/Artwork/Components/__stories__/ArtworkImages.story.tsx
+++ src/Apps/Artwork/Components/__stories__/ArtworkImages.story.tsx
@@ -4,22 +4,19 @@ import { Col } from "Styleguide/Elements/Grid"
 import { Section } from "Styleguide/Utils/Section"
 import { ArtworkImagesQueryRenderer } from "../ArtworkImages"
 
-storiesOf("Styleguide/Artwork/ArtworkImages", module).add(
-  "With live data",
-  () => {
-    return (
-      <>
-        <Section title="Multiple images">
-          <Col sm="8">
-            <ArtworkImagesQueryRenderer artworkID="andy-warhol-lenin-fs-ii-dot-402-1" />
-          </Col>
-        </Section>
-        <Section title="Single image">
-          <Col sm="8">
-            <ArtworkImagesQueryRenderer artworkID="pablo-picasso-david-et-bethsabee" />
-          </Col>
-        </Section>
-      </>
-    )
-  }
-)
+storiesOf("Styleguide/Artwork", module).add("ArtworkImage", () => {
+  return (
+    <>
+      <Section title="Multiple images">
+        <Col sm="8">
+          <ArtworkImagesQueryRenderer artworkID="andy-warhol-lenin-fs-ii-dot-402-1" />
+        </Col>
+      </Section>
+      <Section title="Single image">
+        <Col sm="8">
+          <ArtworkImagesQueryRenderer artworkID="pablo-picasso-david-et-bethsabee" />
+        </Col>
+      </Section>
+    </>
+  )
+})
