9de2bbad2d50e7cc9b774d731822dabd6daba177
Eloy Durán <eloy.de.enige@gmail.com>
2018-11-21 15:56:39 +0100

    [BREAKING Router] Move hydration data outside of React's control.
    Because this is different on server vs client, it doesn't hydrate well.
    Additionally, hydration data should come after the main markup, so the
    browser can render the markup ASAP, whereas hydration data is only
    needed when the client-side JS is loaded.
    
    The `buildServerApp` function will now yield a `scripts` string that
    contains raw `<script>` tags with e.g. dehydrated data. These scripts
    should be inserted into the HTML document after the main markup, but
    before the JS code that hydrates the server-side content.

--- src/Artsy/Router/__tests__/buildServerApp.test.tsx
+++ src/Artsy/Router/__tests__/buildServerApp.test.tsx
@@ -23,7 +23,7 @@ describe("buildServerApp", () => {
     Component = defaultComponent,
     options = {},
   } = {}) => {
-    const { ServerApp, status, headTags } = await buildServerApp({
+    const { ServerApp, status, headTags, scripts } = await buildServerApp({
       routes: [
         {
           path: "/",
@@ -38,6 +38,7 @@ describe("buildServerApp", () => {
       wrapper: render(<ServerApp />),
       status,
       headTags,
+      scripts,
     }
   }
 
@@ -47,9 +48,9 @@ describe("buildServerApp", () => {
   })
 
   it("bootstraps relay and loadable-components SSR data", async () => {
-    const { wrapper } = await getWrapper()
-    expect(wrapper.html()).toContain("__RELAY_BOOTSTRAP__")
-    expect(wrapper.html()).toContain("__LOADABLE_STATE__")
+    const { scripts } = await getWrapper()
+    expect(scripts).toContain("__RELAY_BOOTSTRAP__")
+    expect(scripts).toContain("__LOADABLE_STATE__")
   })
 
   it("resolves with a 200 status if url matches request", async () => {
