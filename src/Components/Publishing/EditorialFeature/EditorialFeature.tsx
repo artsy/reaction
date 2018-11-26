4365ee8aab95ee8ceb07746cfb50b4a7ee8886a6
Eve Essex <eve.essex@gmail.com>
2018-11-26 16:44:04 -0500

    [PATCH publishing] rename eoy keys

--- src/Components/Publishing/EditorialFeature/EditorialFeature.tsx
+++ src/Components/Publishing/EditorialFeature/EditorialFeature.tsx
@@ -8,10 +8,10 @@ import { Eoy2018Culture } from "./Components/Eoy2018Culture"
 
 export const EditorialFeature: React.SFC<ArticleProps> = props => {
   switch (props.customEditorial) {
-    case "EOY_2018_1": {
+    case "EOY_2018_ARTISTS": {
       return <Eoy2018Artists {...props} />
     }
-    case "EOY_2018_2": {
+    case "EOY_2018_CULTURE": {
       return <Eoy2018Culture {...props} />
     }
     default: {
