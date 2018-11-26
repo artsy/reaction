73e4ceec5d77047efebfc841adafbd2fa320886f
Will Doenlen <wdoenlen@gmail.com>
2018-11-26 15:11:11 -0500

    Update tests

--- src/Utils/__tests__/errors.test.ts
+++ src/Utils/__tests__/errors.test.ts
@@ -1,28 +1,27 @@
 import * as Sentry from "@sentry/browser"
-import { ErrorWithMetadata, sendErrorToService } from "Utils/errors"
+import { ErrorWithMetadata, reportError } from "Utils/errors"
 
 jest.mock("@sentry/browser")
 
 describe("errors", () => {
-  describe("#ErrorWithMetadata", () => {
-    // todo
-  })
-  describe("#sendErrorToService", () => {
-    describe("with an Error (without metadata)", () => {
-      it("sends an error to Sentry", () => {
-        sendErrorToService(new Error("msg"))
-        expect(Sentry.withScope).toHaveBeenCalled()
-      })
+  describe("#reportError", () => {
+    const scope = { setExtra: jest.fn() }
+    const err = new Error("some error")
+    const errWithMetadata = new ErrorWithMetadata("some message", {
+      foo: "bar",
     })
-    describe("with an ErrorWithMetadata", () => {
-      it("sends an error to Sentry", () => {
-        const errorMetadata = { someProp: "more error info" }
-        sendErrorToService(new ErrorWithMetadata("msg", errorMetadata))
-        expect(Sentry.withScope).toHaveBeenCalled()
-      })
-      it("sets metadata on Sentry scope", () => {
-        // todo
-      })
+    it("does not call setExtra on scope if the error has no metadata", () => {
+      scope.setExtra.mockReset()
+      reportError(err)(scope)
+      expect(scope.setExtra).not.toBeCalled()
+    })
+    it("calls setExtra on scope for errors with metadata", () => {
+      reportError(errWithMetadata)(scope)
+      expect(scope.setExtra).toBeCalledWith("foo", "bar")
+    })
+    it("sends the error to Sentry", () => {
+      reportError(errWithMetadata)(scope)
+      expect(Sentry.captureException).toBeCalledWith(err)
     })
   })
 })
