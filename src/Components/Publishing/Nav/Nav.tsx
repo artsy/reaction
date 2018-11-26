d092293e0c2e873d3ef109754d9b51c1979a9006
Eve Essex <eve.essex@gmail.com>
2018-11-26 17:20:20 -0500

    [PATCH publishing] editorial nav accepts custom color props

--- src/Components/Publishing/Nav/Nav.tsx
+++ src/Components/Publishing/Nav/Nav.tsx
@@ -1,12 +1,14 @@
 import { garamond } from "Assets/Fonts"
 import { pMedia } from "Components/Helpers"
+import { PartnerInline } from "Components/Publishing/Partner/PartnerInline"
 import React from "react"
 import Waypoint from "react-waypoint"
-import styled, { StyledFunction } from "styled-components"
-import { PartnerInline } from "../Partner/PartnerInline"
+import styled from "styled-components"
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
+  backgroundColor?: string
   canFix?: boolean
+  color?: string
   sponsor?: any
   title?: string
   transparent?: boolean
@@ -16,14 +18,11 @@ interface State {
   isFixed: boolean
 }
 
-interface DivProps {
-  transparent: boolean
-  isFixed: boolean
-}
-
 export class NavComponent extends React.Component<Props, State> {
   static defaultProps = {
+    backgroundColor: "black",
     canFix: true,
+    color: "white",
     transparent: false,
   }
 
@@ -41,23 +40,33 @@ export class NavComponent extends React.Component<Props, State> {
   }
 
   render() {
-    const { sponsor, className, canFix, transparent, title } = this.props
+    const {
+      backgroundColor,
+      color,
+      sponsor,
+      className,
+      canFix,
+      transparent,
+      title,
+    } = this.props
     const { isFixed } = this.state
 
     return (
       <div>
         <NavContainer
-          transparent={!isFixed && transparent}
+          backgroundColor={backgroundColor}
           className={className}
+          color={color}
           isFixed={canFix && isFixed}
+          transparent={!isFixed && transparent}
         >
           <PartnerInline
             url={sponsor && sponsor.partner_logo_link}
             logo={sponsor && sponsor.partner_condensed_logo}
-            color="white"
+            color={color}
             margin="0 10px"
           />
-          <Title>
+          <Title color={color}>
             {title ? title : <a href="/magazine">Artsy Editorial</a>}
           </Title>
         </NavContainer>
@@ -70,19 +79,22 @@ export class NavComponent extends React.Component<Props, State> {
   }
 }
 
-const Div: StyledFunction<DivProps & React.HTMLProps<HTMLDivElement>> =
-  styled.div
-
-const NavContainer = Div`
-  background-color: ${props => (props.transparent ? "transparent" : "black")};
-  border-bottom: 1px solid white;
+const NavContainer = styled.div<{
+  backgroundColor: string
+  transparent: boolean
+  isFixed: boolean
+}>`
+  background-color: ${props =>
+    props.transparent ? "transparent" : props.backgroundColor};
+  border-bottom: 1px solid ${props => props.color};
   ${props =>
     props.transparent &&
     !props.isFixed &&
     `
     position: absolute;
     top: 0;
-  `}
+  `};
+
   ${props =>
     props.isFixed &&
     `
@@ -90,7 +102,7 @@ const NavContainer = Div`
     top: 0;
     left: 0;
     right: 0;
-  `}
+  `};
 `
 export const Nav = styled(NavComponent)`
   position: relative;
@@ -99,20 +111,20 @@ export const Nav = styled(NavComponent)`
   align-items: center;
   height: 52px;
   width: 100%;
-  color: white;
+  color: ${props => props.color};
   z-index: 10;
   a {
     z-index: 10;
   }
 `
-const Title = styled.div`
+const Title = styled.div<{ color: string }>`
   ${garamond("s17")};
   position: absolute;
   width: 100%;
   text-align: center;
   font-weight: 600;
   a {
-    color: white;
+    color: ${props => props.color};
     text-decoration: none;
   }
   ${pMedia.sm`
