154458fc2b9613b1210ee651665377687e66421a
David Sheldrick <djsheldrick@gmail.com>
2018-12-07 14:22:48 +0000

    add Counter page

---
+++ src/Apps/Order/Routes/__tests__/Counter.test.tsx
@@ -0,0 +1,153 @@
+import { Button } from "@artsy/palette"
+import {
+  Buyer,
+  OfferOrderWithShippingDetails,
+  Offers,
+  OfferWithTotals,
+} from "Apps/__tests__/Fixtures/Order"
+import { ArtworkSummaryItemFragmentContainer } from "Apps/Order/Components/ArtworkSummaryItem"
+import { CreditCardSummaryItemFragmentContainer } from "Apps/Order/Components/CreditCardSummaryItem"
+import { OrderStepper } from "Apps/Order/Components/OrderStepper"
+import { ShippingSummaryItemFragmentContainer } from "Apps/Order/Components/ShippingSummaryItem"
+import { TransactionDetailsSummaryItemFragmentContainer } from "Apps/Order/Components/TransactionDetailsSummaryItem"
+import { MockBoot } from "DevTools"
+import { mount } from "enzyme"
+import moment from "moment"
+import React from "react"
+import { commitMutation as _commitMutation } from "react-relay"
+import { Stepper } from "Styleguide/Components"
+import { CountdownTimer } from "Styleguide/Components/CountdownTimer"
+import { CounterFragmentContainer as CounterRoute } from "../Counter"
+
+jest.mock("Utils/getCurrentTimeAsIsoString")
+
+const NOW = "2018-12-05T13:47:16.446Z"
+
+require("Utils/getCurrentTimeAsIsoString").__setCurrentTime(NOW)
+
+jest.mock("react-relay", () => ({
+  commitMutation: jest.fn(),
+  createFragmentContainer: component => component,
+}))
+
+const testOrder = {
+  ...OfferOrderWithShippingDetails,
+  stateExpiresAt: moment(NOW)
+    .add(1, "day")
+    .toISOString(),
+  lastOffer: {
+    ...OfferWithTotals,
+    createdAt: moment(NOW)
+      .subtract(1, "day")
+      .toISOString(),
+    amount: "$sellers.offer",
+  },
+  myLastOffer: {
+    ...OfferWithTotals,
+    createdAt: moment(NOW)
+      .subtract(1, "seconds")
+      .toISOString(),
+    amount: "$your.offer",
+    fromParticipant: "BUYER",
+  },
+  offers: { edges: Offers },
+  buyer: Buyer,
+}
+
+let mockPushRoute: jest.Mock<string>
+let mockMediatorTrigger: jest.Mock<string>
+
+describe("Offer InitialMutation", () => {
+  const getWrapper = (extraOrderProps?) => {
+    return mount(
+      <MockBoot>
+        <CounterRoute
+          relay={{ environment: {} }}
+          router={{ push: mockPushRoute }}
+          mediator={{ trigger: mockMediatorTrigger }}
+          order={{
+            ...testOrder,
+            ...extraOrderProps,
+          }}
+        />
+      </MockBoot>
+    )
+  }
+
+  beforeEach(() => {
+    mockPushRoute = jest.fn()
+    mockMediatorTrigger = jest.fn()
+  })
+
+  it("Shows the stepper", () => {
+    const component = getWrapper()
+    const stepper = component.find(OrderStepper)
+    expect(stepper.text()).toMatchInlineSnapshot(`"Respond Review"`)
+
+    const index = component.find(Stepper).props().currentStepIndex
+    expect(index).toBe(1)
+  })
+
+  it("shows the countdown timer", () => {
+    const component = getWrapper({
+      stateExpiresAt: moment(NOW)
+        .add(1, "day")
+        .add(4, "hours")
+        .add(22, "minutes")
+        .add(59, "seconds"),
+    })
+    const timer = component.find(CountdownTimer)
+    expect(timer.text()).toContain("01d 04h 22m 59s left")
+  })
+
+  it("shows the transaction summary", () => {
+    const component = getWrapper()
+    const transactionSummary = component.find(
+      TransactionDetailsSummaryItemFragmentContainer
+    )
+    expect(transactionSummary).toHaveLength(1)
+
+    expect(transactionSummary.text()).toMatch("Your counterofferChange")
+    expect(transactionSummary.text()).toMatch("Your offer$your.offer")
+    expect(transactionSummary.text()).toMatch("Seller's offer$sellers.offer")
+  })
+
+  it("shows the artwork summary", () => {
+    const component = getWrapper()
+    const artworkSummary = component.find(ArtworkSummaryItemFragmentContainer)
+    expect(artworkSummary).toHaveLength(1)
+
+    expect(artworkSummary.text()).toMatch("Lisa BreslowGramercy Park South")
+  })
+
+  it("shows the shipping details", () => {
+    const component = getWrapper()
+    const shippingSummary = component.find(ShippingSummaryItemFragmentContainer)
+    expect(shippingSummary).toHaveLength(1)
+
+    expect(shippingSummary.text()).toMatch("Ship toJoelle Van Dyne401 Broadway")
+  })
+
+  it("shows the payment details", () => {
+    const component = getWrapper()
+    const paymentSummary = component.find(
+      CreditCardSummaryItemFragmentContainer
+    )
+    expect(paymentSummary).toHaveLength(1)
+
+    expect(paymentSummary.text()).toMatchInlineSnapshot(`"•••• 4444  Exp 3/21"`)
+  })
+
+  it("shows the submit button", () => {
+    const component = getWrapper()
+    const continueButton = component.find(Button).last()
+    expect(continueButton.text()).toBe("Submit")
+  })
+
+  it("Shows the conditions of sale disclaimer.", () => {
+    const component = getWrapper()
+    expect(component.text()).toMatch(
+      "By clicking Submit, I agree to Artsy’s Conditions of Sale."
+    )
+  })
+})
---
+++ src/__generated__/Counter_order.graphql.ts
@@ -0,0 +1,148 @@
+/* tslint:disable */
+
+import { ConcreteFragment } from "relay-runtime";
+import { ArtworkSummaryItem_order$ref } from "./ArtworkSummaryItem_order.graphql";
+import { CreditCardSummaryItem_order$ref } from "./CreditCardSummaryItem_order.graphql";
+import { OfferHistoryItem_order$ref } from "./OfferHistoryItem_order.graphql";
+import { ShippingSummaryItem_order$ref } from "./ShippingSummaryItem_order.graphql";
+import { TransactionDetailsSummaryItem_order$ref } from "./TransactionDetailsSummaryItem_order.graphql";
+export type OrderModeEnum = "BUY" | "OFFER" | "%future added value";
+declare const _Counter_order$ref: unique symbol;
+export type Counter_order$ref = typeof _Counter_order$ref;
+export type Counter_order = {
+    readonly id: string | null;
+    readonly mode: OrderModeEnum | null;
+    readonly state: string | null;
+    readonly itemsTotal: string | null;
+    readonly totalListPrice: string | null;
+    readonly stateExpiresAt: string | null;
+    readonly lastOffer?: ({
+        readonly createdAt: string | null;
+    }) | null;
+    readonly " $fragmentRefs": TransactionDetailsSummaryItem_order$ref & ArtworkSummaryItem_order$ref & ShippingSummaryItem_order$ref & CreditCardSummaryItem_order$ref & OfferHistoryItem_order$ref;
+    readonly " $refType": Counter_order$ref;
+};
+
+
+
+const node: ConcreteFragment = (function(){
+var v0 = {
+  "kind": "ScalarField",
+  "alias": "__id",
+  "name": "id",
+  "args": null,
+  "storageKey": null
+},
+v1 = [
+  {
+    "kind": "Literal",
+    "name": "precision",
+    "value": 2,
+    "type": "Int"
+  }
+];
+return {
+  "kind": "Fragment",
+  "name": "Counter_order",
+  "type": "Order",
+  "metadata": null,
+  "argumentDefinitions": [],
+  "selections": [
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "id",
+      "args": null,
+      "storageKey": null
+    },
+    v0,
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "state",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "itemsTotal",
+      "args": v1,
+      "storageKey": "itemsTotal(precision:2)"
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "totalListPrice",
+      "args": v1,
+      "storageKey": "totalListPrice(precision:2)"
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "stateExpiresAt",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "mode",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "FragmentSpread",
+      "name": "TransactionDetailsSummaryItem_order",
+      "args": null
+    },
+    {
+      "kind": "FragmentSpread",
+      "name": "ArtworkSummaryItem_order",
+      "args": null
+    },
+    {
+      "kind": "FragmentSpread",
+      "name": "ShippingSummaryItem_order",
+      "args": null
+    },
+    {
+      "kind": "FragmentSpread",
+      "name": "CreditCardSummaryItem_order",
+      "args": null
+    },
+    {
+      "kind": "FragmentSpread",
+      "name": "OfferHistoryItem_order",
+      "args": null
+    },
+    {
+      "kind": "InlineFragment",
+      "type": "OfferOrder",
+      "selections": [
+        {
+          "kind": "LinkedField",
+          "alias": null,
+          "name": "lastOffer",
+          "storageKey": null,
+          "args": null,
+          "concreteType": "Offer",
+          "plural": false,
+          "selections": [
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "name": "createdAt",
+              "args": null,
+              "storageKey": null
+            },
+            v0
+          ]
+        }
+      ]
+    }
+  ]
+};
+})();
+(node as any).hash = '53059a64667f6767a97fbacce52642d3';
+export default node;
