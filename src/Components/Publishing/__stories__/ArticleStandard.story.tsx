d09c51535f7e7e36f3379304d6e4ccaf98c6350c
Lillan Pace <lillanpace@Lillan-Pace.local>
2018-12-18 18:14:41 -0500

    added more standard article stories

--- src/Components/Publishing/__stories__/ArticleStandard.story.tsx
+++ src/Components/Publishing/__stories__/ArticleStandard.story.tsx
@@ -1,11 +1,15 @@
 import { storiesOf } from "@storybook/react"
 import { Article } from "Components/Publishing/Article"
+import { clone, extend } from "lodash"
 import React from "react"
 
 import {
+  BasicArticle,
   ImageHeavyStandardArticle,
   MissingVerticalStandardArticle,
+  SeriesArticle,
   StandardArticle,
+  SuperArticle,
 } from "../Fixtures/Articles"
 
 import { ContextProvider } from "Artsy"
@@ -21,6 +25,31 @@ const story = storiesOf("Publishing/Articles/Standard", module)
           relatedArticlesForPanel={RelatedPanel}
           relatedArticlesForCanvas={RelatedCanvas}
         />
+      </ContextProvider>
+    )
+  })
+  .add("Super Article", () => {
+    return (
+      <ContextProvider>
+        <Article
+          article={StandardArticle}
+          relatedArticlesForPanel={RelatedPanel}
+          relatedArticlesForCanvas={RelatedCanvas}
+          isSuper
+        />
+      </ContextProvider>
+    )
+  })
+  .add("In series", () => {
+    const article = clone({
+      ...StandardArticle,
+      seriesArticle: SeriesArticle,
+      relatedArticles: [BasicArticle, SuperArticle],
+    } as ArticleData)
+
+    return (
+      <ContextProvider>
+        <Article article={article} />
       </ContextProvider>
     )
   })
