47caf73a76b33c8abb496f0188dc4c0c03bfe316
Devon Blandin <dblandin@gmail.com>
2019-01-09 10:13:08 -0500

    Update svg element attribute names for JSX
    JSX expectes camelCased attribute names so these should be translated
    from their non-JSX hypen-separated formats.
    
    ```
    fill-rule -> fillRule
    stroke-linecap -> strokeLinecap
    ```

--- src/Assets/Icons/Auction.tsx
+++ src/Assets/Icons/Auction.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Auction: React.SFC = () => {
   return (
     <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
-      <g stroke="#333" fill="none" fill-rule="evenodd">
+      <g stroke="#333" fill="none" fillRule="evenodd">
         <path d="M10.977 6.698l4.547 2.625-4.41 7.64-4.547-2.626zM13.385 13.243l4.546 2.625-.5.866-4.546-2.625z" />
       </g>
     </svg>
--- src/Assets/Icons/Solo.tsx
+++ src/Assets/Icons/Solo.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Solo: React.SFC = () => {
   return (
     <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fill-rule="evenodd">
+      <g fill="none" fillRule="evenodd">
         <path d="M0 0h25v25H0z" />
         <g transform="translate(7.294 4.941)" stroke="#333">
           <path d="M0 13.717V10.86a3 3 0 0 1 3-3h4.195a3 3 0 0 1 3 3v2.856" />
