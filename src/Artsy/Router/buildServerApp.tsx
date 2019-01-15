f2c833ac4b0067754c9eddbd8f866d2f82c631fc
Christopher Pappas <damassi.pappas@gmail.com>
2019-01-14 16:29:39 -0800

    Add collectSSRStyles util and update buildServerApp

--- src/Artsy/Router/buildServerApp.tsx
+++ src/Artsy/Router/buildServerApp.tsx
@@ -8,6 +8,7 @@ import { getLoadableState } from "loadable-components/server"
 import React, { ComponentType } from "react"
 import ReactDOMServer from "react-dom/server"
 import serialize from "serialize-javascript"
+import { collectSSRStyles } from "Utils/collectSSRStyles"
 import { getUser } from "Utils/getUser"
 import { createMediaStyle } from "Utils/Responsive"
 import { trace } from "Utils/trace"
@@ -23,6 +24,7 @@ interface Resolve {
   status?: number
   headTags?: any[]
   scripts?: string
+  styleTags?: string
 }
 
 // No need to invoke this for each request.
@@ -39,8 +41,7 @@ export function buildServerApp(config: ServerRouterConfig): Promise<Resolve> {
       try {
         const { context = {}, routes = [], url, userAgent } = config
         const user = getUser(context.user)
-        const relayEnvironment =
-          context.relayEnvironment || createRelaySSREnvironment({ user })
+        const relayEnvironment = context.relayEnvironment || createRelaySSREnvironment({ user }) // prettier-ignore
         const historyMiddlewares = [queryMiddleware]
         const resolver = new Resolver(relayEnvironment)
         const render = createRender({})
@@ -57,19 +58,12 @@ export function buildServerApp(config: ServerRouterConfig): Promise<Resolve> {
         )
 
         if (redirect) {
-          resolve({
-            redirect,
-            // TODO: The docs seem to indicate that if there’s a redirect there
-            //       will not be a status.
-            //       https://github.com/4Catalyzer/found#server-side-rendering
-            status,
-          })
+          resolve({ redirect })
           return
         }
 
         const headTags = [<style type="text/css">{MediaStyle}</style>]
-        const matchingMediaQueries =
-          userAgent && matchingMediaQueriesForUserAgent(userAgent)
+        const matchingMediaQueries = userAgent && matchingMediaQueriesForUserAgent(userAgent) // prettier-ignore
 
         const ServerApp = () => {
           return (
@@ -87,40 +81,36 @@ export function buildServerApp(config: ServerRouterConfig): Promise<Resolve> {
           )
         }
 
-        const { relayData, loadableState } = await trace(
+        const { loadableState, relayData: _relayData, styleTags } = await trace(
           "buildServerApp.fetch",
           (async () => {
-            // Kick off relay requests to prime cache
-            // TODO: Remove the need to do this by using persisted queries.
+            // Kick off relay requests to prime cache. TODO: Remove the need to
+            // do this by using persisted queries.
             ReactDOMServer.renderToString(<ServerApp />)
-            // Serializable data to be rehydrated on client
-            const data = await relayEnvironment.relaySSRMiddleware.getCache()
+            // Extract render queue for bundle split components using dyanamic `import()`
             const state = await getLoadableState(<ServerApp />)
-            return { relayData: data, loadableState: state }
+            // Extract CSS styleTags to inject for SSR pass
+            const tags = collectSSRStyles(<ServerApp />)
+            // Get serializable Relay data for rehydration on the client
+            const data = await relayEnvironment.relaySSRMiddleware.getCache()
+
+            return {
+              loadableState: state,
+              relayData: data,
+              styleTags: tags,
+            }
           })()
         )
 
-        /**
-         * FIXME: Relay SSR middleware is passing a _res object across which
-         * has circular references, leading to issues *ONLY* on staging / prod
-         * which can't be reproduced locally. This strips out _res as a quickfix
-         * though this should be PR'd back at relay-modern-network-modern-ssr.
-         */
-        try {
-          relayData.forEach(item => {
-            item.forEach(i => {
-              delete i._res
-            })
-          })
-        } catch (error) {
-          console.error(
-            "[Artsy/Router/buildServerApp] Error cleaning data",
-            error
-          )
+        // Strip response of problematic data structures
+        const relayData = cleanRelayData(_relayData)
+
+        // Build up script tags to inject into head
+        const scripts = []
+        if (loadableState) {
+          scripts.push(loadableState.getScriptTag())
         }
 
-        const scripts = []
-        loadableState && scripts.push(loadableState.getScriptTag())
         scripts.push(`
           <script>
             var __RELAY_BOOTSTRAP__ = ${serializeRelayData(relayData)};
@@ -131,6 +121,7 @@ export function buildServerApp(config: ServerRouterConfig): Promise<Resolve> {
           ServerApp,
           status,
           headTags,
+          styleTags,
           scripts: scripts.join("\n"),
         })
       } catch (error) {
@@ -141,6 +132,28 @@ export function buildServerApp(config: ServerRouterConfig): Promise<Resolve> {
   )
 }
 
+/**
+ * FIXME: Relay SSR middleware is passing a _res object across which
+ * has circular references, leading to issues *ONLY* on staging / prod
+ * which can't be reproduced locally. This strips out _res as a quickfix
+ * though this should be PR'd back at relay-modern-network-modern-ssr.
+ */
+function cleanRelayData(relayData: any) {
+  try {
+    relayData.forEach(item => {
+      item.forEach(i => {
+        delete i._res
+      })
+    })
+  } catch (error) {
+    console.error("[Artsy/Router/buildServerApp] Error cleaning data", error)
+  }
+
+  return relayData
+}
+/**
+ * Serialize data for client-side consumption
+ */
 function serializeRelayData(relayData: any) {
   let hydrationData
   try {
@@ -150,7 +163,7 @@ function serializeRelayData(relayData: any) {
   } catch (error) {
     hydrationData = "{}"
     console.error(
-      "reaction/Router/buildServerApp Error serializing data:",
+      "[Artsy/Router/buildServerApp] Error serializing data:",
       error
     )
   }
