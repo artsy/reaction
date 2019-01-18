87d91c43962af0fbce38a9c90fdac57a40fe7b51
Matt Dole <mdole7@gmail.com>
2019-01-18 16:43:51 -0500

    fixes image overlapping with text

--- src/Components/v2/AuctionCard.tsx
+++ src/Components/v2/AuctionCard.tsx
@@ -78,7 +78,7 @@ export const LargeAuctionCard = props => (
     </Serif>
     {props.src && (
       <Box height="200px">
-        <ResponsiveImage src={props.src} my={2} />
+        <ResponsiveImage src={props.src} my={2} pb="160px" />
       </Box>
     )}
     <Sans size="2" weight="medium">
