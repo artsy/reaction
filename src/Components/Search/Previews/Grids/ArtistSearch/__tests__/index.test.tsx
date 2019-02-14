d3609e6e1e1f9c7abb4461a658b531b29ae5873b
Steven Hicks <steven.j.hicks@gmail.com>
2019-02-14 15:42:49 -0600

    Reorganize ArtistSearchPreview tests

--- src/Components/Search/Previews/Grids/ArtistSearch/__tests__/index.test.tsx
+++ src/Components/Search/Previews/Grids/ArtistSearch/__tests__/index.test.tsx
@@ -33,104 +33,108 @@ const getWrapper = (viewer, breakpoint = "xl") => {
 }
 
 describe("ArtistSearchPreviewFragmentContainer", () => {
-  it("renders related artworks when there are no marketing collections for the artist", async () => {
-    const viewer = {
-      artist: {
-        id: "andy-warhol",
-        marketingCollections: [],
-      },
-      filter_artworks: {
-        artworks_connection: {
-          edges: [
-            {
-              node: {
-                artist_names: "Andy Warhol",
-                date: "yesterday",
-                href: "http://artsy.net/artwork/pretty-flower",
-                image: { cropped: { url: "http://path/to/image.jpg" } },
-                title: "Pretty Flower",
+  describe("an artist with no marketing collections", () => {
+    it("renders related artworks", async () => {
+      const viewer = {
+        artist: {
+          id: "andy-warhol",
+          marketingCollections: [],
+        },
+        filter_artworks: {
+          artworks_connection: {
+            edges: [
+              {
+                node: {
+                  artist_names: "Andy Warhol",
+                  date: "yesterday",
+                  href: "http://artsy.net/artwork/pretty-flower",
+                  image: { cropped: { url: "http://path/to/image.jpg" } },
+                  title: "Pretty Flower",
+                },
               },
+              {
+                node: {
+                  artist_names: "Andy Warhol",
+                  date: "tomorrow",
+                  href: "http://artsy.net/artwork/ugly-flower",
+                  image: { cropped: { url: "http://path/to/ugly-image.jpg" } },
+                  title: "Ugly Flower",
+                },
+              },
+            ],
+          },
+        },
+      }
+
+      const wrapper = await getWrapper(viewer)
+
+      expect(wrapper.find(MarketingCollectionsPreview).length).toEqual(0)
+      expect(wrapper.find(RelatedArtworksPreview).length).toEqual(1)
+      expect(wrapper.find(PreviewGridItem).length).toEqual(2)
+    })
+  })
+
+  describe("an artist with marketing collections", () => {
+    it("renders marketing collections", async () => {
+      const viewer = {
+        artist: {
+          id: "andy-warhol",
+          marketingCollections: [
+            {
+              headerImage: "http://path/to/image.jpg",
+              slug: "flowers-in-spring",
+              title: "Flowers in spring",
             },
             {
-              node: {
-                artist_names: "Andy Warhol",
-                date: "tomorrow",
-                href: "http://artsy.net/artwork/ugly-flower",
-                image: { cropped: { url: "http://path/to/ugly-image.jpg" } },
-                title: "Ugly Flower",
-              },
+              headerImage: "http://path/to/another-image.jpg",
+              slug: "shovels-in-winter",
+              title: "Shovels in winter",
             },
           ],
         },
-      },
-    }
-
-    const wrapper = await getWrapper(viewer)
-
-    expect(wrapper.find(MarketingCollectionsPreview).length).toEqual(0)
-    expect(wrapper.find(RelatedArtworksPreview).length).toEqual(1)
-    expect(wrapper.find(PreviewGridItem).length).toEqual(2)
-  })
-
-  it("renders marketing collections when there are marketing collections for the artist", async () => {
-    const viewer = {
-      artist: {
-        id: "andy-warhol",
-        marketingCollections: [
-          {
-            headerImage: "http://path/to/image.jpg",
-            slug: "flowers-in-spring",
-            title: "Flowers in spring",
+        filter_artworks: {
+          artworks_connection: {
+            edges: [],
           },
-          {
-            headerImage: "http://path/to/another-image.jpg",
-            slug: "shovels-in-winter",
-            title: "Shovels in winter",
-          },
-        ],
-      },
-      filter_artworks: {
-        artworks_connection: {
-          edges: [],
         },
-      },
-    }
+      }
 
-    const wrapper = await getWrapper(viewer)
+      const wrapper = await getWrapper(viewer)
 
-    expect(wrapper.find(RelatedArtworksPreview).length).toEqual(0)
-    expect(wrapper.find(MarketingCollectionsPreview).length).toEqual(1)
-  })
+      expect(wrapper.find(RelatedArtworksPreview).length).toEqual(0)
+      expect(wrapper.find(MarketingCollectionsPreview).length).toEqual(1)
+    })
 
-  it("includes links to collections", async () => {
-    const viewer = {
-      artist: {
-        id: "andy-warhol",
-        marketingCollections: [
-          {
-            title: "Andy Warhol: Prettiest Flowers",
-            slug: "andy-warhol-prettiest-flowers",
-            headerImage: "http://path/to/image.jpg",
-          },
-          {
-            title: "Andy Warhol: Soup Cans Yo!",
-            slug: "andy-warhol-soup-cans-yo",
-            headerImage: "http://path/to/another-image.jpg",
-          },
-        ],
-      },
-      filter_artworks: {
-        artworks_connection: {
-          edges: [],
+    it("includes links to collections", async () => {
+      const viewer = {
+        artist: {
+          id: "andy-warhol",
+          marketingCollections: [
+            {
+              title: "Andy Warhol: Prettiest Flowers",
+              slug: "andy-warhol-prettiest-flowers",
+              headerImage: "http://path/to/image.jpg",
+            },
+            {
+              title: "Andy Warhol: Soup Cans Yo!",
+              slug: "andy-warhol-soup-cans-yo",
+              headerImage: "http://path/to/another-image.jpg",
+            },
+          ],
         },
-      },
-    }
+        filter_artworks: {
+          artworks_connection: {
+            edges: [],
+          },
+        },
+      }
 
-    const component = await getWrapper(viewer)
-    const hrefs = component.find("a").map(node => node.props().href)
-    const expected = viewer.artist.marketingCollections.map(
-      collection => `collection/${collection.slug}`
-    )
-    expect(hrefs).toEqual(expected)
+      const component = await getWrapper(viewer)
+      const hrefs = component.find("a").map(node => node.props().href)
+      const expected = viewer.artist.marketingCollections.map(
+        collection => `collection/${collection.slug}`
+      )
+      expect(hrefs).toEqual(expected)
+    })
   })
 })
