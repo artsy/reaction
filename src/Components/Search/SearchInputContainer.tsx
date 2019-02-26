6946ca5a5108060bac37c63d738e290f6f7792b8
Jon Allured <jon.allured@gmail.com>
2019-02-26 09:27:45 -0600

    Fix some oversights
    h/t @pepopowitz

--- src/Components/Search/SearchInputContainer.tsx
+++ src/Components/Search/SearchInputContainer.tsx
@@ -20,7 +20,7 @@ const SearchButton = styled.button`
     border-radius: 50%;
 
     svg > path {
-      fill: #fff;
+      fill: ${color("white100")};
     }
   }
 `
@@ -31,10 +31,11 @@ export const SearchInputContainer = props => {
       <Input style={{ width: "100%" }} {...props} />
       <SearchButton>
         <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg">
+          <title>search</title>
           <path
             d="M11.5 3a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7zm0-1A4.5 4.5 0 1 0 16 6.5 4.49 4.49 0 0 0 11.5 2zM9.442 9.525l-.88-.88L2.06 15.06l.88.88 6.502-6.415z"
             fill="#000"
-            fill-rule="nonzero"
+            fillRule="nonzero"
           />
         </svg>
       </SearchButton>
