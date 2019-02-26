3ac38e325ddef48e01e7c6451df611f086b87dd5
Steven Hicks <steven.j.hicks@gmail.com>
2019-02-26 11:23:38 -0600

    Extend test coverage of search previews
    Co-authored-by: Jon Allured <jon.allured@gmail.com>

--- src/Components/Search/Previews/Grids/ArtistSearch/__tests__/index.test.tsx
+++ src/Components/Search/Previews/Grids/ArtistSearch/__tests__/index.test.tsx
@@ -37,45 +37,54 @@ const getWrapper = (viewer, breakpoint = "xl") => {
   })
 }
 
+const buildArtworksConnection: any = (numberOfItems: number) => {
+  const edges = Array(numberOfItems)
+    .fill(1)
+    .map((_, i) => ({
+      node: buildNode(i),
+    }))
+
+  return {
+    edges,
+  }
+}
+
+const buildNode: any = (nodeIndex: number) => ({
+  artist_names: "Andy Warhol",
+  date: "tomorrow",
+  href: `http://artsy.net/artwork/ugly-flower-${nodeIndex}`,
+  image: {
+    cropped: { url: "http://path/to/ugly-image.jpg" },
+  },
+  title: `Ugly Flower-${nodeIndex}`,
+})
+
 describe("ArtistSearchPreviewFragmentContainer", () => {
   describe("an artist with no marketing collections", () => {
-    it("renders related artworks", async () => {
-      const viewer = {
-        artist: {
-          id: "andy-warhol",
-          marketingCollections: [],
-        },
-        filter_artworks: {
-          artworks_connection: {
-            edges: [
-              {
-                node: {
-                  artist_names: "Andy Warhol",
-                  date: "yesterday",
-                  href: "http://artsy.net/artwork/pretty-flower",
-                  image: { cropped: { url: "http://path/to/image.jpg" } },
-                  title: "Pretty Flower",
-                },
-              },
-              {
-                node: {
-                  artist_names: "Andy Warhol",
-                  date: "tomorrow",
-                  href: "http://artsy.net/artwork/ugly-flower",
-                  image: { cropped: { url: "http://path/to/ugly-image.jpg" } },
-                  title: "Ugly Flower",
-                },
-              },
-            ],
+    describe("an artist with related artworks", () => {
+      it("renders 10 related artworks at lg", async () => {
+        const viewer = {
+          artist: {
+            id: "andy-warhol",
+            marketingCollections: [],
           },
-        },
-      }
+          filter_artworks: {
+            artworks_connection: buildArtworksConnection(10),
+          },
+        }
 
-      const wrapper = await getWrapper(viewer)
+        const wrapper = await getWrapper(viewer)
 
-      expect(wrapper.find(MarketingCollectionsPreview).length).toEqual(0)
-      expect(wrapper.find(RelatedArtworksPreview).length).toEqual(1)
-      expect(wrapper.find(PreviewGridItem).length).toEqual(2)
+        expect(wrapper.find(MarketingCollectionsPreview).length).toEqual(0)
+        expect(wrapper.find(RelatedArtworksPreview).length).toEqual(1)
+        expect(wrapper.find(PreviewGridItem).length).toEqual(10)
+      })
+
+      // it("renders 5 artworks at md", async() => {})
+
+      describe("an artist with no related artworks", () => {
+        // it("renders an empty preview", async() => {})
+      })
     })
   })
 
