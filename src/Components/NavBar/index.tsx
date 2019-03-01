a175749e9980471fd04740494f7406a5f58b551a
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-28 20:22:31 -0800

    Add Menu layout and toggle

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -1,10 +1,11 @@
-import React, { Reducer, useReducer, useState } from "react"
+import React, { useReducer, useState } from "react"
 import styled from "styled-components"
 
 import { SearchBarQueryRenderer as SearchBar } from "Components/Search/SearchBar"
 
 import {
   ArtsyMarkIcon,
+  BorderBox,
   Box,
   BoxProps,
   Button,
@@ -13,6 +14,7 @@ import {
   Link,
   MoreIcon,
   Sans,
+  Separator,
   Spacer,
 } from "@artsy/palette"
 
@@ -22,12 +24,92 @@ const NavSection = ({ children, ...props }) => (
   </Flex>
 )
 
-const NavItem: React.SFC<BoxProps> = ({ children, display = "block" }) => {
+interface NavItemProps extends BoxProps {
+  href?: string
+  menu?: any
+  onClick?: () => void
+}
+
+const MenuItem = ({ children }) => {
+  const [hover, toggleHover] = useState(false)
+
   return (
-    <Box p={1} display={display}>
+    <Box
+      bg={hover ? "black5" : ""}
+      style={{ cursor: "pointer" }}
+      onMouseOver={() => toggleHover(true)}
+      onMouseOut={() => toggleHover(false)}
+    >
+      <Box px={2} py={1}>
+        <Sans size="2" weight="medium">
+          <Link
+            href="/"
+            style={{
+              textDecoration: hover ? "underline" : "none",
+            }}
+          >
+            {children}
+          </Link>
+        </Sans>
+      </Box>
+    </Box>
+  )
+}
+
+const MenuContainer = styled(Box)`
+  position: absolute;
+  background-color: white;
+  transform: translate(-80%);
+  box-shadow: 2px 2px ${color("black5")};
+`
+
+const Menu = ({ title, children }) => {
+  return (
+    <MenuContainer mt={2} width={230}>
+      <BorderBox p={0}>
+        <Flex flexDirection="column" width="100%">
+          {title && (
+            <Box px={2} py={1}>
+              <Sans size="3" weight="medium">
+                {title}
+              </Sans>
+              <Spacer py={0.5} />
+              <Separator />
+            </Box>
+          )}
+
+          {children}
+        </Flex>
+      </BorderBox>
+    </MenuContainer>
+  )
+}
+
+const NavMenu = () => {
+  return (
+    <Menu title="More">
+      <MenuItem>Artists</MenuItem>
+      <MenuItem>Shows</MenuItem>
+      <MenuItem>Museums</MenuItem>
+      <MenuItem>Artsy for Galleries</MenuItem>
+    </Menu>
+  )
+}
+
+const NavItem: React.SFC<NavItemProps> = ({
+  display = "block",
+  children,
+  href,
+  menu,
+  onClick,
+}) => {
+  return (
+    <Box p={1} display={display} position="relative" onClick={onClick}>
       <Sans size="3" weight="medium">
-        <Link href="/">{children}</Link>
+        {href ? <Link href="/">{children}</Link> : children}
       </Sans>
+
+      {menu}
     </Box>
   )
 }
@@ -65,8 +147,6 @@ export const NavBar = () => {
     showNotificationsMenu: false,
   })
 
-  console.log(navState)
-
   return (
     <Container mt={1} p={1}>
       <NavSection>
@@ -83,15 +163,24 @@ export const NavBar = () => {
 
       <Spacer mr={3} />
 
-      {/* Desktop */}
+      {/*
+        Desktop
+      */}
       <NavSection display={["none", "flex"]}>
         <NavSection>
           <NavItem>Artworks</NavItem>
           <NavItem>Auctions</NavItem>
+
+          {/**
+            Only show Galleries and Fairs at `lg` and `xl`
+          */}
           <NavItem display={["none", "none", "block"]}>Galleries</NavItem>
           <NavItem display={["none", "none", "none", "block"]}>Fairs</NavItem>
           <NavItem>Magazine</NavItem>
-          <NavItem>
+          <NavItem
+            menu={navState.showNavMenu && <NavMenu />}
+            onClick={() => dispatch({ type: navActions.TOGGLE_NAV_MENU })}
+          >
             <MoreIcon top="3px" />
           </NavItem>
         </NavSection>
@@ -105,7 +194,9 @@ export const NavBar = () => {
         </NavSection>
       </NavSection>
 
-      {/* Mobile */}
+      {/*
+        Mobile
+      */}
       <NavSection display={["flex", "none"]}>Click me</NavSection>
     </Container>
   )
