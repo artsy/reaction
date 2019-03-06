845372f31468dccd77c3e004858a511cb3961122
Jon Allured <jon.allured@gmail.com>
2019-03-06 08:17:15 -0600

    Remove TODOs
    Todos like these are lies we tell ourselves. This code is in git and can
    be returned to at any time - no need to comment it out.

--- src/Artsy/Relay/renderWithLoadProgress.tsx
+++ src/Artsy/Relay/renderWithLoadProgress.tsx
@@ -21,10 +21,6 @@ export function renderWithLoadProgress<P>(
   Container: RelayContainer<P>,
   initialProps: object = {}
 ): (readyState: ReadyState<P>) => React.ReactElement<RelayContainer<P>> | null {
-  // TODO: We need design for retry-ing, or the go-ahead to re-use the design of
-  //       the iOS app.
-  //
-  // let retrying = false
   return ({ error, props, retry }) => {
     if (error) {
       // In tests we want errors to clearly bubble up.
@@ -47,18 +43,6 @@ export function renderWithLoadProgress<P>(
         }
       }
 
-      // if (retrying) {
-      //   retrying = false
-      //   // TODO: Even though this code path is reached, the retry button keeps spinning. iirc it _should_ disappear when
-      //   //      `onRetry` on the instance is unset.
-      //   //
-      //   // This will re-use the native view first created in the renderFailure callback, which means it can
-      //   // continue its ‘retry’ animation.
-      //   return <LoadFailureView style={{ flex: 1 }} />
-      // } else {
-      //   retrying = true
-      //   return <LoadFailureView onRetry={retry} style={{ flex: 1 }} />
-      // }
       return null
     } else if (props) {
       return <Container {...initialProps} {...props as any} />
