7aa97d799b437d64f4a8b07e69af230d338392d3
Matthew Zikherman <mzikherman@gmail.com>
2019-03-21 13:34:39 -0400

    [SearchResults] Add zero state to categories tab

--- src/Apps/Search/Routes/Categories/SearchResultsCategories.tsx
+++ src/Apps/Search/Routes/Categories/SearchResultsCategories.tsx
@@ -1,8 +1,10 @@
 import { Box, Separator, Spacer } from "@artsy/palette"
 import { SearchResultsCategories_viewer } from "__generated__/SearchResultsCategories_viewer.graphql"
 import { GenericSearchResultItem } from "Apps/Search/Components/GenericSearchResultItem"
+import { ZeroState } from "Apps/Search/Components/ZeroState"
 import { PaginationFragmentContainer as Pagination } from "Components/v2"
 import { LoadingArea, LoadingAreaState } from "Components/v2/LoadingArea"
+import { Location } from "found"
 import React from "react"
 import { createRefetchContainer, graphql, RelayRefetchProp } from "react-relay"
 import { get } from "Utils/get"
@@ -10,6 +12,7 @@ import { get } from "Utils/get"
 export interface Props {
   viewer: SearchResultsCategories_viewer
   relay: RelayRefetchProp
+  location: Location
 }
 
 const PAGE_SIZE = 10
@@ -62,13 +65,13 @@ export class SearchResultCategoriesRoute extends React.Component<
     )
   }
 
-  render() {
+  renderCategories() {
     const { viewer } = this.props
     const { search: searchConnection } = viewer
 
     const genes = get(viewer, v => v.search.edges, []).map(e => e.node)
     return (
-      <LoadingArea isLoading={this.state.isLoading}>
+      <>
         {genes.map((gene, index) => {
           return (
             <Box key={index}>
@@ -98,6 +101,22 @@ export class SearchResultCategoriesRoute extends React.Component<
           scrollTo="#jumpto--searchResultTabs"
           hasNextPage={searchConnection.pageInfo.hasNextPage}
         />
+      </>
+    )
+  }
+
+  render() {
+    const { viewer, location } = this.props
+    const { term } = get(location, l => l.query)
+
+    const genes = get(viewer, v => v.search.edges, []).map(e => e.node)
+    return (
+      <LoadingArea isLoading={this.state.isLoading}>
+        {genes.length === 0 ? (
+          <ZeroState entity="categories" term={term} />
+        ) : (
+          this.renderCategories()
+        )}
       </LoadingArea>
     )
   }
