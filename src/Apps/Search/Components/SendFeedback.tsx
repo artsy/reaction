fe2e733f65b55d934ac4e22a650fe2f7c89f813f
Matthew Zikherman <mzikherman@gmail.com>
2019-03-21 19:19:11 -0400

    [SearchResults] Add name/email inputs and allow feedback for logged out users

--- src/Apps/Search/Components/SendFeedback.tsx
+++ src/Apps/Search/Components/SendFeedback.tsx
@@ -2,65 +2,108 @@ import { Box, Button, color, Flex, Serif, TextArea } from "@artsy/palette"
 import { SendFeedbackSearchResultsMutation } from "__generated__/SendFeedbackSearchResultsMutation.graphql"
 import { ContextProps } from "Artsy"
 import { withContext } from "Artsy/SystemContext"
+import Input from "Components/Input"
 import React from "react"
 import { commitMutation, graphql } from "react-relay"
 
 interface State {
   submitted: boolean
   message: string
+  name: string
+  email: string
 }
 
 class SendFeedbackForm extends React.Component<ContextProps, State> {
   state = {
     submitted: false,
     message: null,
+    name: null,
+    email: null,
   }
 
   handleClick() {
     const { user, relayEnvironment } = this.props
-    const { message } = this.state
+    const { message, name, email } = this.state
     if (!message) return
-    if (user && user.id) {
-      commitMutation<SendFeedbackSearchResultsMutation>(relayEnvironment, {
-        mutation: graphql`
-          mutation SendFeedbackSearchResultsMutation(
-            $input: SendFeedbackMutationInput!
-          ) {
-            sendFeedback(input: $input) {
-              feedbackOrError {
-                ... on SendFeedbackMutationSuccess {
-                  feedback {
-                    message
-                  }
+    if (!user && !name && !email) return
+
+    commitMutation<SendFeedbackSearchResultsMutation>(relayEnvironment, {
+      mutation: graphql`
+        mutation SendFeedbackSearchResultsMutation(
+          $input: SendFeedbackMutationInput!
+        ) {
+          sendFeedback(input: $input) {
+            feedbackOrError {
+              ... on SendFeedbackMutationSuccess {
+                feedback {
+                  message
                 }
-                ... on SendFeedbackMutationFailure {
-                  mutationError {
-                    type
-                    message
-                    detail
-                  }
+              }
+              ... on SendFeedbackMutationFailure {
+                mutationError {
+                  type
+                  message
+                  detail
                 }
               }
             }
           }
-        `,
-        variables: {
-          input: { message },
-        },
-        optimisticUpdater: () => {
-          this.setState({
-            submitted: true,
-          })
-        },
-      })
-    }
+        }
+      `,
+      variables: {
+        input: { message, email, name },
+      },
+      optimisticUpdater: () => {
+        this.setState({
+          submitted: true,
+        })
+      },
+    })
+  }
+
+  renderPersonalInfoInputs() {
+    return (
+      <Flex mt={2} width="484px">
+        <Box width="237px" mr={1}>
+          <Input
+            placeholder="Your name"
+            onChange={({ currentTarget: { value: name } }) => {
+              this.setState({ name })
+            }}
+          />
+        </Box>
+        <Box width="237px">
+          <Input
+            placeholder="Email address"
+            onChange={({ currentTarget: { value: email } }) => {
+              this.setState({ email })
+            }}
+          />
+        </Box>
+      </Flex>
+    )
+  }
+
+  renderFeedbackTextArea() {
+    return (
+      <Box mt={2} width="484px">
+        <TextArea
+          onChange={({ value: message }) => {
+            this.setState({ message })
+          }}
+          placeholder="Your comments here"
+        />
+      </Box>
+    )
   }
 
   render() {
+    const { user } = this.props
+
     return (
       <Box bg={color("black5")} p={3} mt={3}>
         <Flex
-          height="212px"
+          style={{ minHeight: "212px" }}
           flexDirection="column"
           alignItems="center"
           justifyContent="center"
@@ -75,14 +118,8 @@ class SendFeedbackForm extends React.Component<ContextProps, State> {
               for.
             </Serif>
           </Box>
-          <Box mt={2} width="484px">
-            <TextArea
-              onChange={({ value: message }) => {
-                this.setState({ message })
-              }}
-              placeholder="Your comments here"
-            />
-          </Box>
+          {!user ? this.renderPersonalInfoInputs() : null}
+          {this.renderFeedbackTextArea()}
           <Button
             onClick={() => {
               this.handleClick()
