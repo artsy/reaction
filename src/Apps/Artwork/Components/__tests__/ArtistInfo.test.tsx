f75f40cf5327d95cc873ef092d09f13beda47792
Lily Pacegit <lilyfpace@gmail.com>
2019-03-25 12:17:33 -0400

    added test

--- src/Apps/Artwork/Components/__tests__/ArtistInfo.test.tsx
+++ src/Apps/Artwork/Components/__tests__/ArtistInfo.test.tsx
@@ -1,5 +1,6 @@
 import { ArtistInfoFixture } from "Apps/__tests__/Fixtures/Artwork/ArtistInfo"
 import { ArtistInfoFragmentContainer } from "Apps/Artwork/Components/ArtistInfo"
+import { track } from "Artsy/Analytics"
 import { MockBoot, renderRelayTree } from "DevTools"
 import { cloneDeep } from "lodash"
 import React from "react"
@@ -39,46 +40,64 @@ describe("ArtistInfo", () => {
       expect(wrapper.find("MarketInsights").length).toBe(0)
       expect(wrapper.find("SelectedExhibitions").length).toBe(0)
     })
+
+    it("shows artist insights when the 'Show artist insights' button is clicked", () => {
+      wrapper.find("Button").simulate("click")
+      expect(wrapper.find("MarketInsights").length).toBe(1)
+      expect(wrapper.find("SelectedExhibitions").length).toBe(1)
+    })
   })
 
-  it("shows artist insights when the 'Show artist insights' button is clicked", () => {
-    wrapper.find("Button").simulate("click")
-    expect(wrapper.find("MarketInsights").length).toBe(1)
-    expect(wrapper.find("SelectedExhibitions").length).toBe(1)
+  describe("ArtistInfo for artwork with incomplete artist info", () => {
+    it("Hides 'Show artist insights' button if no market insights or selected exhibitions data", async () => {
+      const data = cloneDeep(ArtistInfoFixture)
+      data.highlights.partners = null
+      data.collections = null
+      data.auctionResults = null
+      data.exhibition_highlights = null
+      wrapper = await getWrapper(data)
+      expect(wrapper.find("Button").length).toBe(0)
+    })
+
+    it("hides ArtistBio if no data", async () => {
+      const data = cloneDeep(ArtistInfoFixture)
+      data.biography_blurb.text = null
+      wrapper = await getWrapper(data)
+      expect(wrapper.find("ArtistBio").length).toBe(0)
+    })
+
+    it("hides MarketInsights if no data", async () => {
+      const data = cloneDeep(ArtistInfoFixture)
+      data.highlights.partners = null
+      data.collections = null
+      data.auctionResults = null
+      wrapper = await getWrapper(data)
+      wrapper.find("Button").simulate("click")
+      expect(wrapper.find("MarketInsights").html()).toBe(null)
+    })
+
+    it("hides SelectedExhibitions if no data", async () => {
+      const data = cloneDeep(ArtistInfoFixture)
+      data.exhibition_highlights = []
+      wrapper = await getWrapper(data)
+      wrapper.find("Button").simulate("click")
+      expect(wrapper.find("SelectedExhibitions").html()).toBe(null)
+    })
   })
 
-  it("Hides 'Show artist insights' button if no market insights or selected exhibitions data", async () => {
-    const data = cloneDeep(ArtistInfoFixture)
-    data.highlights.partners = null
-    data.collections = null
-    data.auctionResults = null
-    data.exhibition_highlights = null
-    wrapper = await getWrapper(data)
-    expect(wrapper.find("Button").length).toBe(0)
-  })
-
-  it("hides ArtistBio if no data", async () => {
-    const data = cloneDeep(ArtistInfoFixture)
-    data.biography_blurb.text = null
-    wrapper = await getWrapper(data)
-    expect(wrapper.find("ArtistBio").length).toBe(0)
-  })
-
-  it("hides MarketInsights if no data", async () => {
-    const data = cloneDeep(ArtistInfoFixture)
-    data.highlights.partners = null
-    data.collections = null
-    data.auctionResults = null
-    wrapper = await getWrapper(data)
-    wrapper.find("Button").simulate("click")
-    expect(wrapper.find("MarketInsights").html()).toBe(null)
-  })
-
-  it("hides SelectedExhibitions if no data", async () => {
-    const data = cloneDeep(ArtistInfoFixture)
-    data.exhibition_highlights = []
-    wrapper = await getWrapper(data)
-    wrapper.find("Button").simulate("click")
-    expect(wrapper.find("SelectedExhibitions").html()).toBe(null)
+  describe("Analytics", () => {
+    beforeAll(async () => {
+      wrapper = await getWrapper()
+    })
+    it("tracks click on 'Artist Insights'", () => {
+      const button = wrapper.find("Button")
+      button.simulate("click")
+      expect(track).toBeCalledWith({
+        action_type: "Click",
+        subject: "Show artist insights",
+        flow: "Artwork about the artist",
+        type: "Button",
+      })
+    })
   })
 })
