fc97b1130c346c6ce4125a5453fa34f301ee5a98
Lily Pacegit <lilyfpace@gmail.com>
2019-03-27 14:10:57 -0400

    Fix spec and switch to mockTracking

--- src/Apps/Artwork/Components/__tests__/ArtistInfo.test.tsx
+++ src/Apps/Artwork/Components/__tests__/ArtistInfo.test.tsx
@@ -1,99 +1,90 @@
 import { ArtistInfoFixture } from "Apps/__tests__/Fixtures/Artwork/ArtistInfo"
-import { ArtistInfoFragmentContainer } from "Apps/Artwork/Components/ArtistInfo"
-import { track } from "Artsy/Analytics"
-import { MockBoot, renderRelayTree } from "DevTools"
+import { mockTracking } from "Artsy/Analytics"
+import { mount } from "enzyme"
 import { cloneDeep } from "lodash"
 import React from "react"
-import { graphql } from "react-relay"
+import { ArtistInfo } from "../ArtistInfo"
 
-jest.unmock("react-relay")
+jest.unmock("react-tracking")
 
 describe("ArtistInfo", () => {
-  const getWrapper = async (response = ArtistInfoFixture) => {
-    return await renderRelayTree({
-      Component: ArtistInfoFragmentContainer,
-      query: graphql`
-        query ArtistInfo_Test_Query {
-          artist(id: "pablo-picasso") {
-            ...ArtistInfo_artist
-          }
-        }
-      `,
-      wrapper: n => <MockBoot breakpoint="xs">{n}</MockBoot>,
-      mockResolvers: {
-        Artist: () => response,
-      },
-    })
+  const getWrapper = props => {
+    return mount(<ArtistInfo {...props} />)
   }
-  let wrapper
+
+  let testProps
+  beforeEach(() => {
+    testProps = { artist: ArtistInfoFixture }
+  })
 
   describe("ArtistInfo for artwork with complete artist info", () => {
-    beforeAll(async () => {
-      wrapper = await getWrapper()
-    })
-
     it("renders a correct component tree", () => {
-      expect(wrapper.find("EntityHeader").length).toBe(1)
-      expect(wrapper.find("ArtistBio").length).toBe(1)
-      expect(wrapper.find("Button").length).toBe(1)
-      expect(wrapper.find("Button").text()).toEqual("Show artist insights")
-      expect(wrapper.find("MarketInsights").length).toBe(0)
-      expect(wrapper.find("SelectedExhibitions").length).toBe(0)
+      const component = getWrapper(testProps)
+      expect(component.find("EntityHeader").length).toBe(1)
+      expect(component.find("ArtistBio").length).toBe(1)
+      expect(component.find("Button").length).toBe(1)
+      expect(component.find("Button").text()).toEqual("Show artist insights")
+      expect(component.find("MarketInsights").length).toBe(0)
+      expect(component.find("SelectedExhibitions").length).toBe(0)
     })
 
     it("shows artist insights when the 'Show artist insights' button is clicked", () => {
-      wrapper.find("Button").simulate("click")
-      expect(wrapper.find("MarketInsights").length).toBe(1)
-      expect(wrapper.find("SelectedExhibitions").length).toBe(1)
+      const component = getWrapper(testProps)
+      component.find("Button").simulate("click")
+      expect(component.find("MarketInsights").length).toBe(1)
+      expect(component.find("SelectedExhibitions").length).toBe(1)
     })
   })
 
   describe("ArtistInfo for artwork with incomplete artist info", () => {
     it("Hides 'Show artist insights' button if no market insights or selected exhibitions data", async () => {
-      const data = cloneDeep(ArtistInfoFixture)
-      data.highlights.partners = null
-      data.collections = null
-      data.auctionResults = null
-      data.exhibition_highlights = null
-      wrapper = await getWrapper(data)
-      expect(wrapper.find("Button").length).toBe(0)
+      const artist = cloneDeep(ArtistInfoFixture)
+      artist.highlights.partners = null
+      artist.collections = null
+      artist.auctionResults = null
+      artist.exhibition_highlights = null
+      const component = getWrapper({ artist })
+      expect(component.find("Button").length).toBe(0)
     })
 
     it("hides ArtistBio if no data", async () => {
-      const data = cloneDeep(ArtistInfoFixture)
-      data.biography_blurb.text = null
-      wrapper = await getWrapper(data)
-      expect(wrapper.find("ArtistBio").length).toBe(0)
+      const artist = cloneDeep(ArtistInfoFixture)
+      artist.biography_blurb.text = null
+      const component = getWrapper({ artist })
+      expect(component.find("ArtistBio").length).toBe(0)
     })
 
     it("hides MarketInsights if no data", async () => {
-      const data = cloneDeep(ArtistInfoFixture)
-      data.highlights.partners = null
-      data.collections = null
-      data.auctionResults = null
-      wrapper = await getWrapper(data)
-      wrapper.find("Button").simulate("click")
-      expect(wrapper.find("MarketInsights").html()).toBe(null)
+      const artist = cloneDeep(ArtistInfoFixture)
+      artist.highlights.partners = null
+      artist.collections = null
+      artist.auctionResults = null
+      const component = getWrapper({ artist })
+      component.find("Button").simulate("click")
+      expect(component.find("MarketInsights").html()).toBe(null)
     })
 
     it("hides SelectedExhibitions if no data", async () => {
-      const data = cloneDeep(ArtistInfoFixture)
-      data.exhibition_highlights = []
-      wrapper = await getWrapper(data)
-      wrapper.find("Button").simulate("click")
-      expect(wrapper.find("SelectedExhibitions").html()).toBe(null)
+      const artist = cloneDeep(ArtistInfoFixture)
+      artist.exhibition_highlights = []
+      const component = getWrapper({ artist })
+      component.find("Button").simulate("click")
+      expect(component.find("SelectedExhibitions").html()).toBe(null)
     })
   })
 
   describe("Analytics", () => {
-    beforeAll(async () => {
-      wrapper = await getWrapper()
-    })
     it("tracks click on 'Show artist insights' button", () => {
-      const button = wrapper.find("Button")
+      const user = {}
+      const { Component, dispatch } = mockTracking(ArtistInfo)
+      const component = mount(
+        <Component user={user} artist={ArtistInfoFixture} />
+      )
+      const button = component.find("Button")
       button.simulate("click")
-      expect(track).toBeCalledWith({
+      expect(dispatch).toBeCalledWith({
         action_type: "Click",
+        context_module: "Biography",
         subject: "Show artist insights",
         flow: "Artwork about the artist",
         type: "Button",
