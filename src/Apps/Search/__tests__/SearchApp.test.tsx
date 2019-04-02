59d61d27166e7b8e7d8e099b0434b82ac159afc5
Matthew Zikherman <mzikherman@gmail.com>
2019-04-02 10:58:33 -0400

    [Search] Make sure to get artwork tab count from filter endpoint aggregation

--- src/Apps/Search/__tests__/SearchApp.test.tsx
+++ src/Apps/Search/__tests__/SearchApp.test.tsx
@@ -34,16 +34,20 @@ describe("SearchApp", () => {
     },
     viewer: {
       search: {
-        totalCount: 420,
         aggregations: [
           {
             slice: "TYPE",
             counts: [
-              { name: "artwork", count: 100 },
+              { name: "PartnerGallery", count: 100 },
               { name: "artist", count: 320 },
             ],
           },
         ],
+      },
+      filter_artworks: {
+        counts: {
+          total: 100,
+        },
       },
     },
   }
@@ -51,7 +55,7 @@ describe("SearchApp", () => {
   it("includes the total count", () => {
     const wrapper = getWrapper(props) as any
     const html = wrapper.html()
-    expect(html).toContain('420 Results for "andy"')
+    expect(html).toContain('520 Results for "andy"')
   })
 
   it("includes tabs w/ counts", () => {
@@ -59,5 +63,6 @@ describe("SearchApp", () => {
     const html = wrapper.html()
     expect(html).toContain("Artworks 100")
     expect(html).toContain("Artists 320")
+    expect(html).toContain("Galleries 100")
   })
 })
