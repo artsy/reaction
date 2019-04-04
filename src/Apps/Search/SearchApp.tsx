510437d70ddbb6cc2a9919164a61f5c02a336bf6
Matthew Zikherman <mzikherman@gmail.com>
2019-04-04 19:17:50 -0400

    [SearchResults] Tweak color and background when hovering over items

--- src/Apps/Search/SearchApp.tsx
+++ src/Apps/Search/SearchApp.tsx
@@ -65,7 +65,6 @@ export class SearchApp extends React.Component<Props> {
                 term={term}
                 searchableConnection={search}
               />
-              <Spacer mb={3} />
               <Box minHeight="30vh">{this.props.children}</Box>
             </Col>
           </Row>
