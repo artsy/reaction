dd2fa7d955f51008b7c96a2d3ea8de11642279e0
David Sheldrick <djsheldrick@gmail.com>
2019-04-05 15:19:19 +0200

    fix CreditCardDetails

--- src/Apps/Order/Components/CreditCardDetails.tsx
+++ src/Apps/Order/Components/CreditCardDetails.tsx
@@ -14,14 +14,19 @@ export const CreditCardDetails = ({
   expiration_year: number
 }) => (
   <Flex alignItems="center">
-    <CreditCardIcon type={brand} style={{ marginRight: space(1) }} />
+    <CreditCardIcon
+      type={brand}
+      style={{ marginRight: space(1) }}
+      width="25px"
+    />
     <Serif
       size={["2", "3"]}
       color="black100"
       style={{ position: "relative", top: "1px" }}
     >
       •••• {last_digits}
-      &nbsp; Exp {expiration_month}/{expiration_year}
+      &nbsp;&nbsp; Exp {expiration_month.toString().padStart(2, "0")}/
+      {expiration_year.toString().slice(-2)}
     </Serif>
   </Flex>
 )
