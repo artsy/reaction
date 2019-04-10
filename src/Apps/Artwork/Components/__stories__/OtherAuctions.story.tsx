c6bb83aab1fa249838f005a5648dd90c4081da96
Ash Furrow <ash@ashfurrow.com>
2019-04-10 12:17:18 -0400

    Updates stories to use fragment container.

--- src/Apps/Artwork/Components/__stories__/OtherAuctions.story.tsx
+++ src/Apps/Artwork/Components/__stories__/OtherAuctions.story.tsx
@@ -1,46 +1,45 @@
-import React from "react"
+import { OtherAuctionsQuery } from "__generated__/OtherAuctionsQuery.graphql"
+import { SystemContext } from "Artsy"
+import { renderWithLoadProgress } from "Artsy/Relay/renderWithLoadProgress"
+import React, { useContext } from "react"
+import { graphql, QueryRenderer } from "react-relay"
 import { storiesOf } from "storybook/storiesOf"
 import { Section } from "Utils/Section"
-import { OtherAuctions } from "../OtherAuctions"
+import { OtherAuctionsFragmentContainer } from "../OtherAuctions"
 
-const auctions = [
-  {
-    src: "https://picsum.photos/200/180/?random",
-    headline: "Sotheby’s",
-    subHeadline: "Contemporary Day Sale",
-    badge: "In progress",
-    href: "#",
-  },
-  {
-    src: "https://picsum.photos/400/180/?random",
-    headline: "Sotheby’s",
-    subHeadline: "Contemporary Day Sale",
-    badge: "In progress",
-    href: "#",
-  },
-  {
-    src: "https://picsum.photos/200/600/?random",
-    headline: "Sotheby’s",
-    subHeadline: "Contemporary Day Sale",
-    badge: "In progress",
-    href: "#",
-  },
-  {
-    src: "https://picsum.photos/200/180/?random",
-    headline: "Sotheby’s",
-    subHeadline: "Contemporary Day Sale",
-    badge: "In progress",
-    href: "#",
-  },
-] as any
+const OtherAuctions = ({ size }: { size?: number }) => {
+  const { relayEnvironment } = useContext(SystemContext)
 
-storiesOf("Apps/Artwork Page/Components/OtherAuctions", module).add(
-  "Other Auctions",
-  () => {
+  return (
+    <QueryRenderer<OtherAuctionsQuery>
+      environment={relayEnvironment}
+      query={graphql`
+        query OtherAuctionsQuery($size: Int!) {
+          sales(size: $size, sort: TIMELY_AT_NAME_ASC) {
+            ...OtherAuctions_sales
+            # Relay isn't transitively expanding the child fragments for some reason, so we'll expand manually.
+            ...AuctionCard_sale
+          }
+        }
+      `}
+      variables={{ size: size || 4 }}
+      render={renderWithLoadProgress(OtherAuctionsFragmentContainer as any)}
+    />
+  )
+}
+
+storiesOf("Apps/Artwork Page/Components/OtherAuctions", module)
+  .add("Other Auctions (default)", () => {
     return (
       <Section title="Responsive Other Auctions">
-        <OtherAuctions viewer={{ sales: auctions } as any} />
+        <OtherAuctions />
       </Section>
     )
-  }
-)
+  })
+  .add("Other Auctions (many)", () => {
+    return (
+      <Section title="Responsive Other Auctions">
+        <OtherAuctions size={20} />
+      </Section>
+    )
+  })
