0c52acba254ed4274fbcf0227012ffa6d810cd1d
Devon Blandin <dblandin@gmail.com>
2019-04-25 14:27:05 -0400

    Update responsive grid styling for search results skeleton
    This commit updates styling for various responsive breakpoints.

--- src/Apps/Search/Components/SearchResultsSkeleton/index.tsx
+++ src/Apps/Search/Components/SearchResultsSkeleton/index.tsx
@@ -1,6 +1,5 @@
 import { Box, Col, Flex, Grid, Row } from "@artsy/palette"
 import { AppContainer } from "Apps/Components/AppContainer"
-import { HorizontalPadding } from "Apps/Components/HorizontalPadding"
 import React from "react"
 import { FilterSidebar } from "./FilterSidebar"
 import { GridItem } from "./GridItem"
@@ -9,34 +8,32 @@ import { Header } from "./Header"
 export const SearchResultsSkeleton: React.FC<any> = props => {
   return (
     <AppContainer>
-      <HorizontalPadding>
-        <Box>
-          <Header />
-          <Flex>
-            <FilterSidebar />
-            <Box width={["100%", "75%"]}>
-              <Grid fluid>
-                <Row>
-                  <Col sm={4} pr={1}>
-                    <GridItem height={200} />
-                    <GridItem height={400} />
-                    <GridItem height={240} />
-                  </Col>
-                  <Col sm={4} pr={1}>
-                    <GridItem height={300} />
-                    <GridItem height={200} />
-                    <GridItem height={320} />
-                  </Col>
-                  <Col sm={4}>
-                    <GridItem height={240} />
-                    <GridItem height={400} />
-                  </Col>
-                </Row>
-              </Grid>
-            </Box>
-          </Flex>
-        </Box>
-      </HorizontalPadding>
+      <Box pl={20}>
+        <Header />
+        <Flex>
+          <FilterSidebar />
+          <Box width={["100%", "75%"]}>
+            <Grid fluid>
+              <Row>
+                <Col xs="6" sm="6" md="6" lg="4">
+                  <GridItem height={200} />
+                  <GridItem height={400} />
+                  <GridItem height={240} />
+                </Col>
+                <Col xs="6" sm="6" md="6" lg="4">
+                  <GridItem height={300} />
+                  <GridItem height={200} />
+                  <GridItem height={320} />
+                </Col>
+                <Col xs="6" sm="6" md="6" lg="4">
+                  <GridItem height={240} />
+                  <GridItem height={400} />
+                </Col>
+              </Row>
+            </Grid>
+          </Box>
+        </Flex>
+      </Box>
     </AppContainer>
   )
 }
