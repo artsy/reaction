e57d6acafbd73d4999c51f549e0e543b8c61fb3b
Devon Blandin <dblandin@gmail.com>
2019-04-26 12:58:26 -0400

    Update style for search results page skeleton
    Minor updates to the positioning of the skeleton.

--- src/Apps/Search/Components/SearchResultsSkeleton/Header.tsx
+++ src/Apps/Search/Components/SearchResultsSkeleton/Header.tsx
@@ -3,9 +3,9 @@ import React from "react"
 
 export const Header: React.SFC<any> = props => {
   return (
-    <Box height={100} mb={30} mt={120}>
-      <Box mb={40} background={color("black10")} width={320} height={20} />
-      <Box width={700}>
+    <Box height={100} mb={30} mt={120} pr={[0, 20]} pl={[0, 20]}>
+      <Box pl={[20, 0]}>
+        <Box mb={40} background={color("black10")} width={260} height={20} />
         <Box
           width={80}
           height={12}
@@ -24,28 +24,28 @@ export const Header: React.SFC<any> = props => {
           width={80}
           height={12}
           mr={3}
-          display="inline-block"
+          display={["none", "inline-block"]}
           background={color("black10")}
         />
         <Box
           width={80}
           height={12}
           mr={3}
-          display="inline-block"
+          display={["none", "inline-block"]}
           background={color("black10")}
         />
         <Box
           width={80}
           height={12}
           mr={3}
-          display="inline-block"
+          display={["none", "inline-block"]}
           background={color("black10")}
         />
         <Box
           width={80}
           height={12}
           mr={3}
-          display="inline-block"
+          display={["none", "inline-block"]}
           background={color("black10")}
         />
       </Box>
