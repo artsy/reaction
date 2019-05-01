87676d2f083c05b3f391b104f8e41b477391690b
Ashley Jelks <arjelks@gmail.com>
2019-05-01 17:17:22 -0400

    removes debug console.log

--- src/Components/Publishing/Series/__tests__/SeriesAbout.test.tsx
+++ src/Components/Publishing/Series/__tests__/SeriesAbout.test.tsx
@@ -59,7 +59,6 @@ describe("SeriesAbout", () => {
 
     it("Does not render PartnerBlock for an unsponsored series", () => {
       const component = mount(<SeriesAbout article={SeriesArticle} />)
-      console.log(component.debug())
       expect(component.find(".PartnerBlock").length).toBe(0)
     })
 
