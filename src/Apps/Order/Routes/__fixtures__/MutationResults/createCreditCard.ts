a105ff180c4a1cc85e06d78448b3ef44de281504
Lily Pace <lilypace@Lilys-MacBook-Pro.local>
2019-05-28 11:43:09 -0400

    updated test mutation results

--- src/Apps/Order/Routes/__fixtures__/MutationResults/createCreditCard.ts
+++ src/Apps/Order/Routes/__fixtures__/MutationResults/createCreditCard.ts
@@ -3,6 +3,17 @@ export const creatingCreditCardSuccess = {
     creditCardOrError: {
       creditCard: {
         id: "gravityCreditCardId",
+        name: "Artsy UK Ltd",
+        street1: "14 Gower's Walk",
+        street2: "Suite 2.5, The Loom",
+        city: "London",
+        state: "Whitechapel",
+        country: "UK",
+        postal_code: "E1 8PY",
+        expiration_month: 12,
+        expiration_year: 2022,
+        last_digits: "1234",
+        brand: "Visa",
       },
     },
   },
