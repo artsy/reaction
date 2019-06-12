cc49213999a1cf2056adc5007f9e01212375d2aa
Eloy Durán <eloy.de.enige@gmail.com>
2019-06-12 10:40:59 +0200

    [package] Use relay-config for centralized config

---
+++ relay.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  src: "./src",
+  schema: "./data/schema.graphql",
+  language: "typescript",
+  artifactDirectory: "./src/__generated__",
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+}
