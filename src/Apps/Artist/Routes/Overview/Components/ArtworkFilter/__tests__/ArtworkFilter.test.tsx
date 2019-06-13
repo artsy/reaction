a2ed74211e1bfaf7a79005320b3d4bb68a291ba5
Steven Hicks <steven.j.hicks@gmail.com>
2019-06-13 13:21:22 -0500

    Add tests to guide extraction of mobile action sheet management from Apps/Artist/…state.ts

--- src/Apps/Artist/Routes/Overview/Components/ArtworkFilter/__tests__/ArtworkFilter.test.tsx
+++ src/Apps/Artist/Routes/Overview/Components/ArtworkFilter/__tests__/ArtworkFilter.test.tsx
@@ -1,14 +1,20 @@
 // FIXME: Uncomment tests, currently WIP
 
-import { ArtworkFilter_Test_Query } from "__generated__/ArtworkFilter_Test_Query.graphql"
+import { ArtworkFilterFixture } from "Apps/__tests__/Fixtures/Artist/Components/ArtworkFilter"
 import { ArtworkFilterFragmentContainer as ArtworkFilter } from "Apps/Artist/Routes/Overview/Components/ArtworkFilter"
 import { FilterState } from "Apps/Artist/Routes/Overview/state"
-import { MockBoot, MockRelayRenderer, renderUntil } from "DevTools"
+import { MockBoot, renderRelayTree } from "DevTools"
 import React from "react"
 import { graphql } from "react-relay"
 import { Provider } from "unstated"
+import { flushPromiseQueue } from "Utils/flushPromiseQueue"
+import { get } from "Utils/get"
+import { Breakpoint } from "Utils/Responsive"
 
 jest.unmock("react-relay")
+jest.mock("../ArtworkFilterRefetch", () => ({
+  ArtworkFilterRefetchContainer: () => <div>Mock ArtworkFilterRefetch</div>,
+}))
 
 describe("ArtworkFilter", () => {
   let filterState: FilterState = null
@@ -17,154 +23,76 @@ describe("ArtworkFilter", () => {
     filterState = new FilterState({} as any)
   })
 
-  it("renders the current mediums", async () => {
-    const tree = await renderUntil(
-      wrapper => {
-        return wrapper.find(ArtworkFilter).length > 0
+  async function getWrapper(breakpoint: Breakpoint = "lg") {
+    return await renderRelayTree({
+      Component: ArtworkFilter,
+      query: graphql`
+        query ArtworkFilter_Test_Query {
+          artist(id: "andy-warhol") {
+            ...ArtworkFilter_artist
+          }
+        }
+      `,
+      mockData: {
+        artist: ArtworkFilterFixture,
       },
-      <MockBoot breakpoint="lg">
-        <Provider inject={[filterState]}>
-          <MockRelayRenderer<ArtworkFilter_Test_Query>
-            Component={ArtworkFilter}
-            query={graphql`
-              query ArtworkFilter_Test_Query {
-                artist(id: "andy-warhol") {
-                  ...ArtworkFilter_artist
-                }
-              }
-            `}
-            mockResolvers={{
-              Artist: () => data,
-            }}
-          />
-        </Provider>
-      </MockBoot>
-    )
+
+      wrapper: children => (
+        <MockBoot breakpoint={breakpoint}>
+          <Provider inject={[filterState]}>{children}</Provider>
+        </MockBoot>
+      ),
+    })
+  }
+
+  it("renders the current mediums", async () => {
+    const tree = await getWrapper()
+
     const html = tree.html()
     expect(html).toContain("Catty Painting")
   })
-})
 
-const data = {
-  id: "cecily-brown",
-  __id: "blah",
-  name: "Cecily Brown",
-  is_followed: false,
-  counts: {
-    for_sale_artworks: 28,
-    ecommerce_artworks: 4,
-    auction_artworks: 1,
-    artworks: 116,
-    follows: 20,
-  },
-  filtered_artworks: {
-    __id: "testest",
-    aggregations: [
-      {
-        slice: "INSTITUTION",
-        counts: [
-          {
-            name: "The FLAG Art Foundation",
-            id: "the-flag-art-foundation",
-            __id: "QWdncmVnYXRpb25Db3VudDp0aGUtZmxhZy1hcnQtZm91bmRhdGlvbg==",
-          },
-        ],
-      },
-      {
-        slice: "GALLERY",
-        counts: [
-          {
-            name: "The FLAG Art Foundation",
-            id: "the-flag-art-foundation",
-            __id: "QWdncmVnYXRpb25Db3VudDp0aGUtZmxhZy1hcnQtZm91bmRhdGlvbg==",
-          },
-        ],
-      },
-      {
-        slice: "MAJOR_PERIOD",
-        counts: [
-          {
-            name: "The 90s",
-            id: "90s",
-            __id: "blah==",
-          },
-        ],
-      },
-      {
-        slice: "MEDIUM",
-        counts: [
-          {
-            name: "Catty Painting",
-            id: "painting",
-            __id: "QWdncmVnYXRpb25Db3VudDpwYWludGluZw==",
-          },
-        ],
-      },
-    ],
-  },
-  grid: {
-    __id:
-      "RmlsdGVyQXJ0d29ya3M6eyJhY3F1aXJlYWJsZSI6dHJ1ZSwiYWdncmVnYXRpb25zIjpbInRvdGFsIl0sIm1ham9yX3BlcmlvZHMiOltdLCJzaXplIjowLCJzb3J0IjoiLWRlY2F5ZWRfbWVyY2giLCJhcnRpc3RfaWQiOiJjZWNpbHktYnJvd24ifQ==",
-    artworks: {
-      pageInfo: {
-        hasNextPage: false,
-        endCursor: "YXJyYXljb25uZWN0aW9uOjM=",
-      },
-      pageCursors: {
-        around: [
-          {
-            cursor: "YXJyYXljb25uZWN0aW9uOi0x",
-            page: 1,
-            isCurrent: true,
-          },
-        ],
-        first: null,
-        last: null,
-        previous: null,
-      },
-      edges: [
-        {
-          node: {
-            artists: [
-              {
-                __id: "QXJ0aXN0OmNlY2lseS1icm93bg==",
-                href: "/artist/cecily-brown",
-                name: "Cecily Brown",
-              },
-            ],
-            collecting_institution: null,
-            cultural_maker: null,
-            date: "2009",
-            href:
-              "/artwork/cecily-brown-cecily-brown-deichtorhallen-hamburg-germany-signed-3",
-            id:
-              "cecily-brown-cecily-brown-deichtorhallen-hamburg-germany-signed-3",
-            image: {
-              aspect_ratio: 1,
-              placeholder: "100.18382352941177%",
-              url:
-                "https://d32dm0rphc51dk.cloudfront.net/DnTE4ynxczEq8rFHRMWeqQ/large.jpg",
-            },
-            is_acquireable: true,
-            is_biddable: false,
-            is_inquireable: true,
-            is_saved: false,
-            partner: {
-              href: "/alpha-137-gallery",
-              name: "Alpha 137 Gallery",
-              type: "Gallery",
-              __id: "UGFydG5lcjphbHBoYS0xMzctZ2FsbGVyeQ==",
-            },
-            sale: null,
-            sale_artwork: null,
-            sale_message: "$800",
-            title: "Cecily Brown, Deichtorhallen Hamburg, Germany (Signed) ",
-            __id:
-              "QXJ0d29yazpjZWNpbHktYnJvd24tY2VjaWx5LWJyb3duLWRlaWNodG9yaGFsbGVuLWhhbWJ1cmctZ2VybWFueS1zaWduZWQtMw==",
-            _id: "59227f8e7622dd4d747b6993",
-          },
-        },
-      ],
-    },
-  },
-}
+  describe("Mobile Action Sheet", () => {
+    it("Doesn't display the filters by default at xs", async () => {
+      const wrapper = await getWrapper("xs")
+
+      expect(wrapper.find("Filters").length).toEqual(0)
+    })
+
+    it("Displays the filters after clicking the 'Filter' button at xs", async () => {
+      const wrapper = await getWrapper("xs")
+      const filterButton = findButtonWithText(wrapper, "Filter")
+
+      filterButton.simulate("click")
+      await flushPromiseQueue()
+      wrapper.update()
+
+      expect(wrapper.find("Filters").length).toEqual(1)
+    })
+
+    it("Closes the filters after clicking the 'Apply' button at xs", async () => {
+      const wrapper = await getWrapper("xs")
+
+      const filterButton = findButtonWithText(wrapper, "Filter")
+      filterButton.simulate("click")
+      await flushPromiseQueue()
+      wrapper.update()
+
+      const applyButton = findButtonWithText(wrapper, "Apply")
+      applyButton.simulate("click")
+      await flushPromiseQueue()
+      wrapper.update()
+
+      expect(wrapper.find("Filters").length).toEqual(0)
+    })
+
+    function findButtonWithText(wrapper, buttonText) {
+      return wrapper
+        .find("Button")
+        .findWhere(button => {
+          return get(button, b => b.text().includes(buttonText), false)
+        })
+        .first()
+    }
+  })
+})
