31f83f10b77bd9a4df493985e86afcdb241796a3
Ashley Jelks <arjelks@gmail.com>
2019-07-12 17:16:06 -0400

    update tests

--- src/Components/Publishing/Layouts/__tests__/VideoLayout.test.tsx
+++ src/Components/Publishing/Layouts/__tests__/VideoLayout.test.tsx
@@ -1,11 +1,19 @@
+import { DisplayAd } from "Components/Publishing/Display/DisplayAd"
+import { targetingData } from "Components/Publishing/Display/DisplayTargeting"
 import {
   SeriesArticle,
   StandardArticle,
   VideoArticle,
 } from "Components/Publishing/Fixtures/Articles"
+import {
+  VideoArticle as VideoArticleFixture,
+  VideoArticleSponsored,
+} from "Components/Publishing/Fixtures/Articles"
 import { Nav } from "Components/Publishing/Nav/Nav"
 import { ArticleCard } from "Components/Publishing/RelatedArticles/ArticleCards/ArticleCard"
 import { SeriesAbout } from "Components/Publishing/Series/SeriesAbout"
+import { AdDimension, AdUnit } from "Components/Publishing/Typings"
+import { isEditorialSponsored } from "Components/Publishing/utils/Sponsored"
 import { VideoPlayer } from "Components/Publishing/Video/Player/VideoPlayer"
 import { VideoAbout } from "Components/Publishing/Video/VideoAbout"
 import { mount } from "enzyme"
@@ -78,5 +86,135 @@ describe("Video Layout", () => {
     const component = getWrapper() as any
     component.instance().onPlayToggle(false)
     expect(component.state().isPlaying).toBe(false)
+  })
+})
+
+describe("Video Layout ads", () => {
+  let props
+  const getWrapper = (passedProps = props) => {
+    return mount(<VideoLayout {...passedProps} />)
+  }
+
+  beforeEach(() => {
+    props = {
+      areHostedAdsEnabled: true,
+      article: VideoArticleFixture,
+      isSeries: true,
+    }
+  })
+
+  it("renders the video layout properly with ads", () => {
+    const layout = renderer
+      .create(<VideoLayout article={VideoArticleFixture} areHostedAdsEnabled />)
+      .toJSON()
+    expect(layout).toMatchSnapshot()
+  })
+
+  it("renders an ad component on video series", () => {
+    const component = getWrapper()
+
+    expect(component.find(DisplayAd).length).toBe(1)
+  })
+})
+
+describe("display ad data on video series", () => {
+  it("renders the component with the correct target properties when video article is sponsored", () => {
+    const isSponsored = isEditorialSponsored(VideoArticleSponsored.sponsor)
+
+    const canvas = mount(
+      <DisplayAd
+        adDimension={
+          AdDimension.Desktop_SponsoredSeriesLandingPageAndVideoPage_LeaderboardBottom
+        }
+        adUnit={
+          AdUnit.Desktop_SponsoredSeriesLandingPageAndVideoPage_LeaderboardBottom
+        }
+        displayNewAds
+        isSeries
+        targetingData={targetingData(
+          VideoArticleSponsored.id,
+          isSponsored ? "sponsorlanding" : "video"
+        )}
+      />
+    )
+
+    expect(canvas.props().targetingData).toEqual({
+      is_testing: true,
+      page_type: "sponsorlanding",
+      post_id: "597b9f652d35b80017a2a6a7",
+    })
+  })
+
+  it("renders the component with the correct target properties when video article is not sponsored", () => {
+    const isSponsored = isEditorialSponsored(VideoArticleFixture.sponsor)
+
+    const canvas = mount(
+      <DisplayAd
+        adDimension={
+          AdDimension.Desktop_SponsoredSeriesLandingPageAndVideoPage_LeaderboardBottom
+        }
+        adUnit={
+          AdUnit.Desktop_SponsoredSeriesLandingPageAndVideoPage_LeaderboardBottom
+        }
+        displayNewAds
+        isSeries
+        targetingData={targetingData(
+          VideoArticleFixture.id,
+          isSponsored ? "sponsorlanding" : "video"
+        )}
+      />
+    )
+
+    expect(canvas.props().targetingData).toEqual({
+      is_testing: true,
+      page_type: "video",
+      post_id: "597b9f652d35b80017a2a6a7",
+    })
+  })
+
+  it("renders the component with the correct data and properties on video landing pages on desktop", () => {
+    const isSponsored = isEditorialSponsored(VideoArticleFixture.sponsor)
+
+    const canvas = mount(
+      <DisplayAd
+        adDimension={
+          AdDimension.Desktop_SponsoredSeriesLandingPageAndVideoPage_LeaderboardBottom
+        }
+        adUnit={
+          AdUnit.Desktop_SponsoredSeriesLandingPageAndVideoPage_LeaderboardBottom
+        }
+        displayNewAds
+        isSeries
+        targetingData={targetingData(
+          VideoArticleFixture.id,
+          isSponsored ? "sponsorlanding" : "video"
+        )}
+      />
+    )
+
+    expect(canvas.props().adDimension).toEqual("970x250")
+    expect(canvas.props().adUnit).toEqual("Desktop_InContentLB2")
+    expect(canvas.props().displayNewAds).toBe(true)
+
+    expect(canvas).toHaveLength(1)
+  })
+
+  it("renders the component with the correct data and properties on series landing pages on mobile", () => {
+    const canvas = mount(
+      <DisplayAd
+        adDimension={
+          AdDimension.Mobile_SponsoredSeriesLandingPageAndVideoPage_Bottom
+        }
+        adUnit={AdUnit.Mobile_SponsoredSeriesLandingPageAndVideoPage_Bottom}
+        displayNewAds
+        isSeries
+        targetingData={targetingData(VideoArticleFixture.id, "sponsorlanding")}
+      />
+    )
+
+    expect(canvas.props().adDimension).toEqual("300x250")
+    expect(canvas.props().adUnit).toEqual("Mobile_InContentLB2")
+    expect(canvas.props().displayNewAds).toBe(true)
+    expect(canvas).toHaveLength(1)
   })
 })
