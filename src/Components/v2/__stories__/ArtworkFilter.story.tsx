cb3d48142a4486f495fb5b77247b28f5cf133f43
Christopher Pappas <damassi.pappas@gmail.com>
2019-07-26 14:14:34 -0700

    [ArtworkFilter v2] Simplify pagination and fix refetch bug

--- src/Components/v2/__stories__/ArtworkFilter.story.tsx
+++ src/Components/v2/__stories__/ArtworkFilter.story.tsx
@@ -1,7 +1,12 @@
+import { Box } from "@artsy/palette"
 import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { ArtworkFilterQueryRenderer } from "../ArtworkFilter"
 
 storiesOf("Components/ArtworkFilter", module).add("ArtworkFilter", () => {
-  return <ArtworkFilterQueryRenderer />
+  return (
+    <Box p={4}>
+      <ArtworkFilterQueryRenderer />
+    </Box>
+  )
 })
