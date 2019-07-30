4530d788ff9a05ab7ede3dc7c04333a05bf8cf4e
Devon Blandin <dblandin@gmail.com>
2019-07-30 18:31:55 -0400

    Add redirects to AuctionApp bidder registration flow
    Add redirects within the bidder registration flow for the following
    cases:
    
    - Requested sale is not an auction
    - Auction is not in a preview or open state
    - Registration window for auction has already closed
    - Authenticated user is already registered to bid in the auction

---
+++ src/Apps/__tests__/Fixtures/Sale.ts
@@ -0,0 +1,8 @@
+export const SaleAuction = {
+  is_auction: true,
+  id: "yuki-contemporary-art-july-22th",
+  is_registration_closed: false,
+  is_open: true,
+  is_preview: false,
+  registrationStatus: null,
+}
