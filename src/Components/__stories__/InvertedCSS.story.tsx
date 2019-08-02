0899c061ab72c24589bfb3413961afc20606f315
Ashley Jelks <arjelks@gmail.com>
2019-08-02 11:08:52 -0400

    add story for inverted text css over html

--- src/Components/__stories__/InvertedCSS.story.tsx
+++ src/Components/__stories__/InvertedCSS.story.tsx
@@ -1,34 +1,37 @@
-import { Box, Flex } from "@artsy/palette"
+import { Box, color, Flex } from "@artsy/palette"
 import { storiesOf } from "@storybook/react"
 import { unica } from "Assets/Fonts"
 import React from "react"
 import styled from "styled-components"
+import { getViewportWidth } from "Utils/viewport"
+import { ReactComponent as Logo } from "../../Assets/ArtsyVanguard/NewlyEstablished-01"
+
+const { height, width } = getViewportWidth()
 
 storiesOf("Components/InvertedCSS", module)
+  .add("Inverted CSS Over HTML", () => (
+    <Box height={height} width={width}>
+      <Background />
+      <Middleground />
+      <Foreground />
+      <InvertedText>Statement.</InvertedText>
+    </Box>
+  ))
   .add("Inverted CSS Over SVG", () => (
     <div style={{ padding: 10 }}>
       <Title>Inverted CSS Over SVG</Title>
       <section>
-        <SVGRect>
-          <Flex flexDirection="column">
-            <Vanguard />
-          </Flex>
-        </SVGRect>
-      </section>
-    </div>
-  ))
-  .add("Inverted CSS Over HTML Div", () => (
-    <div style={{ padding: 10 }}>
-      <section>
-        <Title>Inverted CSS Over Video </Title>
+        <SVGRect />
+        <Flex flexDirection="column">
+          <Vanguard>Vanguard</Vanguard>
+        </Flex>
       </section>
     </div>
   ))
   .add("Inverted CSS Over Video", () => (
     <div style={{ padding: 10 }}>
       <section>
-        <Title>Inverted CSS Over HTML</Title>
-        <Background />
+        <Title>Inverted CSS Over Video</Title>
       </section>
     </div>
   ))
@@ -38,11 +41,11 @@ const SVGContainer = styled.svg`
   width: 1000px;
   padding: 10px;
   flex-shrink: 0;
+  isolation: isolate;
 `
 
 const SVGRect = props => (
   <SVGContainer {...props}>
-    <title>my vector image</title>
     <rect
       id="backgroundrect"
       width="1000px"
@@ -59,28 +62,54 @@ const SVGRect = props => (
 const Vanguard = styled(Flex)`
   flex-direction: column;
   transform: rotate(0.75turn);
-  max-height: 100%;
-  &:after {
-    me
+  position: absolute;
+  left: 0;
+  bottom: 350;
+    mix-blend-mode: multiply;    
     padding-left: 20px;
-    content: "Vanguard";
     text-align: center;
-    mix-blend-mode: difference;
-    color:white;
     font-size: 50px;
-  }
-
 }
 `
 const Title = styled.h1`
   ${unica("s40")};
 `
 
+// Background and Middleground height needs to take up the height of the viewport
+// so that the mix blend mode color inversion has a backdrop to contrast against.
+// Use inherit on height it's height is always as large as the parent.
 const Background = styled(Box)`
-  background-color: black;
-  width: 1000px;
-  height: 500px;
-  overflow: visible;
-  color: red;
+  background: ${color("white100")};
+  width: 100%;
+  height: inherit;
+  z-index: 1;
+  position: absolute;
+`
+
+const Middleground = styled(Box)`
+  background: ${color("black100")};
+  width: 435px;
+  height: inherit;
+  z-index: 2;
+  position: absolute;
+`
+
+const Foreground = styled(Box)`
+  mix-blend-mode: screen;
+  background-color: ${color("black100")};
+  width: 800px;
+  height: 200px;
+  z-index: 4;
+  position: absolute;
+`
+
+const InvertedText = styled(Box)`
+  position: fixed;
+  font-family: Arial, Helvetica;
+  font-size: 100px;
   mix-blend-mode: difference;
+  color: white;
+  z-index: 3;
+  transform: rotate(270deg);
+  margin: 250px 0 0 195px;
 `
