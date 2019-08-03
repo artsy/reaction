54e6efa2c00a04b5e69128b0e74896ba2c0cfed3
Ashley Jelks <arjelks@gmail.com>
2019-08-03 00:32:27 -0400

    upload newly established svgs

--- src/Components/__stories__/InvertedCSS.story.tsx
+++ src/Components/__stories__/InvertedCSS.story.tsx
@@ -1,10 +1,10 @@
 import { Box, color, Flex } from "@artsy/palette"
 import { storiesOf } from "@storybook/react"
+import { NewlyEstablished1 } from "Assets/ArtsyVanguard/NewlyEstablished"
 import { unica } from "Assets/Fonts"
 import React from "react"
 import styled from "styled-components"
 import { getViewportWidth } from "Utils/viewport"
-import { NewlyEstablished } from "../../Assets/ArtsyVanguard/VanguardAssets"
 
 const { height, width } = getViewportWidth()
 
@@ -35,9 +35,11 @@ storiesOf("Components/InvertedCSS", module)
     </div>
   ))
 
-// Background and Middleground height needs to take up the height of the viewport
-// so that the mix blend mode color inversion has a backdrop to contrast against.
-// Use inherit on height it's height is always as large as the parent.
+/**
+ * Background and Middleground heights need to take up the height of the viewport
+ * so that the mix-blend-mode color inversion has a backdrop to contrast against.
+ * Use inherit on height it's height is always as large as the parent.
+ */
 const Background = styled(Box)`
   background: ${color("white100")};
   width: 100%;
@@ -88,10 +90,10 @@ const Title = styled.h1`
   ${unica("s40")};
 `
 /**
- * Adding huge padding bottom here as a temp hack to push the transparent border
- * on the below the rest of the page content
+ * Adding huge padding bottom here as a temporary hack to push the transparent border
+ * below the rest of the page content
  */
-const SVG = styled(NewlyEstablished)`
+const SVG = styled(NewlyEstablished1)`
   padding-bottom: 7000px;
   background-color: ${color("white100")};
   mix-blend-mode: difference;
