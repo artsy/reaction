5305c34406ff2f3d5929257ae14d7ae77cbecab1
Erik Stockmeier <erik.d.stock@gmail.com>
2019-08-12 17:13:34 -0400

    pass props explicitly

--- src/Components/Auction/ConditionsOfSaleCheckbox.tsx
+++ src/Components/Auction/ConditionsOfSaleCheckbox.tsx
@@ -2,9 +2,12 @@ import { Checkbox, CheckboxProps, Link, Serif } from "@artsy/palette"
 import React from "react"
 import { data as sd } from "sharify"
 
-export const ConditionsOfSaleCheckbox: React.FC<CheckboxProps> = props => {
+export const ConditionsOfSaleCheckbox: React.FC<CheckboxProps> = ({
+  selected,
+  onSelect,
+}) => {
   return (
-    <Checkbox {...props}>
+    <Checkbox selected={selected} onSelect={onSelect}>
       <Serif display="inline" color="black60" size="3t" ml={0.5}>
         {"Agree to "}
         <Serif display="inline" color="black100" size="3t">
