702c54c16852d4038467e720088e9f8ec7bdb979
Steven Hicks <steven.j.hicks@gmail.com>
2019-08-20 17:05:52 -0500

    [CollectionsHubsNav] Restyle collection hub nav according to new designs

--- src/Components/v2/CollectionsHubsNav.tsx
+++ src/Components/v2/CollectionsHubsNav.tsx
@@ -3,17 +3,12 @@ import { CollectionsHubsNav_marketingCollections } from "__generated__/Collectio
 import React, { FC } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 
-import { AutoScalingImageLink } from "./AutoScalingImageLink"
+import { CollectionsHubsNavItem } from "./CollectionsHubsNavItem"
 
 interface CollectionsHubsNavProps {
   marketingCollections: CollectionsHubsNav_marketingCollections
 }
 
-// largest sized image at each breakpoint:
-// xs: 346x216 ~= 1.6 aspect ratio
-// sm: 254x159 ~= 1.6 aspect ratio
-// md: 138x86 ~= 1.6 aspect ratio
-// lg/xl: 168x105 = 1.6 aspect ratio
 export const CollectionsHubsNav: FC<CollectionsHubsNavProps> = props => {
   return (
     <CSSGrid
@@ -26,23 +21,21 @@ export const CollectionsHubsNav: FC<CollectionsHubsNavProps> = props => {
       gridGap={20}
     >
       {props.marketingCollections.map(hub => (
-        <AutoScalingImageLink
+        <CollectionsHubsNavItem
           href={`/collection/${hub.slug}`}
-          imageUrl={hub.thumbnail || plainWhitePixel}
+          imageUrl={placeholderImage}
           key={hub.id}
-          fontSize={["5t", "5t", "4t", "5t"]}
-          maxWidth={346}
-          maxHeight={216}
         >
           {hub.title}
-        </AutoScalingImageLink>
+        </CollectionsHubsNavItem>
       ))}
     </CSSGrid>
   )
 }
 
-const plainWhitePixel =
-  "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+// This is a temporary image URL until we get real data.
+const placeholderImage =
+  "https://d7hftxdivxxvm.cloudfront.net/?resize_to=fill&width=357&height=175&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FeF_qAORql7lSnD2BwbFOYg%2Flarge.jpg"
 
 export const CollectionsHubsNavFragmentContainer = createFragmentContainer(
   CollectionsHubsNav,
