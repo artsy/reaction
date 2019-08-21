d33fb0511282b0fd1132859cb47cd6d699db8db9
Ashley Jelks <arjelks@gmail.com>
2019-08-21 12:54:44 -0400

    wip

--- src/Components/Publishing/EditorialFeature/Components/Vanguard2019/Components/Introduction.tsx
+++ src/Components/Publishing/EditorialFeature/Components/Vanguard2019/Components/Introduction.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Sans, Serif } from "@artsy/palette"
 import { Byline, BylineContainer } from "Components/Publishing/Byline/Byline"
-import { VanguardVideoBackground } from "Components/Publishing/EditorialFeature/Components/Vanguard2019/Components/VanguardVideoBackground"
+import { VanguardVideoPrimarySeries } from "Components/Publishing/EditorialFeature/Components/Vanguard2019/Components/VanguardVideoBackground"
 import { Text } from "Components/Publishing/Sections/Text"
 import { ArticleData } from "Components/Publishing/Typings"
 import React from "react"
@@ -18,9 +18,7 @@ export const VanguardIntroduction: React.SFC<{
     <IntroContainer>
       <Box minHeight="calc(100vh - 50px)" mb={150} pt={50}>
         <VideoWrapper>
-          {isVideo && (
-            <VanguardVideoBackground id="#clip-svg-intro" url={url} />
-          )}
+          {isVideo && <VanguardVideoPrimarySeries url={url} />}
         </VideoWrapper>
         <HeaderText pt={70} size="8" textAlign="center">
           The Artsy
