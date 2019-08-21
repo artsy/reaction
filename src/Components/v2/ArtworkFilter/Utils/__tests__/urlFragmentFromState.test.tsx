3470225c1e40e263d0ef89cc556771512372a774
Christopher Pappas <damassi.pappas@gmail.com>
2019-08-21 15:08:26 -0700

    [ArtworkFilter Tests] Add tests for /Utils

--- src/Components/v2/ArtworkFilter/Utils/__tests__/urlFragmentFromState.test.tsx
+++ src/Components/v2/ArtworkFilter/Utils/__tests__/urlFragmentFromState.test.tsx
@@ -1,5 +1,21 @@
-describe("fill", () => {
-  it("fill", () => {
-    //
+import {
+  ArtworkFilters,
+  initialArtworkFilterState,
+} from "../../ArtworkFilterContext"
+
+import { urlFragmentFromState } from "../urlFragmentFromState"
+
+describe("urlFragmentFromState", () => {
+  it("returns a queryParam-safe representation of artworkFilterState", () => {
+    const artworkFilterState: ArtworkFilters = {
+      ...initialArtworkFilterState,
+      price_range: "100-200",
+      height: "300-400",
+      width: "500-600",
+    }
+
+    expect(urlFragmentFromState(artworkFilterState)).toEqual(
+      "height=300-400&price_range=100-200&width=500-600"
+    )
   })
 })
--- src/Components/v2/ArtworkFilter/Utils/hasFilters.tsx
+++ src/Components/v2/ArtworkFilter/Utils/hasFilters.tsx
@@ -3,13 +3,6 @@ import { isDefaultFilter } from "./isDefaultFilter"
 
 export const hasFilters: (state: ArtworkFilters) => boolean = state => {
   return Object.entries(state).some(([key, value]) => {
-    /**
-     * TODO: I think this can be based on some flag on the filter that indicates
-     * if this one should trip hasFilters.
-     */
-    if (key === "keyword") {
-      return false
-    }
     return !isDefaultFilter(key, value)
   })
 }
