e0fea8bd9dd3cf2878537156dd00e94acb6cc864
Christopher Pappas <damassi.pappas@gmail.com>
2019-08-21 17:52:11 -0700

    [ArtworkFilter Tests] Add test for ArtworkFilterMobileActionSheet

--- src/Components/v2/ArtworkFilter/__tests__/ArtworkFilterMobileActionSheet.test.tsx
+++ src/Components/v2/ArtworkFilter/__tests__/ArtworkFilterMobileActionSheet.test.tsx
@@ -1,5 +1,64 @@
+import { mount } from "enzyme"
+import React from "react"
+import {
+  ArtworkFilterContextProvider,
+  initialArtworkFilterState,
+  useArtworkFilterContext,
+} from "../ArtworkFilterContext"
+import { ArtworkFilterMobileActionSheet } from "../ArtworkFilterMobileActionSheet"
+
 describe("ArtworkFilterMobileActionSheet", () => {
-  it("fill", () => {
-    //
+  let context
+  let spy
+
+  const getWrapper = (props = {}) => {
+    return mount(
+      <ArtworkFilterContextProvider {...props}>
+        <ArtworkFilterMobileActionSheetTest />
+      </ArtworkFilterContextProvider>
+    )
+  }
+
+  const ArtworkFilterMobileActionSheetTest = () => {
+    context = useArtworkFilterContext()
+    spy = jest.fn()
+
+    return (
+      <ArtworkFilterMobileActionSheet onClose={spy}>
+        <div>found children</div>
+      </ArtworkFilterMobileActionSheet>
+    )
+  }
+
+  it("resets filters to defaults on `Reset` button click", () => {
+    const wrapper = getWrapper({
+      filters: {
+        ...initialArtworkFilterState,
+        page: 20,
+      },
+    })
+    wrapper
+      .find("Button")
+      .first()
+      .simulate("click")
+
+    setTimeout(() => {
+      expect(context.filters).toEqual(initialArtworkFilterState)
+    })
+  })
+
+  it("calls onClose callback on `Apply` button click", () => {
+    const wrapper = getWrapper()
+    wrapper
+      .find("Button")
+      .last()
+      .simulate("click")
+
+    expect(spy).toHaveBeenCalled()
+  })
+
+  it("renders childrent content", () => {
+    const wrapper = getWrapper()
+    expect(wrapper.html()).toContain("found children")
   })
 })
