3f62411c1b2d40c78755dd297447a122f7bd7085
Justin Bennett <zephraph@gmail.com>
2019-08-24 10:31:09 -0400

    Ensure jest continues to transpile to cjs

---
+++ .babelrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  presets: [
+    "@babel/typescript",
+    "@babel/react",
+    [
+      "@babel/env",
+      {
+        useBuiltIns: "usage",
+        modules: process.env.NODE_ENV === "test" ? "auto" : false,
+      },
+    ],
+  ],
+  plugins: [
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        legacy: true,
+      },
+    ],
+    "@babel/plugin-proposal-class-properties",
+    [
+      "relay",
+      {
+        artifactDirectory: "./src/__generated__",
+      },
+    ],
+    [
+      "styled-components",
+      {
+        ssr: true,
+      },
+    ],
+    [
+      "module-resolver",
+      {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        root: ["./src"],
+        alias: {
+          storybook: "./src/__stories__",
+          Analytics: "./src/Analytics",
+          Assets: "./src/Assets",
+          Components: "./src/Components",
+          Styleguide: "./src/Styleguide",
+          "package.json": "./package.json",
+        },
+      },
+    ],
+    "lodash",
+  ],
+}
