97abb5d415c0f1bc3a19ff4d3cdc607005e2a1a4
Daniel Levenson <dleve123@gmail.com>
2019-08-28 17:26:51 -0400

    Experimental: Extract generic DeFraged type
    Allows one to create a fixture that meets a relay generated type
    without needing to be explicit with ref types and fragment refs
    
    Not sure if this is a desirable dev UX, but curious to get people’s thoughts.
    
    Co-authored-by: Daniel Levenson <dleve123@gmail.com>

--- src/Apps/__tests__/Fixtures/Auction/Routes/Register/index.ts
+++ src/Apps/__tests__/Fixtures/Auction/Routes/Register/index.ts
@@ -2,23 +2,22 @@ import { Register_me } from "__generated__/Register_me.graphql"
 import { Register_sale } from "__generated__/Register_sale.graphql"
 import { routes_RegisterQueryResponse } from "__generated__/routes_RegisterQuery.graphql"
 
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+type DeFraged<T> = Omit<T, " $refType" | " $fragmentRefs">
+
 export interface DeFragedRegisterQueryResponse {
-  sale: routes_RegisterQueryResponse["sale"] & Register_sale
-  me: routes_RegisterQueryResponse["me"] & Register_me
+  sale: DeFraged<routes_RegisterQueryResponse["sale"] & Register_sale>
+  me: DeFraged<routes_RegisterQueryResponse["me"] & Register_me>
 }
 
 export const RegisterQueryResponseFixture: DeFragedRegisterQueryResponse = {
   me: {
     has_qualified_credit_cards: false,
     id: "userid",
-    " $fragmentRefs": undefined,
-    " $refType": undefined,
   },
   sale: {
     is_auction: true,
     id: "an-example-auction-sale",
-    " $refType": undefined,
-    " $fragmentRefs": undefined,
     is_registration_closed: false,
     is_open: true,
     is_preview: false,
