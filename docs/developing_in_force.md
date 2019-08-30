f76bd8c432e5e4b6b18634cd58feeee7ad6e588b
Justin Bennett <zephraph@gmail.com>
2019-08-30 13:43:11 -0400

    Setup safer/more robust linking process for reaction/force

---
+++ docs/developing_in_force.md
@@ -0,0 +1,28 @@
+# Developing in force
+
+Often in development you'll to link the development version of reaction into force.
+Traditionally one would use `yarn link` in reaction and `yarn link @artsy/reaction` in a service like force
+to acheive this. Unfortunately, this mechanism is prone to a lot of issues. Below is a guide for a smoother
+linking experience.
+
+## Setup
+
+If you haven't already, install the dependencies for reaction.
+
+```
+yarn --check-files
+```
+
+## Development
+
+Use yarn to kick off the force development process
+
+```
+yarn dev-force
+```
+
+This command will build and compile reaction, publish it locally so it can be used
+in other places, link it into force for you, and then start reaction in watch mode.
+
+_Note:_ for this process to work force _must_ be in a sibling directory of reaction and
+be named `force`.
---
+++ scripts/watch-link.js
@@ -0,0 +1,38 @@
+const chokidar = require("chokidar")
+const { debounce } = require("lodash")
+const { spawn } = require("child_process")
+
+const run = (cmd, args = []) =>
+  new Promise((resolve, reject) => {
+    if (!Array.isArray(args)) {
+      args = [args]
+    }
+    const p = spawn(cmd, args, {
+      stdio: [process.stdin, process.stdout, process.stderr],
+      cwd: process.cwd(),
+      env: process.env,
+      shell: true,
+    })
+    p.once("error", err => {
+      reject(err)
+    })
+    p.once("exit", code => {
+      if (code === 0) {
+        resolve()
+      } else {
+        reject(new Error(`Exited with ${code}`))
+      }
+    })
+  })
+
+const updateDevPackage = () => {
+  run("yarn", "yalc publish --force --changed")
+}
+
+console.log("Running yalc publish in watch mode")
+
+chokidar
+  .watch(process.cwd(), {
+    ignored: /\/node_modules\/|\/dist\//,
+  })
+  .on("all", debounce(updateDevPackage, 750))
