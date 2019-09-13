7696235b7bbae0d9118703aa851853eb8cee54f0
Christopher Pappas <damassi.pappas@gmail.com>
2019-09-12 20:23:13 -0700

    [Refactor] Replace /search artwork filter with new

--- src/Apps/Search/Routes/Artworks/index.tsx
+++ src/Apps/Search/Routes/Artworks/index.tsx
@@ -1,8 +1,10 @@
 import { Box } from "@artsy/palette"
+import { AnalyticsSchema, useTracking } from "Artsy/Analytics"
 import { Location } from "found"
 import React from "react"
 
 import { ArtworkFilter_viewer } from "__generated__/ArtworkFilter_viewer.graphql"
+import { ZeroState } from "Apps/Search/Components/ZeroState"
 import { ArtworkFilter } from "Components/v2/ArtworkFilter"
 import { updateUrl } from "Components/v2/ArtworkFilter/Utils/urlBuilder"
 
@@ -14,27 +16,23 @@ interface SearchResultsRouteProps {
 export const SearchResultsArtworksRoute: React.FC<
   SearchResultsRouteProps
 > = props => {
+  const tracking = useTracking()
+
   return (
     <Box pt={2}>
       <ArtworkFilter
         viewer={props.viewer}
         filters={props.location.query as any}
-        sortOptions={[
-          { value: "-decayed_merch", text: "Default" },
-          { value: "-partner_updated_at", text: "Recently updated" },
-          { value: "-published_at", text: "Recently added" },
-          { value: "-year", text: "Artwork year (desc.)" },
-          { value: "year", text: "Artwork year (asc.)" },
-        ]}
         onChange={updateUrl}
         onFilterClick={(key, value, filterState) => {
-          // TODO: Wire up tracking once data-team gets back to us
-          // trackEvent({
-          //   action_type: Schema.ActionType.CommercialFilterParamsChanged,
-          //   changed: { [key]: value },
-          //   current: filterState,
-          // })
+          tracking.trackEvent({
+            action_type:
+              AnalyticsSchema.ActionType.CommercialFilterParamsChanged,
+            changed: { [key]: value },
+            current: filterState,
+          })
         }}
+        ZeroState={ZeroState}
       />
     </Box>
   )
