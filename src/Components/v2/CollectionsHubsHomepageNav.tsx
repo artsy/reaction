ffd48e186c01d60c3823fefd85881447f36f1315
Steven Hicks <steven.j.hicks@gmail.com>
2019-09-19 10:10:22 -0500

    Update fake hubs data with more accurate fake images

--- src/Components/v2/CollectionsHubsHomepageNav.tsx
+++ src/Components/v2/CollectionsHubsHomepageNav.tsx
@@ -1,7 +1,7 @@
 import { CSSGrid } from "@artsy/palette"
 import { Serif } from "@artsy/palette"
 import { CollectionsHubsHomepageNav_marketingCollections } from "__generated__/CollectionsHubsHomepageNav_marketingCollections.graphql"
-import { placeholderImage } from "Components/v2/CollectionsHubsNav"
+import { placeholderImages } from "Components/v2/CollectionsHubsNav"
 import React, { FC } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { ImageLink } from "./ImageLink"
@@ -26,7 +26,7 @@ export const CollectionsHubsHomepageNav: FC<
       {props.marketingCollections.map((hub, index) => (
         <ImageLink
           to={`/collection/${hub.slug}`}
-          src={placeholderImage}
+          src={placeholderImages[index]}
           ratio={[0.49]}
           title={<Serif size={["3", "4"]}>{hub.title}</Serif>}
           subtitle={<Serif size="2">{subtitleFor(hub.title, index)}</Serif>}
