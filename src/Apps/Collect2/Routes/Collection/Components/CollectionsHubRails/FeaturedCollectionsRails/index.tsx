db115322621a7ab3d8c01e7e37674df41b55d159
xtina-starr <c.thompsonms@gmail.com>
2019-10-10 12:57:24 -0400

    format price guidance for featured collection rail

--- src/Apps/Collect2/Routes/Collection/Components/CollectionsHubRails/FeaturedCollectionsRails/index.tsx
+++ src/Apps/Collect2/Routes/Collection/Components/CollectionsHubRails/FeaturedCollectionsRails/index.tsx
@@ -14,6 +14,7 @@ import { useTracking } from "Artsy/Analytics/useTracking"
 import { RouterLink } from "Artsy/Router/RouterLink"
 import { Truncator } from "Components/Truncator"
 import { ArrowButton, Carousel } from "Components/v2"
+import currency from "currency.js"
 import React, { useEffect } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { data as sd } from "sharify"
@@ -99,6 +100,10 @@ export const FeaturedCollectionEntity: React.FC<
 > = ({ itemNumber, member }) => {
   const { description, price_guidance, slug, thumbnail, title } = member
   const { trackEvent } = useTracking()
+  const formattedPrice = currency(price_guidance, {
+    separator: ",",
+    precision: 0,
+  }).format()
 
   const handleClick = () => {
     trackEvent({
@@ -122,7 +127,7 @@ export const FeaturedCollectionEntity: React.FC<
           <Truncator maxLineCount={1}>{title}</Truncator>
         </Serif>
         {price_guidance && (
-          <Sans size="2" color="black60">{`From $${price_guidance}`}</Sans>
+          <Sans size="2" color="black60">{`From $${formattedPrice}`}</Sans>
         )}
         <ExtendedSerif size="3" mt={1}>
           <ReadMore
