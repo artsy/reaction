dc7db9b6032dfde715e967ce99aec2dcb4b1505a
Matthew Zikherman <mzikherman@gmail.com>
2019-10-16 16:34:43 -0400

    [Test] Switch ArtworkActions to use renderRelayTree

--- src/Apps/__tests__/Fixtures/Artwork/ArtworkActions.fixture.ts
+++ src/Apps/__tests__/Fixtures/Artwork/ArtworkActions.fixture.ts
@@ -5,6 +5,7 @@ export const ArtworkActionsFixture = {
   artwork: {
     __id:
       "QXJ0d29yazpwYWJsby1waWNhc3NvLWZlbW1lLWFzc2lzZS1kYW5zLXVuLWZhdXRldWlsLXRyZXNzZQ==",
+    _id: "5bae581fc60fcc53df98dfcc",
     id: "pablo-picasso-femme-assise-dans-un-fauteuil-tresse",
     is_saved: true,
     is_downloadable: true,
@@ -16,10 +17,13 @@ export const ArtworkActionsFixture = {
       },
     ],
     image: {
+      height: 540,
+      width: 540,
       id: "5aa17f397622dd4f3b006294",
       url:
         "https://d32dm0rphc51dk.cloudfront.net/sSoqyQKLQrfODvlAR-oOjw/larger.jpg",
     },
+    title: "FEMME ASSISE DANS UN FAUTEUIL TRESSE",
     images: [
       {
         url:
@@ -34,7 +38,13 @@ export const ArtworkActionsFixture = {
           "https://d32dm0rphc51dk.cloudfront.net/d_bpX6nAmEDYjNPM1CshMQ/larger.jpg",
       },
     ],
+    date: "1979-1982",
+    dimensions: {
+      in: "30 × 22 in",
+      cm: "76.2 × 55.9 cm",
+    },
     artworkMeta: {
+      share: "Share this please",
       description:
         "Available for sale from Gallery Art, Pablo Picasso, FEMME ASSISE DANS UN FAUTEUIL TRESSE (1979-1982), LITHOGRAPH ON ARCHES PAPER, 30 × 22 in",
     },
