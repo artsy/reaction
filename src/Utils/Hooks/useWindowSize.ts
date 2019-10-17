e801b71737815bcb842ad6adbe153411136e8d72
Ashley Jelks <arjelks@gmail.com>
2019-10-17 14:55:12 +0200

    add a check for window obj before useLayoutEffect hook

--- src/Utils/Hooks/useWindowSize.ts
+++ src/Utils/Hooks/useWindowSize.ts
@@ -2,18 +2,22 @@ import { useLayoutEffect, useState } from "react"
 import { getViewportDimensions } from "Utils/viewport"
 
 export const useWindowSize = () => {
-  const [size, setSize] = useState({ width: 0, height: 0 })
+  if (typeof window !== "undefined") {
+    const [size, setSize] = useState({ width: 0, height: 0 })
 
-  useLayoutEffect(() => {
-    function resize() {
-      const { width, height } = getViewportDimensions()
+    useLayoutEffect(() => {
+      function resize() {
+        const { width, height } = getViewportDimensions()
 
-      setSize({ width, height })
-    }
-    window.addEventListener("resize", resize)
-    resize()
-    return () => window.removeEventListener("resize", resize)
-  }, [])
+        setSize({ width, height })
+      }
+      window.addEventListener("resize", resize)
+      resize()
+      return () => window.removeEventListener("resize", resize)
+    }, [])
 
-  return size
+    return size
+  } else {
+    return { width: 0, height: 0 }
+  }
 }
