6038b68b8dfcce87ce573ef5a4238ec0f24297de
david huang <jiahaodavid4040@gmail.com>
2019-10-17 14:05:49 -0400

    init

--- src/Components/v2/CollectionsHubsHomepageNav.tsx
+++ src/Components/v2/CollectionsHubsHomepageNav.tsx
@@ -1,8 +1,10 @@
 import { CSSGrid } from "@artsy/palette"
 import { Serif } from "@artsy/palette"
 import { CollectionsHubsHomepageNav_marketingHubCollections } from "__generated__/CollectionsHubsHomepageNav_marketingHubCollections.graphql"
-import React, { FC } from "react"
+import { AnalyticsSchema } from "Artsy/Analytics"
+import React, { useEffect } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
+import track, { useTracking } from "react-tracking"
 import { resize } from "Utils/resizer"
 import { ImageLink } from "./ImageLink"
 
@@ -10,32 +12,53 @@ interface CollectionsHubsHomepageNavProps {
   marketingHubCollections: CollectionsHubsHomepageNav_marketingHubCollections
 }
 
-export const CollectionsHubsHomepageNav: FC<
-  CollectionsHubsHomepageNavProps
-> = props => {
-  return (
-    <CSSGrid
-      as="aside"
-      gridTemplateColumns={[
-        "repeat(2, 1fr)",
-        "repeat(2, 1fr)",
-        "repeat(3, 1fr)",
-      ]}
-      gridGap={20}
-    >
-      {props.marketingHubCollections.slice(0, 6).map(hub => (
-        <ImageLink
-          to={`/collection/${hub.slug}`}
-          src={resize(hub.thumbnail, { width: 357, height: 175 })}
-          ratio={[0.49]}
-          title={<Serif size={["3", "4"]}>{hub.title}</Serif>}
-          subtitle={<Serif size="2">{subtitleFor(hub.title)}</Serif>}
-          key={hub.id}
-        />
-      ))}
-    </CSSGrid>
-  )
-}
+export const CollectionsHubsHomepageNav = track()(
+  (props: CollectionsHubsHomepageNavProps) => {
+    const { trackEvent } = useTracking()
+    useEffect(() => {
+      trackEvent({
+        action_type: AnalyticsSchema.ActionType.Impression,
+        context_page: AnalyticsSchema.PageName.HomePage,
+        context_module: AnalyticsSchema.ContextModule.CollectionHubEntryPoint,
+        subject: AnalyticsSchema.Subject.FeaturedCategories,
+      })
+    }, [])
+
+    return (
+      <CSSGrid
+        as="aside"
+        gridTemplateColumns={[
+          "repeat(2, 1fr)",
+          "repeat(2, 1fr)",
+          "repeat(3, 1fr)",
+        ]}
+        gridGap={20}
+      >
+        {props.marketingHubCollections.slice(0, 6).map(hub => (
+          <ImageLink
+            to={`/collection/${hub.slug}`}
+            src={resize(hub.thumbnail, { width: 357, height: 175 })}
+            ratio={[0.49]}
+            title={<Serif size={["3", "4"]}>{hub.title}</Serif>}
+            subtitle={<Serif size="2">{subtitleFor(hub.title)}</Serif>}
+            key={hub.id}
+            onClick={() => {
+              trackEvent({
+                action_type: AnalyticsSchema.ActionType.Click,
+                context_page: AnalyticsSchema.PageName.HomePage,
+                context_module:
+                  AnalyticsSchema.ContextModule.CollectionHubEntryPoint,
+                subject: AnalyticsSchema.Subject.FeaturedCategories,
+                type: AnalyticsSchema.Type.Thumbnail,
+                desination_path: `/collection/${hub.slug}`,
+              })
+            }}
+          />
+        ))}
+      </CSSGrid>
+    )
+  }
+)
 
 export const CollectionsHubsHomepageNavFragmentContainer = createFragmentContainer(
   CollectionsHubsHomepageNav,
