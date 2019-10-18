81b94901a6d2e7919c81940a75823d354386eced
Ashley Jelks <arjelks@gmail.com>
2019-10-18 13:03:13 +0200

    return func early if no window

--- src/Utils/Hooks/useWindowSize.ts
+++ src/Utils/Hooks/useWindowSize.ts
@@ -2,22 +2,22 @@ import { useLayoutEffect, useState } from "react"
 import { getViewportDimensions } from "Utils/viewport"
 
 export const useWindowSize = () => {
-  if (typeof window !== "undefined") {
-    const [size, setSize] = useState({ width: 0, height: 0 })
-
-    useLayoutEffect(() => {
-      function resize() {
-        const { width, height } = getViewportDimensions()
-
-        setSize({ width, height })
-      }
-      window.addEventListener("resize", resize)
-      resize()
-      return () => window.removeEventListener("resize", resize)
-    }, [])
-
-    return size
-  } else {
-    return { width: 0, height: 0 }
+  if (typeof window === "undefined") {
+    return null
   }
+
+  const [size, setSize] = useState({ width: 0, height: 0 })
+
+  useLayoutEffect(() => {
+    function resize() {
+      const { width, height } = getViewportDimensions()
+
+      setSize({ width, height })
+    }
+    window.addEventListener("resize", resize)
+    resize()
+    return () => window.removeEventListener("resize", resize)
+  }, [])
+
+  return size
 }
