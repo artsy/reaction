e9290ba16ed91946c6c6cd2ef69995823e8610cb
Matthew Zikherman <mzikherman@gmail.com>
2019-10-23 17:08:36 -0400

    [ArtworkSidebarArtists] Update story to remove RelayStubProvider

--- src/Apps/__tests__/Fixtures/Artwork/ArtworkSidebar/ArtworkSidebarArtists.ts
+++ src/Apps/__tests__/Fixtures/Artwork/ArtworkSidebar/ArtworkSidebarArtists.ts
@@ -2,11 +2,13 @@ export const SingleFollowedArtist = {
   artists: [
     {
       __id: "QXJ0aXN0Ompvc2VmLWFsYmVycw==",
+      _id: "artist_id",
       id: "josef-albers",
       name: "Josef Albers",
       href: "/artist/josef-albers",
       is_followed: false,
       counts: { follows: 9346 },
+      related: null,
     },
   ],
   cultural_maker: null,
@@ -15,12 +17,14 @@ export const SingleFollowedArtist = {
 export const SingleNonFollowedArtist = {
   artists: [
     {
+      _id: "artist_id",
       __id: "QXJ0aXN0Ompvc2VmLWFsYmVycw==",
       id: "josef-albers",
       name: "Josef Albers",
       href: "/artist/josef-albers",
       is_followed: false,
       counts: { follows: 9346 },
+      related: null,
     },
   ],
   cultural_maker: null,
@@ -30,19 +34,23 @@ export const MultipleArtists = {
   artists: [
     {
       __id: "QXJ0aXN0Ompvc2VmLWFsYmVycw==",
+      _id: "artist_0_id",
       id: "josef-albers",
       name: "Josef Albers",
       href: "/artist/josef-albers",
       is_followed: false,
       counts: { follows: 9346 },
+      related: null,
     },
     {
       __id: "QXJ0aXN0OmVkLXJ1c2NoYQ==",
+      _id: "artist_1_id",
       id: "ed-ruscha",
       name: "Ed Ruscha",
       href: "/artist/ed-ruscha",
       is_followed: false,
       counts: { follows: 15431 },
+      related: null,
     },
   ],
   cultural_maker: null,
