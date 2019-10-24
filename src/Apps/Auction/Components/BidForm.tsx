feec3c5150d4f0e68ed1362b50c520de7003a45f
Erik Stockmeier <erik.d.stock@gmail.com>
2019-10-24 13:02:05 -0400

    clean up stories

--- src/Apps/Auction/Components/BidForm.tsx
+++ src/Apps/Auction/Components/BidForm.tsx
@@ -69,79 +69,81 @@ export const BidForm: React.FC<Props> = ({
   const selectedBid = getSelectedBid({ initialSelectedBid, displayIncrements })
 
   return (
-    <Formik<FormValues>
-      initialValues={{
-        selectedBid,
-        agreeToTerms: false,
-      }}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-      render={({
-        values,
-        touched,
-        errors,
-        isSubmitting,
-        setFieldValue,
-        setFieldTouched,
-      }) => {
-        return (
-          <Form>
-            <Flex flexDirection="column">
-              <Flex flexDirection="column" py={4}>
-                <Serif pb={0.5} size="4t" weight="semibold" color="black100">
-                  Set your max bid
-                </Serif>
-                <LargeSelect
-                  selected={values.selectedBid}
-                  onSelect={value => {
-                    setFieldValue("selectedBid", value)
-                    setFieldTouched("selectedBid")
-                  }}
-                  options={displayIncrements}
-                />
-                {touched.selectedBid && errors.selectedBid && (
-                  <Sans mt={1} color="red100" size="2">
-                    {errors.selectedBid}
-                  </Sans>
-                )}
-                {showPricingTransparency && <PricingTransparency />}
-              </Flex>
-              <Separator />
-              <Flex
-                py={3}
-                flexDirection="column"
-                justifyContent="center"
-                width="100%"
-              >
-                <Box mx="auto" mb={3}>
-                  <ConditionsOfSaleCheckbox
-                    selected={values.agreeToTerms}
+    <Box maxWidth={550}>
+      <Formik<FormValues>
+        initialValues={{
+          selectedBid,
+          agreeToTerms: false,
+        }}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+        render={({
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          setFieldValue,
+          setFieldTouched,
+        }) => {
+          return (
+            <Form>
+              <Flex flexDirection="column">
+                <Flex flexDirection="column" py={4}>
+                  <Serif pb={0.5} size="4t" weight="semibold" color="black100">
+                    Set your max bid
+                  </Serif>
+                  <LargeSelect
+                    selected={values.selectedBid}
                     onSelect={value => {
-                      setFieldValue("agreeToTerms", value)
-                      setFieldTouched("agreeToTerms")
+                      setFieldValue("selectedBid", value)
+                      setFieldTouched("selectedBid")
                     }}
+                    options={displayIncrements}
                   />
-                  {touched.agreeToTerms && errors.agreeToTerms && (
-                    <Sans mt={1} color="red100" size="2" textAlign="center">
-                      {errors.agreeToTerms}
+                  {touched.selectedBid && errors.selectedBid && (
+                    <Sans mt={1} color="red100" size="2">
+                      {errors.selectedBid}
                     </Sans>
                   )}
-                </Box>
-                <Button
-                  size="large"
-                  mt={3}
+                  {showPricingTransparency && <PricingTransparency />}
+                </Flex>
+                <Separator />
+                <Flex
+                  py={3}
+                  flexDirection="column"
+                  justifyContent="center"
                   width="100%"
-                  loading={isSubmitting}
-                  {...{ type: "submit" } as any}
                 >
-                  Confirm bid
-                </Button>
+                  <Box mx="auto" mb={3}>
+                    <ConditionsOfSaleCheckbox
+                      selected={values.agreeToTerms}
+                      onSelect={value => {
+                        setFieldValue("agreeToTerms", value)
+                        setFieldTouched("agreeToTerms")
+                      }}
+                    />
+                    {touched.agreeToTerms && errors.agreeToTerms && (
+                      <Sans mt={1} color="red100" size="2" textAlign="center">
+                        {errors.agreeToTerms}
+                      </Sans>
+                    )}
+                  </Box>
+                  <Button
+                    size="large"
+                    mt={3}
+                    width="100%"
+                    loading={isSubmitting}
+                    {...{ type: "submit" } as any}
+                  >
+                    Confirm bid
+                  </Button>
+                </Flex>
               </Flex>
-            </Flex>
-          </Form>
-        )
-      }}
-    />
+            </Form>
+          )
+        }}
+      />
+    </Box>
   )
 }
 
