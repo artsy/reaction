08a09934497e7a4fe22269c924ba293dbf38bd81
david huang <jiahaodavid4040@gmail.com>
2019-10-25 16:39:39 -0400

    Add dispatch

--- src/Components/v2/CollectionsHubsHomepageNav.tsx
+++ src/Components/v2/CollectionsHubsHomepageNav.tsx
@@ -5,6 +5,7 @@ import { AnalyticsSchema } from "Artsy/Analytics"
 import React, { useEffect } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import track, { useTracking } from "react-tracking"
+import Events from "Utils/Events"
 import { resize } from "Utils/resizer"
 import { ImageLink } from "./ImageLink"
 
@@ -12,53 +13,54 @@ interface CollectionsHubsHomepageNavProps {
   marketingHubCollections: CollectionsHubsHomepageNav_marketingHubCollections
 }
 
-export const CollectionsHubsHomepageNav = track()(
-  (props: CollectionsHubsHomepageNavProps) => {
-    const { trackEvent } = useTracking()
-    useEffect(() => {
-      trackEvent({
-        action_type: AnalyticsSchema.ActionType.Impression,
-        context_page: AnalyticsSchema.PageName.HomePage,
-        context_module: AnalyticsSchema.ContextModule.CollectionHubEntryPoint,
-        subject: AnalyticsSchema.Subject.FeaturedCategories,
-      })
-    }, [])
+export const CollectionsHubsHomepageNav = track(
+  {},
+  { dispatch: data => Events.postEvent(data) }
+)((props: CollectionsHubsHomepageNavProps) => {
+  const { trackEvent } = useTracking()
+  useEffect(() => {
+    trackEvent({
+      action_type: AnalyticsSchema.ActionType.Impression,
+      context_page: AnalyticsSchema.PageName.HomePage,
+      context_module: AnalyticsSchema.ContextModule.CollectionHubEntryPoint,
+      subject: AnalyticsSchema.Subject.FeaturedCategories,
+    })
+  }, [])
 
-    return (
-      <CSSGrid
-        as="aside"
-        gridTemplateColumns={[
-          "repeat(2, 1fr)",
-          "repeat(2, 1fr)",
-          "repeat(3, 1fr)",
-        ]}
-        gridGap={20}
-      >
-        {props.marketingHubCollections.slice(0, 6).map(hub => (
-          <ImageLink
-            to={`/collection/${hub.slug}`}
-            src={resize(hub.thumbnail, { width: 357, height: 175 })}
-            ratio={[0.49]}
-            title={<Serif size={["3", "4"]}>{hub.title}</Serif>}
-            subtitle={<Serif size="2">{subtitleFor(hub.title)}</Serif>}
-            key={hub.id}
-            onClick={() => {
-              trackEvent({
-                action_type: AnalyticsSchema.ActionType.Click,
-                context_page: AnalyticsSchema.PageName.HomePage,
-                context_module:
-                  AnalyticsSchema.ContextModule.CollectionHubEntryPoint,
-                subject: AnalyticsSchema.Subject.FeaturedCategories,
-                type: AnalyticsSchema.Type.Thumbnail,
-                desination_path: `/collection/${hub.slug}`,
-              })
-            }}
-          />
-        ))}
-      </CSSGrid>
-    )
-  }
-)
+  return (
+    <CSSGrid
+      as="aside"
+      gridTemplateColumns={[
+        "repeat(2, 1fr)",
+        "repeat(2, 1fr)",
+        "repeat(3, 1fr)",
+      ]}
+      gridGap={20}
+    >
+      {props.marketingHubCollections.slice(0, 6).map(hub => (
+        <ImageLink
+          to={`/collection/${hub.slug}`}
+          src={resize(hub.thumbnail, { width: 357, height: 175 })}
+          ratio={[0.49]}
+          title={<Serif size={["3", "4"]}>{hub.title}</Serif>}
+          subtitle={<Serif size="2">{subtitleFor(hub.title)}</Serif>}
+          key={hub.id}
+          onClick={() => {
+            trackEvent({
+              action_type: AnalyticsSchema.ActionType.Click,
+              context_page: AnalyticsSchema.PageName.HomePage,
+              context_module:
+                AnalyticsSchema.ContextModule.CollectionHubEntryPoint,
+              subject: AnalyticsSchema.Subject.FeaturedCategories,
+              type: AnalyticsSchema.Type.Thumbnail,
+              desination_path: `/collection/${hub.slug}`,
+            })
+          }}
+        />
+      ))}
+    </CSSGrid>
+  )
+})
 
 export const CollectionsHubsHomepageNavFragmentContainer = createFragmentContainer(
   CollectionsHubsHomepageNav,
