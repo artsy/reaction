faab9f4bf48d35e1cc5e6e492334060aab0c3e57
Justin Bennett <zephraph@gmail.com>
2019-10-25 17:10:43 -0400

    Upgrade betterer, add better betterer results

--- .betterer.js
+++ .betterer.js
@@ -2,9 +2,7 @@ const { typescriptBetterer } = require("@betterer/typescript")
 
 module.exports = {
   "stricter compilation": typescriptBetterer("./tsconfig.json", {
-    useStrict: true,
     strict: true,
-    pretty: true,
     noEmit: true,
   }),
 }
