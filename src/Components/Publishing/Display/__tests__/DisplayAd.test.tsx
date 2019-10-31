6909f4b4edd15d00d595eab7191187849431904c
Ashley Jelks <arjelks@gmail.com>
2019-10-31 18:13:45 +0100

    updates tests to support mock tracking

--- src/Components/Publishing/Display/__tests__/DisplayAd.test.tsx
+++ src/Components/Publishing/Display/__tests__/DisplayAd.test.tsx
@@ -1,3 +1,4 @@
+import { useTracking } from "Artsy/Analytics/useTracking"
 import {
   DisplayAd,
   DisplayAdProps,
@@ -11,51 +12,89 @@ import { Bling as GPT } from "react-gpt"
 import renderer from "react-test-renderer"
 import { StandardArticleHostedAdCanvas as AdData } from "../../Fixtures/Components"
 
-it("renders the new canvas in standard layout", () => {
-  const component = renderer
-    .create(
+jest.mock("Artsy/Analytics/useTracking")
+
+describe("Display Ad", () => {
+  const trackEvent = jest.fn()
+
+  beforeEach(() => {
+    ;(useTracking as jest.Mock).mockImplementation(() => {
+      return {
+        trackEvent,
+      }
+    })
+  })
+
+  it("renders the new canvas in standard layout", () => {
+    const component = renderer
+      .create(
+        <DisplayAd
+          adDimension={AdData.adDimension}
+          adUnit={AdData.adUnit}
+          targetingData={targetingData(StandardArticle, "article")}
+          articleSlug={StandardArticle.slug}
+        />
+      )
+      .toJSON()
+    expect(component).toMatchSnapshot()
+  })
+
+  it("renders the component with the correct data and properties in standard layout articles", () => {
+    const ad = mount(
       <DisplayAd
         adDimension={AdData.adDimension}
         adUnit={AdData.adUnit}
         targetingData={targetingData(StandardArticle, "article")}
+        articleSlug={StandardArticle.slug}
       />
     )
-    .toJSON()
-  expect(component).toMatchSnapshot()
-})
 
-it("renders the component with the correct data and properties in standard layout articles", () => {
-  const ad = mount(
-    <DisplayAd
-      adDimension={AdData.adDimension}
-      adUnit={AdData.adUnit}
-      targetingData={targetingData(StandardArticle, "article")}
-    />
-  )
-
-  const adProps: DisplayAdProps = ad.props()
-  expect(adProps.adDimension).toEqual("970x250")
-  expect(adProps.adUnit).toEqual("Desktop_TopLeaderboard")
-  expect(ad).toHaveLength(1)
-})
-
-it("renders GPT with the correct properties in standard layout articles", () => {
-  const ad = mount(
-    <DisplayAd
-      adDimension={AdData.adDimension}
-      adUnit={AdData.adUnit}
-      targetingData={targetingData(StandardArticle, "article")}
-    />
-  )
-
-  const gptComponent = ad.find(GPT)
-  const gptProps: any = gptComponent.props()
-  expect(gptProps.adUnitPath).toEqual("/21805539690/Desktop_TopLeaderboard")
-  expect(gptProps.targeting).toEqual({
-    is_testing: true,
-    page_type: "article",
-    post_id: "594a7e2254c37f00177c0ea9",
-    tags: "Art Market",
+    const adProps: DisplayAdProps = ad.props()
+    expect(adProps.adDimension).toEqual("970x250")
+    expect(adProps.adUnit).toEqual("Desktop_TopLeaderboard")
+    expect(ad).toHaveLength(1)
   })
-  expect(gptProps.slotSize).toEqual([970, 250])
+
+  it("renders GPT with the correct properties in standard layout articles", () => {
+    const ad = mount(
+      <DisplayAd
+        adDimension={AdData.adDimension}
+        adUnit={AdData.adUnit}
+        targetingData={targetingData(StandardArticle, "article")}
+        articleSlug={StandardArticle.slug}
+      />
+    )
+
+    const gptComponent = ad.find(GPT)
+    const gptProps: any = gptComponent.props()
+    expect(gptProps.adUnitPath).toEqual("/21805539690/Desktop_TopLeaderboard")
+    expect(gptProps.targeting).toEqual({
+      is_testing: true,
+      page_type: "article",
+      post_id: "594a7e2254c37f00177c0ea9",
+      tags: "Art Market",
+    })
+    expect(gptProps.slotSize).toEqual([970, 250])
+  })
+
+  it("Tracks display ad click", () => {
+    const component = mount(
+      <DisplayAd
+        adDimension={AdData.adDimension}
+        adUnit={AdData.adUnit}
+        targetingData={targetingData(StandardArticle, "article")}
+        articleSlug={StandardArticle.slug}
+      />
+    )
+
+    component.at(0).simulate("click")
+
+    expect(trackEvent).toBeCalledWith({
+      context_page: "Article",
+      context_module: "AdServer",
+      context_page_owner_type: "Article",
+      context_page_owner_id: StandardArticle.id,
+      context_page_owner_slug: StandardArticle.slug,
+    })
+  })
 })
