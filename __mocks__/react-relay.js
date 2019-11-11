6e0b7f2873fb8ede6bfd8dbf29b30200330b2662
Eloy Durán <eloy.de.enige@gmail.com>
2019-11-11 23:32:12 +0100

    [mock] Document noop `graphql` tagged template
    [skip ci]

--- __mocks__/react-relay.js
+++ __mocks__/react-relay.js
@@ -1,8 +1,11 @@
 // Comes from https://github.com/facebook/relay/issues/161
 // Should be used in tests as a replacement for `react-storybooks-relay-container`
-//
+
 var React = require.requireActual("react")
 
+// This is a template string function, which returns the original string
+// It's based on https://github.com/lleaff/tagged-template-noop
+// Which is MIT licensed to lleaff
 const graphql = (strings, ...keys) => {
   const lastIndex = strings.length - 1
   return (
