bd524d4bbc46ad5fc0d410006cc7436c4802878d
Yuki Nishijima <yk.nishijima@gmail.com>
2019-11-15 16:45:19 -0500

    Consolidate credit card instructions copy

--- src/Apps/Auction/Components/BidForm.tsx
+++ src/Apps/Auction/Components/BidForm.tsx
@@ -17,6 +17,7 @@ import React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import Yup from "yup"
 
+import { CreditCardInstructions } from "Apps/Auction/Components/CreditCardInstructions"
 import { Address, AddressForm } from "Apps/Order/Components/AddressForm"
 import { CreditCardInput } from "Apps/Order/Components/CreditCardInput"
 
@@ -184,17 +185,7 @@ export const BidForm: React.FC<Props> = ({
                 {requiresPaymentInformation && (
                   <Box>
                     <Separator mb={3} />
-
-                    <Serif size="4" color="black100">
-                      Please enter your credit card information below. The name
-                      on your Artsy account must match the name on the card, and
-                      a valid credit card is required in order to bid.
-                    </Serif>
-                    <Serif size="4" mt={2} color="black100">
-                      Registration is free. Artsy will never charge this card
-                      without your permission, and you are not required to use
-                      this card to pay if you win.
-                    </Serif>
+                    <CreditCardInstructions />
 
                     <Serif
                       mt={4}
