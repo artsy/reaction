67334c579705aadd5445ebae696e65ef515f2710
Lily Pacegit <lilyfpace@gmail.com>
2019-11-18 13:58:59 -0500

    added coa modal, updated TrustSignal to accept onClick

--- src/Apps/Artwork/Components/TrustSignals/TrustSignal.tsx
+++ src/Apps/Artwork/Components/TrustSignals/TrustSignal.tsx
@@ -1,25 +1,29 @@
-import { Flex, FlexProps, Sans } from "@artsy/palette"
+import { Flex, FlexProps, Link, Sans } from "@artsy/palette"
 import React, { FC } from "react"
 
 export interface TrustSignalProps extends Omit<FlexProps, "flexDirection"> {
   Icon: JSX.Element
   label: string
   description: string | JSX.Element
+  onClick?: () => void
 }
 
 export const TrustSignal: FC<TrustSignalProps> = ({
   Icon,
   label,
   description,
+  onClick,
   ...other
 }) => {
   return (
     <Flex {...other}>
       {Icon}
       <Flex flexDirection="column" ml={1}>
-        <Sans size="2" weight="medium" color="black100">
-          {label}
-        </Sans>
+        <Link onClick={onClick}>
+          <Sans size="2" weight="medium" color="black100">
+            {label}
+          </Sans>
+        </Link>
         <Sans size="2" color="black60">
           {description}
         </Sans>
