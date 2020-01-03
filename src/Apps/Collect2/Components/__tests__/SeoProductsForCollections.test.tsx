0b4d550fc6674585839d536749311dc21316fa5d
Steven Hicks <steven.j.hicks@gmail.com>
2020-01-03 12:02:23 -0600

    Refactor SEOProductsForCollections tests

--- src/Apps/Collect2/Components/__tests__/SeoProductsForCollections.test.tsx
+++ src/Apps/Collect2/Components/__tests__/SeoProductsForCollections.test.tsx
@@ -6,61 +6,75 @@ import { HeadProvider } from "react-head"
 import { SeoProductsForCollections } from "../SeoProductsForCollections"
 
 describe("Seo Products for Collection Page", () => {
+  function buildPriceRange(minPrice = 1, maxPrice = 2) {
+    return {
+      __typename: "PriceRange",
+      minPrice: {
+        major: minPrice,
+        currencyCode: "USD",
+      },
+      maxPrice: {
+        major: maxPrice,
+        currencyCode: "USD",
+      },
+    }
+  }
+
+  function buildIndividualPrice(price) {
+    return {
+      __typename: "Money",
+      major: price,
+      currencyCode: "USD",
+    }
+  }
+
+  function buildDescendingArtworks(
+    listPrice
+  ): SeoProductsForCollections_descending_artworks {
+    return {
+      " $refType": null,
+      edges: [
+        {
+          node: {
+            id: "1",
+            availability: "yes",
+            listPrice,
+          },
+        },
+      ],
+    }
+  }
+
+  function buildAscendingArtworks(
+    listPrice
+  ): SeoProductsForCollections_ascending_artworks {
+    return {
+      " $refType": null,
+      edges: [
+        {
+          node: {
+            id: "1",
+            availability: "yes",
+            listPrice,
+          },
+        },
+      ],
+    }
+  }
+
   let props
   beforeEach(() => {
     props = {
-      descending_artworks: {
-        " $refType": null,
-        edges: [
-          {
-            node: {
-              id: "1",
-              availability: "yes",
-              listPrice: {
-                __typename: "PriceRange",
-                minPrice: {
-                  major: 8800,
-                  currencyCode: "USD",
-                },
-                maxPrice: {
-                  major: 9000,
-                  currencyCode: "USD",
-                },
-              },
-            },
-          },
-        ],
-      } as SeoProductsForCollections_descending_artworks,
-      ascending_artworks: {
-        " $refType": null,
-        edges: [
-          {
-            node: {
-              id: "2",
-              availability: "yes",
-              listPrice: {
-                __typename: "PriceRange",
-                minPrice: {
-                  major: 10,
-                  currencyCode: "USD",
-                },
-                maxPrice: {
-                  major: 20,
-                  currencyCode: "USD",
-                },
-              },
-            },
-          },
-        ],
-      } as SeoProductsForCollections_ascending_artworks,
+      descending_artworks: buildDescendingArtworks(buildPriceRange(8800, 9000)),
+      ascending_artworks: buildAscendingArtworks(buildPriceRange(10, 20)),
       collectionDescription: "A fake description for collection",
       collectionURL: "A fake URL for collection",
       collectionName: "A fake name for collection",
     }
   })
 
-  it("pass data into the component, and it correctly re-shows itself on ld-json", () => {
-    const wrapper = mount(
+  const renderProducts = () => {
+    return mount(
       <HeadProvider>
         <SeoProductsForCollections
           ascending_artworks={props.ascending_artworks}
@@ -71,9 +85,34 @@ describe("Seo Products for Collection Page", () => {
         />
       </HeadProvider>
     )
+  }
+
+  it("renders collection metadata", () => {
+    const wrapper = renderProducts()
+
     const html = wrapper.html()
     expect(html).toContain('"name":"A fake name for collection"')
+    expect(html).toContain('"url":"A fake URL for collection"')
+    expect(html).toContain('"description":"A fake description for collection"')
+  })
+
+  it("renders pricing data for collections with price ranges", () => {
+    const wrapper = renderProducts()
+
+    const html = wrapper.html()
     expect(html).toContain('"lowPrice":10')
     expect(html).toContain('"highPrice":9000')
+  })
+
+  it("renders pricing data for collections with individual prices", () => {
+    props.descending_artworks = buildDescendingArtworks(
+      buildIndividualPrice(30)
+    )
+    props.ascending_artworks = buildAscendingArtworks(buildIndividualPrice(15))
+    const wrapper = renderProducts()
+
+    const html = wrapper.html()
+    expect(html).toContain('"lowPrice":15')
+    expect(html).toContain('"highPrice":30')
   })
 })
