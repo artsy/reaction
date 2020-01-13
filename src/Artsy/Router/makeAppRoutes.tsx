7f040d6e175e3c8e7c443c8f4072910279cbbe25
Matthew Zikherman <mzikherman@gmail.com>
2020-01-13 16:09:38 -0500

    [PageView Tracking] Ensure pageviews are counted when navigating to subsequent pages of the same type

--- src/Artsy/Router/makeAppRoutes.tsx
+++ src/Artsy/Router/makeAppRoutes.tsx
@@ -1,8 +1,9 @@
-import { RouteConfig } from "found"
+import { RouteConfig, withRouter } from "found"
 import { flatten } from "lodash"
 import React, { useEffect } from "react"
 
 import { AppShell } from "Apps/Components/AppShell"
+import { trackPageView } from "Artsy"
 import { useSystemContext } from "Artsy/SystemContext"
 import { catchLinks } from "Utils/catchLinks"
 
@@ -44,39 +45,50 @@ export function makeAppRoutes(routeList: RouteList[]): RouteConfig[] {
     createRouteConfiguration
   )
 
+  const Component = props => {
+    const { router, setRouter } = useSystemContext()
+    // Store global reference to router instance
+    useEffect(() => {
+      if (props.router !== router) {
+        setRouter(props.router)
+      }
+
+      /**
+       * Intercept <a> tags on page and if contained within router route
+       * manifest, navigate via router versus doing a hard jump between pages.
+       */
+      catchLinks(window, href => {
+        const url = ROUTE_NAMESPACE + href
+        const foundUrl = props.router.matcher.matchRoutes(routes, url)
+        // Make sure to track pageviews when staying on the same page type.
+        // Collection pages link to each other using `RouterLink` which already
+        // handles this, so just in case avoid double triggering.
+        if (foundUrl) {
+          const toPath = url.toString()
+          // TODO: Figure out why the router prop is stale.
+          // const currentPageType = props.match.location.pathname.split("/")[1]
+          const currentPageType = window.location.pathname.split("/")[1]
+          const toPageType = toPath.split("/")[1]
+          if (currentPageType === toPageType && toPageType !== "collection") {
+            trackPageView({ path: toPath })
+          }
+          props.router.push(url)
+        } else {
+          window.location.assign(url)
+        }
+      })
+    }, [])
+
+    return <AppShell {...props} />
+  }
+
   // Return a top-level "meta" route containing all global sub-routes, which is
   // then mounted into the router.
   return [
     {
       path: ROUTE_NAMESPACE,
 
-      Component: props => {
-        const { router, setRouter } = useSystemContext()
-
-        // Store global reference to router instance
-        useEffect(() => {
-          if (props.router !== router) {
-            setRouter(props.router)
-          }
-
-          /**
-           * Intercept <a> tags on page and if contained within router route
-           * manifest, navigate via router versus doing a hard jump between pages.
-           */
-          catchLinks(window, href => {
-            const url = ROUTE_NAMESPACE + href
-            const foundUrl = props.router.matcher.matchRoutes(routes, url)
-
-            if (foundUrl) {
-              props.router.push(url)
-            } else {
-              window.location.assign(url)
-            }
-          })
-        }, [])
-
-        return <AppShell {...props} />
-      },
+      Component: withRouter(Component),
       children: routes,
     },
   ]
