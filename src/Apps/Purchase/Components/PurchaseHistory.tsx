4e59c3bf94f814e904345f95f9fd30d9e7a76149
sepans <sepans@sepans.com>
2020-01-17 10:04:02 -0500

    PR comments

--- src/Apps/Purchase/Components/PurchaseHistory.tsx
+++ src/Apps/Purchase/Components/PurchaseHistory.tsx
@@ -20,69 +20,71 @@ const OrderRow = (props: OrderRowProps) => {
   const { order } = props
   const artwork = get(order, o => o.lineItems.edges[0].node.artwork)
 
+  if (!artwork) {
+    return null
+  }
+
   return (
-    artwork && (
-      <Box p={1}>
-        <StackableBorderBox
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Flex width="350px">
-            <Flex height="auto" alignItems="center">
-              <Image
-                src={get(artwork, a => a.image.resized.url)}
-                width="55px"
-                mr={1}
-              />
-            </Flex>
-            <Flex
-              flexDirection="column"
-              justifyContent="center"
-              style={{ overflow: "hidden" }}
-            >
-              <Link
-                href={`/orders/${order.internalID}/status`}
-                underlineBehavior="hover"
-              >
-                <Serif size="2" weight="semibold">
-                  {artwork.artist_names}
-                </Serif>
-              </Link>
-              <div style={{ lineHeight: "1" }}>
-                <Serif italic size="2" color="black60" display="inline">
-                  {artwork.title}, {artwork.date}
-                </Serif>
-              </div>
-            </Flex>
+    <Box p={1}>
+      <StackableBorderBox
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Flex width="350px">
+          <Flex height="auto" alignItems="center">
+            <Image
+              src={get(artwork, a => a.image.resized.url)}
+              width="55px"
+              mr={1}
+            />
           </Flex>
-          <Flex flexDirection="column" justifyContent="center">
-            <Sans
-              size="2"
-              weight="medium"
-              style={{ textTransform: "capitalize" }}
-            >
-              {order.mode.toLowerCase()}
-            </Sans>
-            <Sans
-              size="2"
-              color="black60"
-              style={{ textTransform: "capitalize" }}
-            >
-              {order.state.toLowerCase()}
-            </Sans>
-          </Flex>
-          <Flex>
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            style={{ overflow: "hidden" }}
+          >
             <Link
               href={`/orders/${order.internalID}/status`}
               underlineBehavior="hover"
             >
-              <Button variant="secondaryGray">View details</Button>
+              <Serif size="2" weight="semibold">
+                {artwork.artist_names}
+              </Serif>
             </Link>
+            <div style={{ lineHeight: "1" }}>
+              <Serif italic size="2" color="black60" display="inline">
+                {artwork.title}, {artwork.date}
+              </Serif>
+            </div>
           </Flex>
-        </StackableBorderBox>
-      </Box>
-    )
+        </Flex>
+        <Flex flexDirection="column" justifyContent="center">
+          <Sans
+            size="2"
+            weight="medium"
+            style={{ textTransform: "capitalize" }}
+          >
+            {order.mode.toLowerCase()}
+          </Sans>
+          <Sans
+            size="2"
+            color="black60"
+            style={{ textTransform: "capitalize" }}
+          >
+            {order.state.toLowerCase()}
+          </Sans>
+        </Flex>
+        <Flex>
+          <Link
+            href={`/orders/${order.internalID}/status`}
+            underlineBehavior="hover"
+          >
+            <Button variant="secondaryGray">View details</Button>
+          </Link>
+        </Flex>
+      </StackableBorderBox>
+    </Box>
   )
 }
 
