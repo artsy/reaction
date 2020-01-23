3bd1a1e3f1e31b7683b81bc60a978e7aef604057
Matthew Zikherman <mzikherman@gmail.com>
2020-01-23 12:00:07 -0500

    [Analytics] More explicitly track AB test events

---
+++ src/Artsy/Analytics/trackExperimentViewed.tsx
@@ -0,0 +1,20 @@
+import * as Schema from "Artsy/Analytics"
+import { data as sd } from "sharify"
+
+declare const window: any
+export const trackExperimentViewed = (name: string) => {
+  if (typeof window.analytics !== "undefined") {
+    const variation = sd[name.toUpperCase()]
+    if (!Boolean(variation))
+      return console.warn(`experiment value for ${name} not found, skipping`)
+
+    window.analytics.track({
+      action_type: Schema.ActionType.ExperimentViewed,
+      experiment_id: name,
+      experiment_name: name,
+      variation_id: variation,
+      variation_name: variation,
+      nonInteraction: 1,
+    })
+  }
+}
