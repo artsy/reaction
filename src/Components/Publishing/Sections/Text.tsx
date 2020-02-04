1f0cb10f5a7e4388d51dfdf362432415229599f5
Eve Essex <eve.essex@gmail.com>
2020-02-04 12:27:09 -0500

    article text components use relay follow button
    use relay follow buttons

--- src/Components/Publishing/Sections/Text.tsx
+++ src/Components/Publishing/Sections/Text.tsx
@@ -1,6 +1,8 @@
 import ReactHtmlParser, { convertNodeToElement } from "@artsy/react-html-parser"
 import { startsWith } from "lodash"
+import PropTypes from "prop-types"
 import React, { Component } from "react"
+import styled from "styled-components"
 import { get } from "Utils/get"
 import { ArticleLayout } from "../Typings"
 import { StyledText } from "./StyledText"
@@ -23,6 +25,11 @@ export class Text extends Component<Props, State> {
   static defaultProps = {
     color: "black",
     showTooltips: false,
+  }
+
+  static contextTypes = {
+    tooltipsData: PropTypes.object,
+    onOpenAuthModal: PropTypes.func,
   }
 
   state = {
@@ -72,12 +79,76 @@ export class Text extends Component<Props, State> {
     return false
   }
 
-  transformNode = (node: Element, index) => {
-    const { color } = this.props
+  renderLinkWithToolTip = (node: Element, index) => {
     // Dont include relay components unless necessary
     // To avoid 'regeneratorRuntime' error
     const LinkWithTooltip = require("../ToolTip/LinkWithTooltip").default
+    const linkNode = node.childNodes[0]
 
+    if (linkNode && linkNode.textContent) {
+      const href = node.getAttribute("href")
+      const { color } = this.props
+      const props = { key: href + index, url: href, color }
+      const next = node.nextSibling
+      const text = linkNode.textContent
+
+      // Check to see if there's an apostrophe following a linked section of
+      // text and if found, return it.
+      const apostrophe = get(next, n => {
+        const str = n.textContent.substr(0, 2)
+        if (/[’'][a-zA-Z]/.test(str)) {
+          return str
+        }
+      })
+
+      if (apostrophe) {
+        // Remove the apostrophe from the original text
+        next.textContent = next.textContent.substring(2)
+        // And wrap the whole thing with a span preventing whitespace breaks
+        return (
+          <span
+            className="preventLineBreak"
+            key={`apostropheLinkNode-${props.key}`}
+          >
+            <LinkWithTooltip {...props}>{text}</LinkWithTooltip>
+            {apostrophe}
+          </span>
+        )
+      }
+
+      return <LinkWithTooltip {...props}>{text}</LinkWithTooltip>
+    }
+  }
+
+  isArtistFollow = (node: Element) => {
+    return (node.getAttribute("class") || "").includes("artist-follow")
+  }
+
+  artistIdToArtistFollow = (artistId: string) => {
+    const data = this.context.tooltipsData
+    if (!data || !data.artists) return null
+    return data.artists[artistId]
+  }
+
+  renderArtistFollowButton = (node: Element) => {
+    // Dont include relay components unless necessary
+    // To avoid 'regeneratorRuntime' error
+    const FollowButton = require("../../FollowButton/FollowArtistButton")
+      .FollowArtistButtonFragmentContainer
+
+    const { onOpenAuthModal } = this.context
+    const artistId = node.getAttribute("data-id")
+    const artist = this.artistIdToArtistFollow(artistId)
+    const props = { artist, onOpenAuthModal }
+
+    return (
+      <FollowContainer>
+        <FollowButton {...props} />
+      </FollowContainer>
+    )
+  }
+
+  transformNode = (node: Element, index) => {
     if (node.tagName === "P") {
       const newNode = node.ownerDocument.createElement("div")
       newNode.setAttribute("class", "paragraph")
@@ -85,40 +156,12 @@ export class Text extends Component<Props, State> {
       return convertNodeToElement(newNode, index, this.transformNode)
     }
 
-    if (node.tagName === "A" && this.shouldShowTooltipForURL(node)) {
-      const href = node.getAttribute("href")
-      const linkNode = node.childNodes[0]
-
-      if (linkNode && linkNode.textContent) {
-        const props = { key: href + index, url: href, color }
-        const next = node.nextSibling
-        const text = linkNode.textContent
-
-        // Check to see if there's an apostrophe following a linked section of
-        // text and if found, return it.
-        const apostrophe = get(next, n => {
-          const str = n.textContent.substr(0, 2)
-          if (/[’'][a-zA-Z]/.test(str)) {
-            return str
-          }
-        })
-
-        if (apostrophe) {
-          // Remove the apostrophe from the original text
-          next.textContent = next.textContent.substring(2)
-          // And wrap the whole thing with a span preventing whitespace breaks
-          return (
-            <span
-              className="preventLineBreak"
-              key={`apostropheLinkNode-${props.key}`}
-            >
-              <LinkWithTooltip {...props}>{text}</LinkWithTooltip>
-              {apostrophe}
-            </span>
-          )
-        }
-
-        return <LinkWithTooltip {...props}>{text}</LinkWithTooltip>
+    if (node.tagName === "A") {
+      if (this.isArtistFollow(node)) {
+        return this.renderArtistFollowButton(node)
+      }
+      if (this.shouldShowTooltipForURL(node)) {
+        return this.renderLinkWithToolTip(node, index)
       }
     }
   }
@@ -158,3 +201,9 @@ export class Text extends Component<Props, State> {
     )
   }
 }
+
+const FollowContainer = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+  padding-left: 15px;
+`
