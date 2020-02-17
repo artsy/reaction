84b9d522b05e6e6c1637c6579fbe08f06512a7f3
Christopher Pappas <damassi.pappas@gmail.com>
2020-02-16 23:25:04 -0800

    [ArtworkApp] Enable artwork caching on inquiry works

--- src/Apps/Artwork/routes.tsx
+++ src/Apps/Artwork/routes.tsx
@@ -1,5 +1,7 @@
 import loadable from "@loadable/component"
+import { request } from "graphql-request"
 import { graphql } from "react-relay"
+import { getENV } from "Utils/getENV"
 
 export const routes = [
   {
@@ -12,8 +14,35 @@ export const routes = [
         }
       }
     `,
-    cacheConfig: {
-      force: true,
+
+    /**
+     * Disable artwork page caching for everything but *inquiry works*
+     */
+    getCacheConfig: async ({ params }) => {
+      if (getENV("EXPERIMENTAL_APP_SHELL")) {
+        const query = `{
+          artwork(id: "${params.artworkID}") {
+            is_inquireable
+            is_acquireable
+            is_offerable
+          }
+        }`
+
+        const { artwork } = await request(getENV("METAPHYSICS_ENDPOINT"), query)
+        const inquiryWork =
+          artwork.is_inquireable &&
+          !artwork.is_acquireable &&
+          !artwork.is_offerable
+        const force = inquiryWork ? false : true
+
+        return {
+          force,
+        }
+      } else {
+        return {
+          force: true,
+        }
+      }
     },
   },
 ]
