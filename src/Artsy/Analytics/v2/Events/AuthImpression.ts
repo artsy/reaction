b13f1d9f1bfda1060a089a9c071e2935653658e9
Eve Essex <eve.essex@gmail.com>
2020-02-18 11:30:10 -0500

    add auth impression event

--- src/Artsy/Analytics/v2/Events/AuthImpression.ts
+++ src/Artsy/Analytics/v2/Events/AuthImpression.ts
@@ -1,16 +1,78 @@
-import { ContextModule } from "Artsy/Analytics/Schema/Values"
+import { ActionTypes } from "./Types"
+
+/*
+ * the component where the modal was triggered.
+ */
+export type ContextModule = "header"
+
+/**
+ * the type of modal to display.
+ */
+export type Type = "signup" | "login" | "forgot"
+
+/**
+ * the action taken that prompted user to signup or login.
+ */
+export type Intent =
+  | "signup"
+  | "login"
+  | "forgot"
+  | "followArtist"
+  | "followGene"
+
+/**
+ * the type of action that opened the modal
+ */
+export type Trigger = "click" | "timed"
 
 export interface AuthImpression {
+  action: ActionTypes.AuthImpression
   context_module: ContextModule
-  trigger: string
-  intent: string
-  type: "auth_impression"
+  intent: Intent
+  modal_copy?: string
   onboarding: boolean
-  trigger_seconds: number
+  trigger: Trigger
+  trigger_seconds?: number
+  type: Type
 }
 
-export const AuthImpression = () => {
+export interface AuthImpressionArgs {
+  copy?: string
+  contextModule: ContextModule
+  intent: Intent
+  onboarding?: boolean
+  trigger?: Trigger
+  triggerSeconds?: number
+  type: Type
+}
+
+/**
+ * Action fired when an auth form is viewed
+ *
+ * @example
+ * authImpression({
+ *   contextModule: "header",
+ *   intent: "signup",
+ *   type: "signup",
+ * })
+ */
+export const authImpression = ({
+  contextModule,
+  intent,
+  copy,
+  onboarding,
+  trigger,
+  triggerSeconds,
+  type,
+}: AuthImpressionArgs): AuthImpression => {
   return {
-    type: "auth_impression",
+    action: ActionTypes.AuthImpression,
+    context_module: contextModule,
+    intent,
+    modal_copy: copy,
+    onboarding: onboarding || false,
+    trigger: trigger || (triggerSeconds && "timed") || "click",
+    trigger_seconds: triggerSeconds,
+    type,
   }
 }
---
+++ src/Artsy/Analytics/v2/Events/__tests__/AuthImpression.test.ts
@@ -0,0 +1,44 @@
+import { authImpression } from "../AuthImpression"
+
+describe("authImpression", () => {
+  it("Works with minimal args", () => {
+    const args = authImpression({
+      contextModule: "header",
+      intent: "signup",
+      type: "signup",
+    })
+
+    expect(args).toEqual({
+      action: "authImpression",
+      context_module: "header",
+      intent: "signup",
+      modal_copy: undefined,
+      onboarding: false,
+      trigger: "click",
+      trigger_seconds: undefined,
+      type: "signup",
+    })
+  })
+
+  it("Works with all args", () => {
+    const args = authImpression({
+      contextModule: "header",
+      intent: "followArtist",
+      copy: "Sign up to follow artists",
+      onboarding: true,
+      triggerSeconds: 4,
+      type: "signup",
+    })
+
+    expect(args).toEqual({
+      action: "authImpression",
+      context_module: "header",
+      intent: "followArtist",
+      modal_copy: "Sign up to follow artists",
+      onboarding: true,
+      trigger: "timed",
+      trigger_seconds: 4,
+      type: "signup",
+    })
+  })
+})
