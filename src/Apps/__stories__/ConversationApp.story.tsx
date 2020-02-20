7763644876c02612912561865da7ff5528cdbc5c
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2020-02-20 16:55:45 -0500

    Remove conversation detail stroy

--- src/Apps/__stories__/ConversationApp.story.tsx
+++ src/Apps/__stories__/ConversationApp.story.tsx
@@ -3,20 +3,11 @@ import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { conversationRoutes } from "../Conversation/routes"
 
-storiesOf("Apps/Conversation", module)
-  .add("Default", () => {
-    return (
-      <MockRouter
-        routes={conversationRoutes}
-        initialRoute="/user/conversations"
-      />
-    )
-  })
-  .add("Detail", () => {
-    return (
-      <MockRouter
-        routes={conversationRoutes}
-        initialRoute="/user/conversations/842"
-      />
-    )
-  })
+storiesOf("Apps/Conversation", module).add("My Conversation", () => {
+  return (
+    <MockRouter
+      routes={conversationRoutes}
+      initialRoute="/user/conversations"
+    />
+  )
+})
