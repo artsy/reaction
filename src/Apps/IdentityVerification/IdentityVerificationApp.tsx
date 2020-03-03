0abab80a0bb9403e17ea1e9f0d2ae8e2624b0a36
Erik Stockmeier <erik.d.stock@gmail.com>
2020-03-03 18:37:44 -0500

    implement mutation happy path

--- src/Apps/IdentityVerification/IdentityVerificationApp.tsx
+++ src/Apps/IdentityVerification/IdentityVerificationApp.tsx
@@ -1,20 +1,37 @@
 import { Box, Button, Sans, Serif } from "@artsy/palette"
 import { IdentityVerificationApp_me } from "__generated__/IdentityVerificationApp_me.graphql"
+import {
+  IdentityVerificationAppStartFlowMutation,
+  IdentityVerificationAppStartFlowMutationResponse,
+} from "__generated__/IdentityVerificationAppStartFlowMutation.graphql"
 import { AppContainer } from "Apps/Components/AppContainer"
 import { useTracking } from "Artsy"
 import * as Schema from "Artsy/Analytics/Schema"
 import { ErrorPage } from "Components/ErrorPage"
 import React from "react"
 import { Meta, Title as HeadTitle } from "react-head"
-import { createFragmentContainer } from "react-relay"
-import { graphql } from "relay-runtime"
+import {
+  commitMutation,
+  createFragmentContainer,
+  graphql,
+  RelayProp,
+} from "react-relay"
+// import { RedirectException } from "found"
 
 interface Props {
   me: IdentityVerificationApp_me
+  // match: any
+  relay: RelayProp
 }
 
-const IdentityVerificationApp: React.FC<Props> = ({ me }) => {
+const IdentityVerificationApp: React.FC<Props> = ({ me, relay }) => {
+  // if (!me) {
+  //   const loginPath =
+  //     "/log_in?redirect_uri=" + encodeURIComponent(match.location.pathname)
+  //   throw new RedirectException(loginPath)
+  // }
   const { identityVerification } = me
+  const { environment } = relay
 
   if (!identityVerification || identityVerification.userID !== me.internalID) {
     return <ErrorPage code={404} />
@@ -22,12 +39,66 @@ const IdentityVerificationApp: React.FC<Props> = ({ me }) => {
 
   const { trackEvent } = useTracking()
 
-  const clickContinueToVerification = () => {
+  const clickedContinueToVerification = () => {
     trackEvent({
       context_page_owner_id: identityVerification.internalID,
       action_type: Schema.ActionType.ClickedContinueToIdVerification,
       context_page: Schema.PageName.IdentityVerificationPage,
     })
+  }
+
+  function startIdentityVerification() {
+    const mutation = new Promise<
+      IdentityVerificationAppStartFlowMutationResponse
+    >((resolve, reject) => {
+      commitMutation<IdentityVerificationAppStartFlowMutation>(environment, {
+        onCompleted: data => resolve(data),
+        onError: error => reject(error),
+        mutation: graphql`
+          mutation IdentityVerificationAppStartFlowMutation(
+            $input: startIdentityVerificationMutationInput!
+          ) {
+            startIdentityVerification(input: $input) {
+              startIdentityVerificationResponseOrError {
+                ... on StartIdentityVerificationSuccess {
+                  identityVerificationFlowUrl
+                }
+                ... on StartIdentityVerificationFailure {
+                  mutationError {
+                    detail
+                    error
+                    message
+                  }
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          input: { identityVerificationId: identityVerification.internalID },
+        },
+      })
+    })
+
+    mutation
+      .then(result => {
+        const {
+          startIdentityVerification: {
+            startIdentityVerificationResponseOrError: {
+              identityVerificationFlowUrl,
+            },
+          },
+        } = result
+        console.log("success: " + { identityVerificationFlowUrl })
+        console.log({ identityVerificationFlowUrl })
+        location.assign(identityVerificationFlowUrl)
+      })
+      .catch(result => {
+        const {
+          startIdentityVerification: { mutationError },
+        } = result
+        console.log("FAIL", { mutationError })
+      })
   }
 
   return (
@@ -85,7 +156,8 @@ const IdentityVerificationApp: React.FC<Props> = ({ me }) => {
             width={["100%", 335]}
             mt={4}
             onClick={() => {
-              clickContinueToVerification()
+              clickedContinueToVerification()
+              startIdentityVerification()
             }}
           >
             Continue to verification
