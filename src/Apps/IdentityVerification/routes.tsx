26747e0deb79b559641d1543df4482e63cbce97e
Barry Hoggard <barry@hoggard.org>
2020-03-18 14:00:33 -0400

    fix preload

--- src/Apps/IdentityVerification/routes.tsx
+++ src/Apps/IdentityVerification/routes.tsx
@@ -2,18 +2,24 @@ import loadable from "@loadable/component"
 import { RouteConfig } from "found"
 import { graphql } from "react-relay"
 
+const IdentityVerificationApp = loadable(() =>
+  import("./IdentityVerificationApp")
+)
 const Processing = loadable(() => import("./Processing"))
 
 export const routes: RouteConfig[] = [
   {
     path: "/identity-verification/processing",
     getComponent: () => Processing,
+    prepare: () => {
+      Processing.preload()
+    },
   },
   {
     path: "/identity-verification/:id",
-    getComponent: () => loadable(() => import("./IdentityVerificationApp")),
+    getComponent: () => IdentityVerificationApp,
     prepare: () => {
-      Processing.preload()
+      IdentityVerificationApp.preload()
     },
     query: graphql`
       query routes_IdentityVerificationAppQuery($id: String!)
