a048e6351cfb86d3059f01ded21aead5886f4c76
xtina-starr <c.thompsonms@gmail.com>
2020-04-15 13:56:27 -0400

    pull section with two columns into its own component

--- src/Components/NavBar/Menus/DropDownSection.tsx
+++ src/Components/NavBar/Menus/DropDownSection.tsx
@@ -1,6 +1,7 @@
-import { Box, color, Flex, MenuItem, Sans } from "@artsy/palette"
+import { Box, color, MenuItem, Sans } from "@artsy/palette"
 import React from "react"
 import { MenuLinkData, SimpleLinkData } from "../menuData"
+import { TwoColumnDropDownSection } from "./TwoColumnDropDownSection"
 
 interface DropDownSectionProps {
   section: MenuLinkData
@@ -14,52 +15,7 @@ export const DropDownSection: React.FC<DropDownSectionProps> = ({
   return (
     <>
       {section.text === "Artist Nationality & Region" ? (
-        <Flex>
-          <Box width={[110, 110, 110, 135, 150]} py={4} mr={[2, 2, 3, 3]}>
-            <Sans size="2" mb={1}>
-              {section.text}
-            </Sans>
-            {section.menu &&
-              section.menu.links.slice(0, 8).map((menuItem: SimpleLinkData) => {
-                return (
-                  <MenuItem
-                    key={menuItem.text}
-                    px={0}
-                    py={0.5}
-                    href={menuItem.href}
-                    textColor={color("black60")}
-                    textWeight="regular"
-                    fontSize="3t"
-                  >
-                    {menuItem.text}
-                  </MenuItem>
-                )
-              })}
-          </Box>
-          <Box
-            width={[110, 110, 110, 135, 150]}
-            pt="66px"
-            pb={4}
-            mr={[2, 2, 3, 3]}
-          >
-            {section.menu &&
-              section.menu.links.slice(-8).map((menuItem: SimpleLinkData) => {
-                return (
-                  <MenuItem
-                    key={menuItem.text}
-                    px={0}
-                    py={0.5}
-                    href={menuItem.href}
-                    textColor={color("black60")}
-                    textWeight="regular"
-                    fontSize="3t"
-                  >
-                    {menuItem.text}
-                  </MenuItem>
-                )
-              })}
-          </Box>
-        </Flex>
+        <TwoColumnDropDownSection section={section} />
       ) : (
         <Box width={[110, 110, 110, 135, 150]} py={4} mr={[2, 2, 3, 3]}>
           <Sans size="2" mb={1}>
