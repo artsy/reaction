c16422676ee1d4b7abe4fa41ff357f0bbe263b47
Justin Bennett <zephraph@gmail.com>
2020-04-30 14:44:04 -0400

    Add fallback logic if routeIndicies aren't present

--- src/Artsy/Router/Utils/findCurrentRoute.tsx
+++ src/Artsy/Router/Utils/findCurrentRoute.tsx
@@ -1,9 +1,13 @@
 import { Match, RouteConfig } from "found"
 
 export const findCurrentRoute = ({
+  route: baseRoute,
   routes,
   routeIndices,
 }: Match & { route?: RouteConfig }) => {
+  if (!routeIndices || routeIndices.length === 0) {
+    return baseRoute
+  }
   let remainingRouteIndicies = [...routeIndices]
   let route: RouteConfig = routes[remainingRouteIndicies.shift()]
 
