1be7fafc800e1352dff8f69216a64c005ef366b4
Devon Blandin <dblandin@gmail.com>
2020-05-08 16:20:28 -0400

    Update 2FA enrollment API modal to use Palette dialog

--- src/Components/UserSettings/TwoFactorAuthentication/Components/ApiErrorModal.tsx
+++ src/Components/UserSettings/TwoFactorAuthentication/Components/ApiErrorModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Sans } from "@artsy/palette"
+import { Dialog } from "@artsy/palette"
 import React from "react"
 import { ApiError } from "../ApiError"
 
@@ -14,15 +14,14 @@ export const ApiErrorModal: React.FC<ApiErrorModalProps> = props => {
   const errorMessage = errors.map(error => error.message).join("\n")
 
   return (
-    <Modal
-      forcedScroll={false}
-      title="An error occurred"
+    <Dialog
       show={show}
-      onClose={onClose}
-    >
-      <Sans color="black60" textAlign="center" size="3t">
-        {errorMessage}
-      </Sans>
-    </Modal>
+      detail={errorMessage}
+      title="An error occurred"
+      primaryCta={{
+        action: onClose,
+        text: "OK",
+      }}
+    />
   )
 }
