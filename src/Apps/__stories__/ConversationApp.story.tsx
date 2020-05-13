89d12da7e86decec2f8f22fd730a0d84b238a9cd
Justin Bennett <zephraph@gmail.com>
2020-05-13 13:37:26 -0400

    Update conversations story to use mocked conversation

--- src/Apps/__stories__/ConversationApp.story.tsx
+++ src/Apps/__stories__/ConversationApp.story.tsx
@@ -2,12 +2,54 @@ import { MockRouter } from "DevTools/MockRouter"
 import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { conversationRoutes } from "../Conversation/routes"
+import { MockedConversation } from "Apps/__tests__/Fixtures/Conversation"
 
-storiesOf("Apps/Conversation", module).add("My Conversation", () => {
-  return (
-    <MockRouter
-      routes={conversationRoutes}
-      initialRoute="/user/conversations"
-    />
-  )
-})
+const mock = {
+  me: {
+    name: "tester",
+    conversationsConnection: {
+      edges: [
+        {
+          to: {
+            name: "Artifical Gallery",
+          },
+        },
+        {
+          to: {
+            name: "Fake it till you make it Gallery",
+          },
+        },
+      ],
+    },
+  },
+}
+
+storiesOf("Apps/Conversation", module)
+  .add("Conversations list", () => {
+    return (
+      <MockRouter
+        routes={conversationRoutes}
+        initialRoute="/user/conversations"
+        mockData={mock}
+        context={{
+          user: {
+            lab_features: ["User Conversations View"],
+          },
+        }}
+      />
+    )
+  })
+  .add("Single Conversation", () => {
+    return (
+      <MockRouter
+        routes={conversationRoutes}
+        initialRoute="/user/conversations/123"
+        context={{
+          user: {
+            lab_features: ["User Conversations View"],
+          },
+        }}
+        mockData={MockedConversation}
+      />
+    )
+  })
