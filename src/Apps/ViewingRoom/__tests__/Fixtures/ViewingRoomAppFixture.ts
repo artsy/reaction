e650171845ff77cce7a637bbf0669b35d9b1a0fa
Christopher Pappas <damassi.pappas@gmail.com>
2020-05-12 22:32:42 -0700

    [Viewing Room] Add tests

---
+++ src/Apps/ViewingRoom/__tests__/Fixtures/ViewingRoomAppFixture.ts
@@ -0,0 +1,16 @@
+import { ViewingRoomApp_Test_QueryRawResponse } from "__generated__/ViewingRoomApp_Test_Query.graphql"
+
+export const ViewingRoomAppFixture: ViewingRoomApp_Test_QueryRawResponse = {
+  viewingRoom: {
+    title: "Guy Yanai",
+    introStatement:
+      "Checked into a Club Med in the French Alps, and quickly discovered it was not what he expected. The hotel was an outdated ski lodge without any snow. “It was this horrible vacation,” the fortysomething artist said of his family trip there, a few years back. Still, he wanted to paint the drab resort—maybe so he could get a do-over of his vacation, this time in colorful and glorious surroundings.",
+    heroImageURL:
+      "https://d7hftxdivxxvm.cloudfront.net/?resize_to=width&src=https%3A%2F%2Fartsy-media-uploads.s3.amazonaws.com%2F0RnxWDsVmKuALfpmd75YyA%2FCTPHSEPT19_018_JO_Guy_Yanai_TLV_031_20190913.jpg&width=1200&quality=80",
+    partner: {
+      name: "Subscription Demo GG",
+      id: "UGFydG5lcjo1NTQxMjM3MzcyNjE2OTJiMTk4YzAzMDA=",
+    },
+    endAt: "2020-06-11T14:42:46Z",
+  },
+}
--- src/Apps/ViewingRoom/__tests__/ViewingRoomApp.test.tsx
+++ src/Apps/ViewingRoom/__tests__/ViewingRoomApp.test.tsx
@@ -1,5 +1,92 @@
-describe.skip("ViewingRoomApp", () => {
-  it("todo", () => {
-    //
+import React from "react"
+import { MockBoot, renderRelayTree } from "DevTools"
+import ViewingRoomApp from "../ViewingRoomApp"
+import { graphql } from "react-relay"
+import { ViewingRoomApp_Test_QueryRawResponse } from "__generated__/ViewingRoomApp_Test_Query.graphql"
+import { ViewingRoomAppFixture } from "./Fixtures/ViewingRoomAppFixture"
+import { Breakpoint } from "@artsy/palette"
+
+jest.unmock("react-relay")
+jest.mock("Artsy/Router/useRouter", () => ({
+  useRouter: () => ({
+    match: {
+      params: {
+        slug: "subscription-demo-gg-guy-yanai",
+      },
+    },
+  }),
+  useIsRouteActive: () => false,
+}))
+
+describe("ViewingRoomApp", () => {
+  const slug = "subscription-demo-gg-guy-yanai"
+
+  const getWrapper = async (
+    breakpoint: Breakpoint = "lg",
+    response: ViewingRoomApp_Test_QueryRawResponse = ViewingRoomAppFixture
+  ) => {
+    return await renderRelayTree({
+      Component: ({ viewingRoom }) => {
+        return (
+          <MockBoot breakpoint={breakpoint}>
+            <ViewingRoomApp viewingRoom={viewingRoom}>
+              some child
+            </ViewingRoomApp>
+          </MockBoot>
+        )
+      },
+      query: graphql`
+        query ViewingRoomApp_Test_Query($slug: ID!) @raw_response_type {
+          viewingRoom(id: $slug) {
+            ...ViewingRoomApp_viewingRoom
+          }
+        }
+      `,
+      variables: {
+        slug,
+      },
+      mockData: response,
+    })
+  }
+
+  it("renders the correct components", async () => {
+    const wrapper = await getWrapper()
+    expect(wrapper.find("ViewingRoomMeta").length).toBe(1)
+    expect(wrapper.find("AppContainer").length).toBe(1)
+    expect(wrapper.find("ViewingRoomHeader").length).toBe(1)
+    expect(wrapper.find("ViewingRoomTabBar").length).toBe(1)
+    expect(wrapper.html()).toContain("some child")
+  })
+
+  describe("ViewingRoomHeader", () => {
+    describe("desktop", () => {
+      it("renders correctly", async () => {
+        const wrapper = await getWrapper()
+        expect(wrapper.find("ResponsiveImage").length).toBe(1)
+        const html = wrapper.html()
+        expect(html).toContain("Guy Yanai")
+        expect(html).toContain("Subscription Demo GG")
+      })
+    })
+
+    describe("mobile", () => {
+      it("renders correctly", async () => {
+        const wrapper = await getWrapper()
+        expect(wrapper.find("ResponsiveImage").length).toBe(1)
+        const html = wrapper.html()
+        expect(html).toContain("Guy Yanai")
+        expect(html).toContain("Subscription Demo GG")
+      })
+    })
+  })
+
+  describe("ViewingRoomTabBar", () => {
+    it("renders correct tabs", async () => {
+      const wrapper = await getWrapper()
+      expect(wrapper.find("Tab").length).toBe(2)
+      const html = wrapper.html()
+      expect(html).toContain(`href="/viewing-room/${slug}"`)
+      expect(html).toContain(`href="/viewing-room/${slug}/works"`)
+    })
   })
 })
