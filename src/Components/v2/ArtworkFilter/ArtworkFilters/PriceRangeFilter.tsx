5a40f3b8f079ee3e32af4c50590f293d8675c2e2
Pavlos Vinieratos <pvinis@gmail.com>
2020-05-19 11:52:09 +0200

    some initial work

--- src/Components/v2/ArtworkFilter/ArtworkFilters/PriceRangeFilter.tsx
+++ src/Components/v2/ArtworkFilter/ArtworkFilters/PriceRangeFilter.tsx
@@ -1,31 +1,62 @@
-import { Flex, PriceRange, Toggle } from "@artsy/palette"
+import { Flex, Radio, RadioGroup, Toggle } from "@artsy/palette"
 import React, { FC } from "react"
 import { useArtworkFilterContext } from "../ArtworkFilterContext"
-import { MAX_PRICE, MIN_PRICE } from "../Utils/rangeToTuple"
 
 export const PriceRangeFilter: FC = () => {
   const filterContext = useArtworkFilterContext()
   const [initialMin, initialMax] = filterContext.rangeToTuple("priceRange")
+  // where can we get this from? what if the range doesnt exist?
 
   return (
     <Toggle label="Price" expanded>
       <Flex flexDirection="column" alignItems="left" my={1}>
-        <PriceRange
-          allowCross={false}
-          min={MIN_PRICE}
-          max={MAX_PRICE}
-          step={50}
-          disabledText="Disabled for biddable works"
-          defaultValue={[initialMin, initialMax]}
-          disabled={Boolean(filterContext.filters.atAuction)}
-          onAfterChange={([min, max]) => {
-            const minStr = min === MIN_PRICE ? "*" : min
-            const maxStr = max === MAX_PRICE ? "*" : max
-
-            filterContext.setFilter("priceRange", `${minStr}-${maxStr}`)
+        <RadioGroup
+          deselectable
+          onSelect={selectedOption => {
+            filterContext.setFilter("priceRange", selectedOption)
           }}
-        />
+        >
+          {priceRanges.map((range, index) => (
+            <Radio
+              key={`${index}`}
+              my={0.3}
+              label={range.name}
+              value={range.value}
+            />
+          ))}
+        </RadioGroup>
       </Flex>
     </Toggle>
   )
 }
+
+const priceRanges = [
+  {
+    name: "$50,000+",
+    value: "50000-*",
+  },
+  // how do i get 40k - 50k?!
+  /// dollars or pounds?
+  {
+    name: "$20,000 - $40,000",
+    value: "20000-40000",
+  },
+  {
+    name: "$10,000 - $20,000",
+    value: "10000-20000",
+  },
+  {
+    name: "$5,000 - $10,000",
+    value: "5000-10000",
+  },
+  {
+    name: "$1,000 - $5,000",
+    value: "1000-5000",
+  },
+  {
+    name: "$0 - $1,000",
+    value: "0-1000",
+  },
+]
+//   disabledText="Disabled for biddable works"
+//   disabled={Boolean(filterContext.filters.atAuction)}
