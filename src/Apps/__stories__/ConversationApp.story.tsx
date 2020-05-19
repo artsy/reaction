17486d64d7534d87159ba14bc5ee004bf9f6523f
Justin Bennett <zephraph@gmail.com>
2020-05-19 14:46:26 -0400

    Add conversation to storybooks

--- src/Apps/__stories__/ConversationApp.story.tsx
+++ src/Apps/__stories__/ConversationApp.story.tsx
@@ -2,54 +2,19 @@ import { MockRouter } from "DevTools/MockRouter"
 import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { conversationRoutes } from "../Conversation/routes"
-import { MockedConversation } from "Apps/__tests__/Fixtures/Conversation"
 
-const mock = {
-  me: {
-    name: "tester",
-    conversationsConnection: {
-      edges: [
-        {
-          to: {
-            name: "Artifical Gallery",
-          },
+storiesOf("Apps/Conversation", module).add("Conversations list", () => {
+  return (
+    <MockRouter
+      routes={conversationRoutes}
+      initialRoute="/user/conversations"
+      context={{
+        user: {
+          id: process.env.USER_ID,
+          accessToken: process.env.USER_ACCESS_TOKEN,
+          lab_features: ["User Conversations View"],
         },
-        {
-          to: {
-            name: "Fake it till you make it Gallery",
-          },
-        },
-      ],
-    },
-  },
-}
-
-storiesOf("Apps/Conversation", module)
-  .add("Conversations list", () => {
-    return (
-      <MockRouter
-        routes={conversationRoutes}
-        initialRoute="/user/conversations"
-        mockData={mock}
-        context={{
-          user: {
-            lab_features: ["User Conversations View"],
-          },
-        }}
-      />
-    )
-  })
-  .add("Single Conversation", () => {
-    return (
-      <MockRouter
-        routes={conversationRoutes}
-        initialRoute="/user/conversations/123"
-        context={{
-          user: {
-            lab_features: ["User Conversations View"],
-          },
-        }}
-        mockData={MockedConversation}
-      />
-    )
-  })
+      }}
+    />
+  )
+})
