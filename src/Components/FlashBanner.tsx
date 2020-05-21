ed2b9480747f613bd3f20e937973687f3a8b0aee
Erik Stockmeier <erik.d.stock@gmail.com>
2020-05-21 15:16:56 -0400

    refactor message formatting and add a test

--- src/Components/FlashBanner.tsx
+++ src/Components/FlashBanner.tsx
@@ -2,28 +2,43 @@ import React from "react"
 import { Banner, Sans } from "@artsy/palette"
 
 interface Props {
-  messageCode: string
+  messageCode: FlashMessageKey
 }
 
-const textMessage: (msg: string) => React.FC = (message: string) => () => (
-  <Sans color="white100" size="3" lineHeight={1} weight="medium">
-    {message}
-  </Sans>
-)
+export type FlashMessageKey =
+  /* email confirmed */
+  | "confirmed"
+  /* email already confirmed */
+  | "already_confirmed"
+  /* email confirmation token was invalid */
+  | "invalid_token"
+  /* email confirmation token was empty */
+  | "blank_token"
+  /* email confirmation token was expired */
+  | "expired_token"
 
-const messages: { [k: string]: React.FC } = {
-  confirmed: textMessage("Your email has been confirmed."),
-  // TODO: I don't think we should show this as a fallback as any string could trigger it
-  fallback: textMessage("An error has occurred. Please try again."),
+type BannerMessage = string | React.FC
+
+const messages: Record<FlashMessageKey, BannerMessage> = {
+  confirmed: "Your email has been confirmed.",
+  already_confirmed: "You have already confirmed your email.",
+  invalid_token: "Your token is invalid. Please try again.",
+  blank_token: "No token found. Please try again.",
+  expired_token: "An error has occurred. Please try again.",
 }
 
 export const FlashBanner: React.FunctionComponent<Props> = ({
   messageCode,
 }) => {
-  const Message = messages[messageCode] || messages.fallback
+  const Message = messages[messageCode]
+  if (!Message) return null
   return (
-    <Banner dismissable backgroundColor="black100">
-      <Message />
-    </Banner>
+    Message && (
+      <Banner dismissable backgroundColor="black100">
+        <Sans color="white100" size="3" lineHeight={1} weight="medium">
+          {typeof Message === "string" ? Message : <Message />}
+        </Sans>
+      </Banner>
+    )
   )
 }
--- src/Components/__stories__/FlashBanner.story.tsx
+++ src/Components/__stories__/FlashBanner.story.tsx
@@ -2,10 +2,9 @@ import React from "react"
 import { storiesOf } from "storybook/storiesOf"
 import { FlashBanner } from "Components/FlashBanner"
 
-storiesOf("Components/FlashBanner", module)
-  .add("Successful Confirmation Message", () => {
+storiesOf("Components/FlashBanner", module).add(
+  "Successful Confirmation Message",
+  () => {
     return <FlashBanner messageCode="confirmed" />
-  })
-  .add("Fallback message", () => {
-    return <FlashBanner messageCode="uhoh" />
-  })
+  }
+)
